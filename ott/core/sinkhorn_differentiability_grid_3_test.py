66ac423214d7a598b83332445656b5e3e48c2963
Marco Cuturi <cuturi@google.com>
2021-01-14 14:18:08 +0000

    - include long flag in BUILD for sinkhorn_differentiability_grid_test, split in 3.
    PiperOrigin-RevId: 351786476

--- ott/core/sinkhorn_differentiability_grid_3_test.py
+++ ott/core/sinkhorn_differentiability_grid_3_test.py
@@ -32,67 +32,6 @@ class SinkhornGradGridTest(jax.test_util.JaxTestCase):
     self.rng = jax.random.PRNGKey(0)
 
   @parameterized.parameters([True], [False])
-  def test_autograd_sinkhorn_grid(self, lse_mode):
-    """Test gradient w.r.t. probability weights."""
-    eps = 1e-3  # perturbation magnitude
-    keys = jax.random.split(self.rng, 3)
-    grid_size = (2, 3, 4)
-    a = jax.random.uniform(keys[0], grid_size) + 1.0
-    b = jax.random.uniform(keys[1], grid_size) + 1.0
-    a = a.ravel() / np.sum(a)
-    b = b.ravel() / np.sum(b)
-    geom = grid.Grid(grid_size=grid_size, epsilon=0.2)
-
-    def reg_ot(a, b):
-      return sinkhorn.sinkhorn(
-          geom, a=a, b=b, threshold=0.1, lse_mode=lse_mode).reg_ot_cost
-
-    reg_ot_and_grad = jax.jit(jax.value_and_grad(reg_ot))
-    _, grad_reg_ot = reg_ot_and_grad(a, b)
-    delta = jax.random.uniform(keys[2], grid_size).ravel()
-    delta = delta - np.mean(delta)
-
-    # center perturbation
-    reg_ot_delta_plus = reg_ot(a + eps * delta, b)
-    reg_ot_delta_minus = reg_ot(a - eps * delta, b)
-    delta_dot_grad = np.sum(delta * grad_reg_ot)
-    self.assertAllClose(delta_dot_grad,
-                        (reg_ot_delta_plus - reg_ot_delta_minus) / (2 * eps),
-                        rtol=1e-03, atol=1e-02)
-
-  @parameterized.parameters([True], [False])
-  def test_autograd_sinkhorn_x_grid(self, lse_mode):
-    """Test gradient w.r.t. probability weights."""
-    eps = 1e-3  # perturbation magnitude
-    keys = jax.random.split(self.rng, 3)
-    x = (np.array([.0, 1.0], dtype=np.float32),
-         np.array([.3, .4, .7], dtype=np.float32),
-         np.array([1.0, 1.3, 2.4, 3.7], dtype=np.float32))
-    grid_size = tuple([xs.shape[0] for xs in x])
-    a = jax.random.uniform(keys[0], grid_size) + 1.0
-    b = jax.random.uniform(keys[1], grid_size) + 1.0
-    a = a.ravel() / np.sum(a)
-    b = b.ravel() / np.sum(b)
-    geom = grid.Grid(x=x, epsilon=1.0)
-
-    def reg_ot(a, b):
-      return sinkhorn.sinkhorn(
-          geom, a=a, b=b, threshold=0.1, lse_mode=lse_mode).reg_ot_cost
-
-    reg_ot_and_grad = jax.jit(jax.value_and_grad(reg_ot))
-    _, grad_reg_ot = reg_ot_and_grad(a, b)
-    delta = jax.random.uniform(keys[2], grid_size).ravel()
-    delta = delta - np.mean(delta)
-
-    # center perturbation
-    reg_ot_delta_plus = reg_ot(a + eps * delta, b)
-    reg_ot_delta_minus = reg_ot(a - eps * delta, b)
-    delta_dot_grad = np.sum(delta * grad_reg_ot)
-    self.assertAllClose(delta_dot_grad,
-                        (reg_ot_delta_plus - reg_ot_delta_minus) / (2 * eps),
-                        rtol=1e-03, atol=1e-02)
-
-  @parameterized.parameters([True], [False])
   def test_autograd_sinkhorn_x_grid_x_perturbation(self, lse_mode):
     """Test gradient w.r.t. probability weights."""
     eps = 1e-3  # perturbation magnitude
