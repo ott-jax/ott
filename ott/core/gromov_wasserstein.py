295c7ed6085b89dc184578c2f99c53060eba76b2
Laetitia Papaxanthos <lpapaxanthos@google.com>
2021-04-26 15:58:12 +0000

    Gradient for GW algorithm. Modification of initialisation and calculation of cost matrix for GW.
    PiperOrigin-RevId: 370468942

--- ott/core/gromov_wasserstein.py
+++ ott/core/gromov_wasserstein.py
@@ -28,8 +28,6 @@ from ott.geometry import epsilon_scheduler
 from ott.geometry import geometry
 from ott.geometry import pointcloud
 
-EPS = 1e-5
-
 GromovWassersteinOutput = collections.namedtuple(
     'GromovWassersteinOutput', ['f', 'g', 'transport', 'cost_matrix', 'gw_cost',
                                 'reg_gw_cost', 'reg_gw_cost_arr',
@@ -49,7 +47,7 @@ class GWLoss(abc.ABC):
     pass
 
   @abc.abstractmethod
-  def fn_y(self, x):
+  def fn_y(self, y):
     pass
 
   @abc.abstractmethod
@@ -57,7 +55,7 @@ class GWLoss(abc.ABC):
     pass
 
   @abc.abstractmethod
-  def right_y(self, x):
+  def right_y(self, y):
     pass
 
   def tree_flatten(self):
@@ -91,19 +89,20 @@ class GWSqEuclLoss(GWLoss):
 class GWKlLoss(GWLoss):
   """Implements the KL divergence for the Gromov-Wasserstein loss."""
 
-  # TODO(lpapaxanthos): stable implementation for KL
-  def fn_x(self, x):
-    x = jnp.clip(x, EPS)
-    return jax.scipy.special.xlogy(x, x) - x
+  def __init__(self, clipping_value=1e-8):
+    self.clipping_value = clipping_value
 
-  def fn_y(self, x):
-    return x
+  def fn_x(self, x):
+    return -jax.scipy.special.entr(x) - x
+
+  def fn_y(self, y):
+    return y
 
   def left_x(self, x):
     return x
 
-  def right_y(self, x):
-    return jnp.log(jnp.clip(x, EPS))
+  def right_y(self, y):
+    return jnp.log(jnp.clip(y, self.clipping_value))
 
 
 _GW_LOSSES = [GWSqEuclLoss, GWKlLoss]
@@ -116,12 +115,10 @@ def gromov_wasserstein(
     geom_y: geometry.Geometry,
     a: Optional[jnp.ndarray] = None,
     b: Optional[jnp.ndarray] = None,
-    epsilon: Union[epsilon_scheduler.Epsilon, float] = 1e-2,
+    epsilon: Union[epsilon_scheduler.Epsilon, float] = 1.,
     loss: Union[str, GWLoss] = 'sqeucl',
-    tau_a: float = 1.0,
-    tau_b: float = 1.0,
-    max_iterations_gromov: int = 20,
-    jit_gromov: bool = False,
+    max_iterations: int = 20,
+    jit: bool = False,
     sinkhorn_kwargs: Optional[Dict[str, Any]] = None,
     **kwargs) -> GromovWassersteinOutput:
   """Fits Gromov Wasserstein.
@@ -135,15 +132,9 @@ def gromov_wasserstein(
     loss: str or GWLoss object, whether to use the square euclidean loss
      ('sqeucl' or GWSqEuclLoss), the KL loss ('kl' or GWKlLoss) or a
      user-defined instance of GWLoss.
-    tau_a: float, ratio lam/(lam+eps) between KL divergence regularizer to first
-     marginal and itself + epsilon regularizer used in the unbalanced
-     formulation.
-    tau_b: float, ratio lam/(lam+eps) between KL divergence regularizer to
-     second marginal and itself + epsilon regularizer used in the unbalanced
-     formulation.
-    max_iterations_gromov: int32, the maximum number of outer iterations for
+    max_iterations: int32, the maximum number of outer iterations for
      Gromov Wasserstein.
-    jit_gromov: bool, if True, jits the function.
+    jit: bool, if True, jits the function.
     sinkhorn_kwargs: Optionally a dictionary containing the keywords arguments
      for calls to the sinkhorn function.
     **kwargs: additional kwargs for epsilon.
@@ -151,11 +142,14 @@ def gromov_wasserstein(
   Returns:
     a GromovWassersteinOutput named tuple.
   """
-
   loss_fn = GW_LOSSES.get(loss, None) if isinstance(loss, str) else loss
   if loss_fn is None:
     raise ValueError('Unknown loss. Either pass an instance of GWLoss or '
                      f'a string among: [{",".join(GW_LOSSES.keys())}]')
+  tau_a = sinkhorn_kwargs.get('tau_a', 1.0)
+  tau_b = sinkhorn_kwargs.get('tau_b', 1.0)
+  if tau_a != 1.0 or tau_b != 1.0:
+    raise ValueError('Unbalanced Gromov-Wasserstein is not supported yet.')
 
   num_a = geom_x.shape[0]
   num_b = geom_y.shape[0]
@@ -163,34 +157,43 @@ def gromov_wasserstein(
   b = jnp.ones((num_b,)) / num_b if b is None else b
 
   gromov_partial = functools.partial(
-      _gromovw_iterations, epsilon=epsilon, loss=loss_fn,
-      tau_a=tau_a, tau_b=tau_b, max_iterations_gromov=max_iterations_gromov,
+      _gw_iterations, epsilon=epsilon, loss=loss_fn,
+      max_iterations=max_iterations,
       sinkhorn_kwargs=sinkhorn_kwargs, **kwargs)
-  gromov_fn = jax.jit(gromov_partial) if jit_gromov else gromov_partial
-  f, g, geom_gw, reg_gw_cost_arr, errors_sinkhorn, converged_sinkhorn = gromov_fn(
-      geom_x, geom_y, a, b)
+  gromov_fn = jax.jit(gromov_partial) if jit else gromov_partial
+  (f, g, geom_gw, reg_gw_cost_arr, errors_sinkhorn,
+      converged_sinkhorn) = gromov_fn(geom_x, geom_y, a, b)
 
+  # TODO(lpapaxanthos): remove stop_gradient when using backprop
+  geom_gw = _update_geometry_gw(
+      jax.lax.stop_gradient(geom_gw),
+      geom_x, geom_y,
+      jax.lax.stop_gradient(f),
+      jax.lax.stop_gradient(g),
+      loss_fn, **kwargs)
   transport = geom_gw.transport_from_potentials(f, g)
   cost_matrix = 0.5 * geom_gw.cost_matrix
   gw_cost = jnp.sum(transport * cost_matrix)
-  reg_gw_cost = sinkhorn.ent_reg_cost(geom_gw, a, b, tau_a, tau_b, f, g)
+  reg_gw_cost = sinkhorn.ent_reg_cost(
+      geom_gw, a, b, tau_a, tau_b,
+      jax.lax.stop_gradient(f),
+      jax.lax.stop_gradient(g))
   return GromovWassersteinOutput(f, g, transport, cost_matrix, gw_cost,
                                  reg_gw_cost, reg_gw_cost_arr, errors_sinkhorn,
                                  converged_sinkhorn)
 
 
-def _gromovw_iterations(
+def _gw_iterations(
     geom_x: geometry.Geometry,
     geom_y: geometry.Geometry,
     a: jnp.ndarray,
     b: jnp.ndarray,
-    epsilon: float,
+    epsilon: Union[epsilon_scheduler.Epsilon, float],
     loss: GWLoss,
-    tau_a: float,
-    tau_b: float,
-    max_iterations_gromov: int,
-    sinkhorn_kwargs: Optional[Dict[str, Any]] = None,
-    **kwargs) -> Tuple[jnp.ndarray]:
+    max_iterations: int,
+    sinkhorn_kwargs: Optional[Dict[str, Any]],
+    **kwargs) -> Tuple[jnp.ndarray, jnp.ndarray, geometry.Geometry, jnp.ndarray,
+                       jnp.ndarray, jnp.ndarray]:
   """Fits Gromov Wasserstein.
 
   Args:
@@ -200,89 +203,51 @@ def _gromovw_iterations(
     b: jnp.ndarray<float>[num_b,] or jnp.ndarray<float>[batch,num_b] weights.
     epsilon: a regularization parameter or a epsilon_scheduler.Epsilon object.
     loss: GWLoss object.
-    tau_a: float, ratio lam/(lam+eps) between KL divergence regularizer to first
-     marginal and itself + epsilon regularizer used in the unbalanced
-     formulation.
-    tau_b: float, ratio lam/(lam+eps) between KL divergence regularizer to 
-     second marginal and itself + epsilon regularizer used in the unbalanced
-     formulation.
-    max_iterations_gromov: int32, the maximum number of outer iterations for
-    Gromov Wasserstein.
+    max_iterations: int, the maximum number of outer iterations for
+     Gromov Wasserstein.
     sinkhorn_kwargs: Optionally a dictionary containing the keywords arguments
      for calls to the sinkhorn function.
     **kwargs: additional kwargs for epsilon.
 
   Returns:
-    f: potential
-    g: potential
-    geom_gw: a Geometry object for Gromov-Wasserstein (GW)
-    reg_gw_cost_arr: ndarray of regularised GW costs
-    errors_sinkhorn: ndarray [max_iterations_gromov, p], where p depends on
+    f: potential.
+    g: potential.
+    geom_gw: a Geometry object for Gromov-Wasserstein (GW).
+    reg_gw_cost_arr: ndarray of regularised GW costs.
+    errors_sinkhorn: ndarray [max_iterations, p], where p depends on
      sinkhorn_kwargs, of errors for the Sinkhorn algorithm for each gromov
      iteration (axis 0) and regularly spaced sinkhorn iterations (axis 1).
-    converged_sinkhorn: ndarray [max_iterations_gromov] of flags indicating that
-     the sinkhorn algorithm converged
+    converged_sinkhorn: ndarray [max_iterations,] of flags indicating
+    that the sinkhorn algorithm converged.
   """
-  # Initialises cost, GW geometry, runs first iteration of sinkhorn
-  gw_constant = _init_constant_cost(geom_x, geom_y, a, b, loss)
-  geom_gw = _init_geometry_gw(geom_x, geom_y, a, b, epsilon,
-                              gw_constant, loss, **kwargs)
-  f, g, reg_gw_cost, error_sinkhorn, converged_sinkhorn = sinkhorn.sinkhorn(
-      geom_gw, a, b, tau_a, tau_b, **sinkhorn_kwargs)
-
+  geom_gw = _init_geometry_gw(
+      geom_x, geom_y, jax.lax.stop_gradient(a), jax.lax.stop_gradient(b),
+      epsilon, loss, **kwargs)
+  f, g, reg_gw_cost, errors_sinkhorn, converged_sinkhorn = sinkhorn.sinkhorn(
+      geom_gw, a, b, **sinkhorn_kwargs)
   carry = geom_gw, f, g
   update_geom_partial = functools.partial(_update_geometry_gw, geom_x=geom_x,
-                                          geom_y=geom_y,
-                                          gw_constant=gw_constant,
-                                          loss=loss, **kwargs)
+                                          geom_y=geom_y, loss=loss, **kwargs)
   sinkhorn_partial = functools.partial(sinkhorn.sinkhorn, a=a, b=b,
-                                       tau_a=tau_a, tau_b=tau_b,
                                        **sinkhorn_kwargs)
   def body_fn(carry=carry, x=None):
     del x
     geom_gw, f, g = carry
+    # TODO(lpapaxanthos): allow for warm start
     geom_gw = update_geom_partial(geom=geom_gw, f=f, g=g)
-    f, g, reg_gw_cost, error_sinkhorn, converged_sinkhorn = sinkhorn_partial(
+    f, g, reg_gw_cost, errors_sinkhorn, converged_sinkhorn = sinkhorn_partial(
         geom=geom_gw)
-    return (geom_gw, f, g), (reg_gw_cost, error_sinkhorn, converged_sinkhorn)
+    return (geom_gw, f, g), (reg_gw_cost, errors_sinkhorn, converged_sinkhorn)
 
   carry, out = jax.lax.scan(f=body_fn, init=carry, xs=None,
-                            length=max_iterations_gromov - 1)
+                            length=max_iterations - 1)
+
   geom_gw, f, g = carry
   reg_gw_cost_arr = jnp.concatenate((jnp.array([reg_gw_cost]), out[0]))
-  errors_sinkhorn = jnp.concatenate((jnp.array([error_sinkhorn]),
-                                     out[1]))
+  errors_sinkhorn = jnp.concatenate((jnp.array([errors_sinkhorn]), out[1]))
   converged_sinkhorn = jnp.concatenate((jnp.array([converged_sinkhorn]),
                                         out[2]))
-  return f, g, geom_gw, reg_gw_cost_arr, errors_sinkhorn, converged_sinkhorn
-
-
-def _init_constant_cost(
-    geom_x: geometry.Geometry,
-    geom_y: geometry.Geometry,
-    a: jnp.ndarray,
-    b: jnp.ndarray,
-    loss: GWLoss) -> float:
-  """Constant term in the GW cost, Equation 6, Proposition 1 of
-  http://proceedings.mlr.press/v48/peyre16.pdf.
-
-  Args:
-    geom_x: a Geometry object for the first view.
-    geom_y: a second Geometry object for the second view.
-    a: jnp.ndarray<float>[num_a,] or jnp.ndarray<float>[batch,num_a] weights.
-    b: jnp.ndarray<float>[num_b,] or jnp.ndarray<float>[batch,num_b] weights.
-    loss: a GWLossFn object.
-
-  Returns:
-    float, constant value in cost over GW iterations
-  """
-  a = a.reshape(-1, 1)
-  b = b.reshape(-1, 1)
-  constant1 = jnp.dot(geom_x.apply_cost(a, 1, loss.fn_x),
-                      jnp.ones((1, b.size)))
-  constant2 = jnp.dot(geom_y.apply_cost(b, 1, loss.fn_y),
-                      jnp.ones((1, a.size))).T
-  return constant1 + constant2
+  return (f, g, geom_gw, reg_gw_cost_arr, errors_sinkhorn, converged_sinkhorn)
 
 
 def _init_geometry_gw(
@@ -290,30 +255,35 @@ def _init_geometry_gw(
     geom_y: geometry.Geometry,
     a: jnp.ndarray,
     b: jnp.ndarray,
-    epsilon: float,
-    gw_constant: float,
+    epsilon: Union[epsilon_scheduler.Epsilon, float],
     loss: GWLoss,
     **kwargs) -> geometry.Geometry:
-  """Initialises the geometry object for GW and updates the cost matrices of
-  the geometries for the two views according to the chosen loss.
+  """Initialises the cost matrix for the geometry object for GW. The equation
+  follows Equation 6, Proposition 1 of 
+  http://proceedings.mlr.press/v48/peyre16.pdf.
 
   Args:
     geom_x: a Geometry object for the first view.
     geom_y: a second Geometry object for the second view.
-    a: jnp.ndarray<float>[num_a,] or jnp.ndarray<float>[batch,num_a] weights.
-    b: jnp.ndarray<float>[num_b,] or jnp.ndarray<float>[batch,num_b] weights.
+    a: jnp.ndarray<float>[num_a,], weights.
+    b: jnp.ndarray<float>[num_b,], weights.
     epsilon: a regularization parameter or a epsilon_scheduler.Epsilon object.
-    gw_constant: float, constant term in the GW cost
     loss: a GWLossFn object.
-    **kwargs: additional kwargs to epsilon
+    **kwargs: additional kwargs to epsilon.
 
   Returns:
     A Geometry object for Gromov-Wasserstein.
   """
+  # Initialization of the transport matrix in the balanced case, following
+  # http://proceedings.mlr.press/v48/peyre16.pdf
   ab = a[:, None] * b[None, :]
+  marginal_x = ab.sum(1)
+  marginal_y = ab.sum(0)
+  marginal_dep_term = _marginal_dependent_cost(
+      marginal_x, marginal_y, geom_x, geom_y, loss)
 
   tmp = geom_x.apply_cost(ab, axis=1, fn=loss.left_x)
-  cost_matrix = gw_constant - geom_y.apply_cost(
+  cost_matrix = marginal_dep_term - geom_y.apply_cost(
       tmp.T, axis=1, fn=loss.right_y).T
   return geometry.Geometry(cost_matrix=cost_matrix,
                            epsilon=epsilon, **kwargs)
@@ -325,20 +295,28 @@ def _update_geometry_gw(
     geom_y: geometry.Geometry,
     f: jnp.ndarray,
     g: jnp.ndarray,
-    gw_constant: float,
     loss: GWLoss,
     **kwargs) -> geometry.Geometry:
-  """Updates the geometry object for GW.
+  """Updates the geometry object for GW by updating the cost matrix. The cost
+  matrix equation follows Equation 6, Proposition 1 of
+  http://proceedings.mlr.press/v48/peyre16.pdf.
+
+  Let :math:`p` [num_a,] be the marginal of the transport matrix for samples
+  from geom_x and :math:`q` [num_b,] be the marginal of the transport matrix for
+  samples from geom_y. Let :math:`T` [num_a, num_b] be the transport matrix.
+  The cost matrix equation can be written as:
+
+  cost_matrix = marginal_dep_term
+              + left_x(cost_x) :math:`T` right_y(cost_y):math:`^T`
 
   Args:
     geom: a Geometry object carrying the cost matrix of Gromov Wasserstein.
     geom_x: a Geometry object for the first view.
     geom_y: a second Geometry object for the second view.
-    f: potentials
-    g: potentials
-    gw_constant: float, constant term in the GW cost
+    f: jnp.ndarray<float>[num_a,], potentials.
+    g: jnp.ndarray<float>[num_b,], potentials.
     loss: a GWLossFn object.
-    **kwargs: additional kwargs for epsilon
+    **kwargs: additional kwargs for epsilon.
 
   Returns:
     A Geometry object for Gromov-Wasserstein.
@@ -364,8 +342,45 @@ def _update_geometry_gw(
     tmp = geom.apply_transport_from_potentials(
         f, g, loss.left_x(geom_x.cost_matrix), axis=0)
 
-  # Computes cost_matrix = gw_constant - tmp * cost_matrix_y.T
-  cost_matrix = gw_constant - apply_cost_fn(geom_y)(
+  # Computes cost_matrix
+  marginal_x = geom.marginal_from_potentials(f, g, axis=1)
+  marginal_y = geom.marginal_from_potentials(f, g, axis=0)
+  marginal_dep_term = _marginal_dependent_cost(
+      marginal_x, marginal_y, geom_x, geom_y, loss)
+  cost_matrix = marginal_dep_term - apply_cost_fn(geom_y)(
       tmp.T, axis=1, fn=loss.right_y).T
   return geometry.Geometry(cost_matrix=cost_matrix,
                            epsilon=geom._epsilon, **kwargs)
+
+
+def _marginal_dependent_cost(marginal_x, marginal_y, geom_x, geom_y, loss):
+  r"""Calculates the term in the cost that depends on the marginals of the
+  transport matrix, according to the first term in Equation 6, Proposition 1 of
+  http://proceedings.mlr.press/v48/peyre16.pdf.
+
+  Let :math:`p` [num_a,] be the marginal of the transport matrix for samples
+  from geom_x and :math:`q` [num_b,] be the marginal of the transport matrix for
+  samples from geom_y. The term in the cost that depends on these marginals can
+  be written as:
+  marginal_dep_term = fn_x(cost_x) :math:`p \mathbb{1}_{num_b}^T`
+                    + (fn_y(cost_y) :math:`q \mathbb{1}_{num_a}^T)^T`
+
+  Args:
+    marginal_x: jnp.ndarray<float>[num_a,], marginal of the transport matrix for
+     samples from geom_x
+    marginal_y: jnp.ndarray<float>[num_b,], marginal of the transport matrix for
+     samples from geom_y
+    geom_x: a Geometry object for the first view.
+    geom_y: a second Geometry object for the second view.
+    loss: a GWLossFn object.
+
+  Returns:
+    jnp.ndarray, [num_a, num_b]
+  """
+  x_term = jnp.dot(
+      geom_x.apply_cost(marginal_x, 1, loss.fn_x).reshape(-1, 1),
+      jnp.ones((1, marginal_y.size)))
+  y_term = jnp.dot(
+      geom_y.apply_cost(marginal_y, 1, loss.fn_y).reshape(-1, 1),
+      jnp.ones((1, marginal_x.size))).T
+  return x_term + y_term
--- tests/core/gromov_wasserstein_test.py
+++ tests/core/gromov_wasserstein_test.py
@@ -33,39 +33,46 @@ class GromovWassersteinGradTest(jax.test_util.JaxTestCase):
     self.rng = jax.random.PRNGKey(0)
 
   @parameterized.parameters([True], [False])
-  def test_autograd_gromov_wasserstein(self, jit):
+  def test_gradient_marginals_gromov_wasserstein(self, jit):
     """Test gradient w.r.t. probability weights."""
     d_x = 2
     d_y = 3
     n, m = 5, 6
-    eps = 1e-3  # perturbation magnitude
     keys = jax.random.split(self.rng, 5)
     x = jax.random.uniform(keys[0], (n, d_x))
     y = jax.random.uniform(keys[1], (m, d_y))
-    a = jax.random.uniform(keys[2], (n,))
-    b = jax.random.uniform(keys[3], (m,))
+    a = jax.random.uniform(keys[2], (n,)) + 0.1
+    b = jax.random.uniform(keys[3], (m,)) + 0.1
     a = a / jnp.sum(a)
     b = b / jnp.sum(b)
-    geom_x = pointcloud.PointCloud(x, x, epsilon=0.1)
-    geom_y = pointcloud.PointCloud(y, y, epsilon=0.1)
+    geom_x = pointcloud.PointCloud(x, x)
+    geom_y = pointcloud.PointCloud(y, y)
 
-    def reg_gw(a, b):
-      sinkhorn_kwargs = {'implicit_differentiation': False,
-                         'max_iterations': 101}
-      return gromov_wasserstein.gromov_wasserstein(
-          geom_x, geom_y, a=a, b=b, loss=gromov_wasserstein.GWSqEuclLoss(),
-          max_iterations_gromov=5, jit_gromov=jit,
-          sinkhorn_kwargs=sinkhorn_kwargs).reg_gw_cost
+    def reg_gw(a, b, implicit):
+      sinkhorn_kwargs = {'implicit_differentiation': implicit,
+                         'max_iterations': 1001}
+      out = gromov_wasserstein.gromov_wasserstein(
+          geom_x, geom_y, a=a, b=b, epsilon=1.0,
+          loss=gromov_wasserstein.GWSqEuclLoss(),
+          max_iterations=10, jit=jit,
+          sinkhorn_kwargs=sinkhorn_kwargs)
+      return out.reg_gw_cost, (out.f, out.g)
 
-    reg_gw_and_grad = jax.value_and_grad(reg_gw)
-    _, grad_reg_gw = reg_gw_and_grad(a, b)
-    delta = jax.random.uniform(keys[4], (n,))
-    reg_gw_delta_plus = reg_gw(a + eps * delta, b)
-    reg_gw_delta_minus = reg_gw(a - eps * delta, b)
-    delta_dot_grad = jnp.nansum(delta * grad_reg_gw)
-    self.assertIsNot(jnp.any(jnp.isnan(delta_dot_grad)), True)
-    self.assertAllClose(delta_dot_grad,
-                        (reg_gw_delta_plus - reg_gw_delta_minus) / (2 * eps),
+    grad_matrices = [None, None]
+    for i, implicit in enumerate([True, False]):
+      reg_gw_and_grad = jax.value_and_grad(reg_gw, has_aux=True,
+                                           argnums=(0, 1,))
+      (_, aux), grad_reg_gw = reg_gw_and_grad(a, b, implicit)
+      grad_matrices[i] = grad_reg_gw
+      grad_manual_a = aux[0] - jnp.log(a)
+      grad_manual_b = aux[1] - jnp.log(b)
+      self.assertIsNot(jnp.any(jnp.isnan(grad_reg_gw[0])), True)
+      self.assertIsNot(jnp.any(jnp.isnan(grad_reg_gw[1])), True)
+      self.assertAllClose(grad_manual_a, grad_reg_gw[0], rtol=1e-2, atol=1e-2)
+      self.assertAllClose(grad_manual_b, grad_reg_gw[1], rtol=1e-2, atol=1e-2)
+    self.assertAllClose(grad_matrices[0][0], grad_matrices[1][0],
+                        rtol=1e-02, atol=1e-02)
+    self.assertAllClose(grad_matrices[0][1], grad_matrices[1][1],
                         rtol=1e-02, atol=1e-02)
 
   @parameterized.parameters([True], [False])
@@ -74,86 +81,66 @@ class GromovWassersteinGradTest(jax.test_util.JaxTestCase):
     d_x = 2
     d_y = 3
     n, m = 5, 6
-    eps = 1e-3  # perturbation magnitude
     keys = jax.random.split(self.rng, 5)
     x = jax.random.uniform(keys[0], (n, d_x))
     y = jax.random.uniform(keys[1], (m, d_y))
-    a = jax.random.uniform(keys[2], (n,))
-    b = jax.random.uniform(keys[3], (m,))
+    a = jax.random.uniform(keys[2], (n,)) + 0.1
+    b = jax.random.uniform(keys[3], (m,)) + 0.1
     a = a / jnp.sum(a)
     b = b / jnp.sum(b)
 
-    def reg_gw(x, y, a, b):
-      geom_x = pointcloud.PointCloud(x, x, epsilon=0.1)
-      geom_y = pointcloud.PointCloud(y, y, epsilon=0.1)
-      sinkhorn_kwargs = {'implicit_differentiation': False,
-                         'max_iterations': 101}
+    def reg_gw(x, y, a, b, implicit):
+      geom_x = pointcloud.PointCloud(x, x)
+      geom_y = pointcloud.PointCloud(y, y)
+      sinkhorn_kwargs = {'implicit_differentiation': implicit,
+                         'max_iterations': 1001, 'lse_mode': lse_mode}
       return gromov_wasserstein.gromov_wasserstein(
-          geom_x, geom_y, a=a, b=b, max_iterations_gromov=5,
+          geom_x, geom_y, a=a, b=b, epsilon=1.0, max_iterations=10,
           sinkhorn_kwargs=sinkhorn_kwargs).reg_gw_cost
 
-    reg_gw_and_grad = jax.value_and_grad(reg_gw, argnums=(0, 1,))
-    _, grad_reg_gw = reg_gw_and_grad(x, y, a, b)
-    delta_x = jax.random.uniform(keys[4], (n, d_x))
-    delta_y = jax.random.uniform(keys[4], (m, d_y))
-    reg_gw_delta_plus_x = reg_gw(x + eps * delta_x, y, a, b)
-    reg_gw_delta_minus_x = reg_gw(x - eps * delta_x, y, a, b)
-    reg_gw_delta_plus_y = reg_gw(x, y + eps * delta_y, a, b)
-    reg_gw_delta_minus_y = reg_gw(x, y - eps * delta_y, a, b)
-    delta_dot_grad_x = jnp.sum(delta_x * grad_reg_gw[0])
-    delta_dot_grad_y = jnp.sum(delta_y * grad_reg_gw[1])
-    self.assertIsNot(jnp.any(jnp.isnan(delta_dot_grad_x)), True)
-    self.assertIsNot(jnp.any(jnp.isnan(delta_dot_grad_y)), True)
-    self.assertAllClose(delta_dot_grad_x,
-                        (reg_gw_delta_plus_x
-                         - reg_gw_delta_minus_x) / (2 * eps),
+    grad_matrices = [None, None]
+    for i, implicit in enumerate([True, False]):
+      reg_gw_and_grad = jax.value_and_grad(reg_gw, argnums=(0, 1,))
+      _, grad_reg_gw = reg_gw_and_grad(x, y, a, b, implicit)
+      grad_matrices[i] = grad_reg_gw
+      self.assertIsNot(jnp.any(jnp.isnan(grad_reg_gw[0])), True)
+      self.assertIsNot(jnp.any(jnp.isnan(grad_reg_gw[1])), True)
+    self.assertAllClose(grad_matrices[0][0], grad_matrices[1][0],
                         rtol=1e-02, atol=1e-02)
-    self.assertAllClose(delta_dot_grad_y,
-                        (reg_gw_delta_plus_y
-                         - reg_gw_delta_minus_y) / (2 * eps),
+    self.assertAllClose(grad_matrices[0][1], grad_matrices[1][1],
                         rtol=1e-02, atol=1e-02)
 
   @parameterized.parameters([True], [False])
   def test_gradient_gromov_wasserstein_geometry(self, lse_mode):
     """Test gradient w.r.t. cost matrices."""
     n, m = 5, 6
-    eps = 1e-3  # perturbation magnitude
     keys = jax.random.split(self.rng, 5)
     cx = jax.random.uniform(keys[0], (n, n))
     cy = jax.random.uniform(keys[1], (m, m))
-    a = jax.random.uniform(keys[2], (n,))
-    b = jax.random.uniform(keys[3], (m,))
+    a = jax.random.uniform(keys[2], (n,)) + 0.1
+    b = jax.random.uniform(keys[3], (m,)) + 0.1
     a = a / jnp.sum(a)
     b = b / jnp.sum(b)
 
-    def reg_gw(cx, cy, a, b):
-      geom_x = geometry.Geometry(cost_matrix=cx, epsilon=0.1)
-      geom_y = geometry.Geometry(cost_matrix=cy, epsilon=0.1)
-      sinkhorn_kwargs = {'implicit_differentiation': False,
-                         'max_iterations': 101}
+    def reg_gw(cx, cy, a, b, implicit):
+      geom_x = geometry.Geometry(cost_matrix=cx)
+      geom_y = geometry.Geometry(cost_matrix=cy)
+      sinkhorn_kwargs = {'implicit_differentiation': implicit,
+                         'max_iterations': 1001, 'lse_mode': lse_mode}
       return gromov_wasserstein.gromov_wasserstein(
-          geom_x, geom_y, a=a, b=b, max_iterations_gromov=5,
+          geom_x, geom_y, a=a, b=b, epsilon=1.0, max_iterations=10,
           sinkhorn_kwargs=sinkhorn_kwargs).reg_gw_cost
 
-    reg_gw_and_grad = jax.value_and_grad(reg_gw, argnums=(0, 1,))
-    _, grad_reg_gw = reg_gw_and_grad(cx, cy, a, b)
-    delta_x = jax.random.uniform(keys[4], (n, n))
-    delta_y = jax.random.uniform(keys[4], (m, m))
-    reg_gw_delta_plus_x = reg_gw(cx + eps * delta_x, cy, a, b)
-    reg_gw_delta_minus_x = reg_gw(cx - eps * delta_x, cy, a, b)
-    reg_gw_delta_plus_y = reg_gw(cx, cy + eps * delta_y, a, b)
-    reg_gw_delta_minus_y = reg_gw(cx, cy - eps * delta_y, a, b)
-    delta_dot_grad_x = jnp.sum(delta_x * grad_reg_gw[0])
-    delta_dot_grad_y = jnp.sum(delta_y * grad_reg_gw[1])
-    self.assertIsNot(jnp.any(jnp.isnan(delta_dot_grad_x)), True)
-    self.assertIsNot(jnp.any(jnp.isnan(delta_dot_grad_y)), True)
-    self.assertAllClose(delta_dot_grad_x,
-                        (reg_gw_delta_plus_x
-                         - reg_gw_delta_minus_x) / (2 * eps),
+    grad_matrices = [None, None]
+    for i, implicit in enumerate([True, False]):
+      reg_gw_and_grad = jax.value_and_grad(reg_gw, argnums=(0, 1,))
+      _, grad_reg_gw = reg_gw_and_grad(cx, cy, a, b, implicit)
+      grad_matrices[i] = grad_reg_gw
+      self.assertIsNot(jnp.any(jnp.isnan(grad_reg_gw[0])), True)
+      self.assertIsNot(jnp.any(jnp.isnan(grad_reg_gw[1])), True)
+    self.assertAllClose(grad_matrices[0][0], grad_matrices[1][0],
                         rtol=1e-02, atol=1e-02)
-    self.assertAllClose(delta_dot_grad_y,
-                        (reg_gw_delta_plus_y
-                         - reg_gw_delta_minus_y) / (2 * eps),
+    self.assertAllClose(grad_matrices[0][1], grad_matrices[1][1],
                         rtol=1e-02, atol=1e-02)
 
 if __name__ == '__main__':
