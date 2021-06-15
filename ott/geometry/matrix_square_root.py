6e2fa559477969bfb00b735c40bca3236239f564
Google Brain <no-reply@google.com>
2021-06-15 23:14:10 +0000

    Add gradient to matrix_square_root.sqrtm, support arbitrary batch sizes in sqrtm, add sqrtm_only and inv_sqrtm_only functions with faster gradients (since only one Sylvester equation needs to be solved)
    PiperOrigin-RevId: 379598341

--- ott/geometry/matrix_square_root.py
+++ ott/geometry/matrix_square_root.py
@@ -17,6 +17,7 @@
 """A Jax backprop friendly version of Matrix square root."""
 
 import functools
+from typing import Tuple
 
 import jax
 import jax.numpy as jnp
@@ -24,12 +25,15 @@ import numpy as np
 from ott.core import fixed_point_loop
 
 
-def sqrtm(x: jnp.ndarray,
-          threshold: float = 1e-3,
-          min_iterations: int = 0,
-          inner_iterations: int = 10,
-          max_iterations: int = 1000,
-          regularization: float = 1e-3):
+@functools.partial(jax.custom_vjp, nondiff_argnums=(1, 2, 3, 4, 5))
+def sqrtm(
+    x: jnp.ndarray,
+    threshold: float = 1e-3,
+    min_iterations: int = 0,
+    inner_iterations: int = 10,
+    max_iterations: int = 1000,
+    regularization: float = 1e-3
+):
   """Implements Higham algorithm to compute matrix square root of p.d. matrix.
 
   See reference below, eq. 2.6.b
@@ -49,10 +53,10 @@ def sqrtm(x: jnp.ndarray,
   dimension = x.shape[-1]
   norm_x = jnp.linalg.norm(x, axis=(-2, -1)) * (1 + regularization)
 
-  if jnp.ndim(x) == 3:
-    norm_x = norm_x[:, jnp.newaxis, jnp.newaxis]
+  if jnp.ndim(x) > 2:
+    norm_x = norm_x[..., jnp.newaxis, jnp.newaxis]
 
-  def cond_fn(iteration, const, state):  # pylint: disable=unused-argument
+  def cond_fn(iteration, const, state):
     """Stopping criterion. Checking decrease of objective is needed here."""
     _, threshold = const
     errors, _, _ = state
@@ -97,8 +101,8 @@ def sqrtm(x: jnp.ndarray,
 
   y = x / norm_x
   z = jnp.eye(dimension)
-  if jnp.ndim(x) == 3:
-    z = jnp.tile(z, (x.shape[0], 1, 1))
+  if jnp.ndim(x) > 2:
+    z = jnp.tile(z, list(x.shape[:-2]) + [1, 1])
   errors = -jnp.ones((np.ceil(max_iterations / inner_iterations).astype(int),))
   state = (errors, y, z)
   const = (x, threshold)
@@ -109,3 +113,147 @@ def sqrtm(x: jnp.ndarray,
   inv_sqrt_x = z / jnp.sqrt(norm_x)
 
   return sqrt_x, inv_sqrt_x, errors
+
+
+def _solve_sylvester(
+    a: jnp.ndarray,
+    b: jnp.ndarray
+) -> Tuple[jnp.ndarray]:
+  """Solve a Sylvester equation of the form XA + AX = B for X.
+
+  When we differentiate X^{1/2}X^{1/2} = X, we obtain the Sylvester equation
+  d(X^{1/2}) X^{1/2} + X^{1/2} d(X^{1/2}) = dX. We solve below by reformulating
+  as a single linear equation using the vec operator (see
+  https://nhigham.com/2020/09/01/what-is-the-sylvester-equation/ )
+  Note that this is quite computationally expensive (O(n^6)!).
+
+  Similarly, when we differentiate X^{-1/2}X^{-1/2} = X^{-1}, we obtain the
+  equation d(X^{-1/2}) X^{-1/2} + X^{-1/2} d(X^{-1/2}) = d X^{-1} =
+  -x^{-T} dx x^{-T}
+
+  Args:
+    a: value A on the left hand side of the Sylvester equation
+    b: value B on the right hand side of the Sylvester equation
+
+  Returns:
+    The solution X.
+  """
+  # TODO(geoffd): reimplement using the Bartels-Stewart algorithm to solve
+  #   in O(n^3) time when there is a JAX solver.
+  d = a.shape[-1]
+  eye_d = jnp.eye(d)
+  a_t = jnp.swapaxes(a, axis1=-2, axis2=-1)
+  l = jnp.kron(a_t, eye_d) + jnp.kron(eye_d, a)
+  b = b.reshape(list(b.shape[:-2]) + [d * d, 1])
+  return jnp.linalg.solve(l, b).reshape(a.shape)
+
+
+def sqrtm_fwd(
+    x: jnp.ndarray,
+    threshold: float,
+    min_iterations: int,
+    inner_iterations: int,
+    max_iterations: int,
+    regularization: float,
+) -> Tuple[Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],
+           Tuple[jnp.ndarray, jnp.ndarray]]:
+  """Forward pass of custom VJP."""
+  sqrt_x, inv_sqrt_x, errors = sqrtm(
+      x=x,
+      threshold=threshold,
+      min_iterations=min_iterations,
+      inner_iterations=inner_iterations,
+      max_iterations=max_iterations,
+      regularization=regularization,
+      )
+  return ((sqrt_x, inv_sqrt_x, errors), (sqrt_x, inv_sqrt_x))
+
+
+def sqrtm_bwd(
+    threshold: float,
+    min_iterations: int,
+    inner_iterations: int,
+    max_iterations: int,
+    regularization: float,
+    residual: Tuple[jnp.ndarray, jnp.ndarray],
+    cotangent: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],
+) -> Tuple[jnp.ndarray,]:
+  """Compute the derivative by solving a Sylvester equation."""
+  del threshold, min_iterations, inner_iterations, max_iterations, regularization
+  sqrt_x, inv_sqrt_x = residual
+  # ignores cotangent associated with errors
+  cot_sqrt, cot_inv_sqrt, _ = cotangent
+
+  # get d(x^{1/2})
+  vjp_cot_sqrt = _solve_sylvester(a=sqrt_x, b=cot_sqrt)
+
+  # If we differentiate x^{-1/2}x^{-1/2} = x^{-1}, we obtain a similar
+  # Sylvester equation to the one used to obtain d(x^{1/2}) above:
+  # d(x^{-1/2})x^{-1/2} + x^{-1/2}d(x^{-1/2}) = d(x^{-1})
+  # We then use the fact that d(x^{-1}) = -x^{-T} dx x^{-T}
+  # (See The Matrix Cookbook section on derivatives of an inverse
+  # https://www.math.uwaterloo.ca/~hwolkowi/matrixcookbook.pdf )
+  inv_x_t = jnp.swapaxes(
+      jnp.matmul(inv_sqrt_x, inv_sqrt_x), axis1=-2, axis2=-1)
+  vjp_cot_inv_sqrt = _solve_sylvester(
+      a=inv_sqrt_x, b=-jnp.matmul(inv_x_t, jnp.matmul(cot_inv_sqrt, inv_x_t)))
+  return (vjp_cot_sqrt + vjp_cot_inv_sqrt,)
+
+
+sqrtm.defvjp(sqrtm_fwd, sqrtm_bwd)
+
+
+# Specialized versions of sqrtm that compute only the square root or inverse.
+# These functions have lower complexity gradients than sqrtm.
+
+
+@jax.custom_vjp
+def sqrtm_only(
+    x: jnp.ndarray
+) -> jnp.ndarray:
+  return sqrtm(x)[0]
+
+
+def sqrtm_only_fwd(
+    x: jnp.ndarray
+) -> Tuple[jnp.ndarray, jnp.ndarray]:
+  sqrt_x = sqrtm(x)[0]
+  return sqrt_x, sqrt_x
+
+
+def sqrtm_only_bwd(
+    sqrt_x: jnp.ndarray,
+    cotangent: jnp.ndarray
+) -> Tuple[jnp.ndarray]:
+  vjp = _solve_sylvester(a=sqrt_x, b=cotangent)
+  return (vjp,)
+
+
+sqrtm_only.defvjp(sqrtm_only_fwd, sqrtm_only_bwd)
+
+
+@jax.custom_vjp
+def inv_sqrtm_only(
+    x: jnp.ndarray,
+) -> jnp.ndarray:
+  return sqrtm(x)[1]
+
+
+def inv_sqrtm_only_fwd(
+    x: jnp.ndarray
+) -> Tuple[jnp.ndarray, jnp.ndarray]:
+  inv_sqrt_x = sqrtm(x)[1]
+  return inv_sqrt_x, inv_sqrt_x
+
+
+def inv_sqrtm_only_bwd(
+    residual: Tuple[jnp.ndarray, jnp.ndarray],
+    cotangent: jnp.ndarray) -> jnp.ndarray:
+  inv_sqrt_x = residual
+  inv_x_t = jnp.swapaxes(jnp.matmul(inv_sqrt_x, inv_sqrt_x), axis1=-2, axis2=-1)
+  vjp = _solve_sylvester(
+      a=inv_sqrt_x, b=-jnp.matmul(inv_x_t, jnp.matmul(cotangent, inv_x_t)))
+  return (vjp,)
+
+
+inv_sqrtm_only.defvjp(inv_sqrtm_only_fwd, inv_sqrtm_only_bwd)
--- tests/geometry/matrix_square_root_test.py
+++ tests/geometry/matrix_square_root_test.py
@@ -15,11 +15,63 @@
 
 # Lint as: python3
 """Tests for matrix square roots."""
+from typing import Callable
+
 from absl.testing import absltest
+from absl.testing import parameterized
 import jax
+from jax.config import config
 import jax.numpy as jnp
 import jax.test_util
+
 from ott.geometry import matrix_square_root
+
+
+def _get_random_spd_matrix(dim: int, key: jnp.ndarray):
+  # Get a random symmetric, positive definite matrix of a specified size.
+
+  key, subkey0, subkey1 = jax.random.split(key, num=3)
+  # Step 1: generate a random orthogonal matrix
+  m = jax.random.normal(
+      key=subkey0,
+      shape=[dim, dim])
+  q, _ = jnp.linalg.qr(m)
+
+  # Step 2: generate random eigenvalues in [1/2. , 2.] to ensure the condition
+  # number is reasonable.
+  eigs = 2. ** (2. * jax.random.uniform(key=subkey1, shape=(dim,)) - 1.)
+
+  return jnp.matmul(eigs[None, :] * q, jnp.transpose(q))
+
+
+def _get_test_fn(
+    fn: Callable[[jnp.ndarray], jnp.ndarray],
+    dim: int,
+    key: jnp.ndarray) -> Callable[[jnp.ndarray], jnp.ndarray]:
+  # We want to test gradients of a function fn that maps positive definite
+  # matrices to positive definite matrices by comparing them to finite
+  # difference approximations. We'll do so via a test function that
+  # (1) takes an arbitrary real as an input,
+  # (2) maps the real to a positive definite matrix,
+  # (3) applies fn, then
+  # (4) maps the matrix-valued output of fn to a scalar.
+  key, subkey0, subkey1, subkey2, subkey3 = jax.random.split(key, num=5)
+  m0 = _get_random_spd_matrix(dim=dim, key=subkey0)
+  m1 = _get_random_spd_matrix(dim=dim, key=subkey1)
+  dx = _get_random_spd_matrix(dim=dim, key=subkey2)
+  unit = jax.random.normal(key=subkey3, shape=(dim, dim))
+  unit /= jnp.sqrt(jnp.sum(unit ** 2.))
+  def _test_fn(x: float) -> float:
+    # m is the product of 2 symmetric, positive definite matrices
+    # so it will be positive definite but not necessarily symmetric
+    m = jnp.matmul(m0, m1 + x * dx)
+    return jnp.sum(fn(m) * unit)
+  return _test_fn
+
+
+def _sqrt_plus_inv_sqrt(x: jnp.ndarray) -> jnp.ndarray:
+  sqrtm = matrix_square_root.sqrtm(x)
+  return sqrtm[0] + sqrtm[1]
 
 
 class MatrixSquareRootTest(jax.test_util.JaxTestCase):
@@ -30,7 +82,7 @@ class MatrixSquareRootTest(jax.test_util.JaxTestCase):
     self.dim = 23
     self.batch = 3
 
-  def test_matrix_sqrtm(self):
+  def test_sqrtm(self):
     """Sample a random p.s.d. (Wishart) matrix, check its sqrt matches."""
 
     matrices = jax.random.normal(self.rng, (self.batch, self.dim, 2 * self.dim))
@@ -39,8 +91,8 @@ class MatrixSquareRootTest(jax.test_util.JaxTestCase):
       x = jnp.matmul(x, jnp.swapaxes(x, -1, -2))
       threshold = 1e-4
 
-      sqrt_x, inv_sqrt_x, errors = matrix_square_root.sqrtm(x, self.dim,
-                                                            threshold)
+      sqrt_x, inv_sqrt_x, errors = matrix_square_root.sqrtm(
+          x, min_iterations=self.dim, threshold=threshold)
       err = errors[errors > -1][-1]
       self.assertGreater(threshold, err)
       self.assertAllClose(x, jnp.matmul(sqrt_x, sqrt_x), rtol=1e-3, atol=1e-3)
@@ -52,6 +104,88 @@ class MatrixSquareRootTest(jax.test_util.JaxTestCase):
           jnp.matmul(x, jnp.matmul(inv_sqrt_x, inv_sqrt_x)) - ids,
           atol=1e-2)
 
+  def test_sqrtm_batch(self):
+    """Check sqrtm on larger of matrices."""
+    batch_dim0 = 2
+    batch_dim1 = 2
+    threshold = 1e-4
+
+    m = jax.random.normal(
+        self.rng, (batch_dim0, batch_dim1, self.dim, 2 * self.dim))
+    x = jnp.matmul(m, jnp.swapaxes(m, axis1=-2, axis2=-1))
+    sqrt_x, inv_sqrt_x, errors = matrix_square_root.sqrtm(
+        x, threshold=threshold, min_iterations=self.dim,)
+
+    err = errors[errors > -1][-1]
+    self.assertGreater(threshold, err)
+
+    eye = jnp.eye(self.dim)
+    for i in range(batch_dim0):
+      for j in range(batch_dim1):
+        self.assertAllClose(
+            x[i, j], jnp.matmul(sqrt_x[i, j], sqrt_x[i, j]),
+            rtol=1e-3, atol=1e-3)
+        self.assertAllClose(
+            eye,
+            jnp.matmul(x[i, j], jnp.matmul(inv_sqrt_x[i, j], inv_sqrt_x[i, j])),
+            atol=1e-2)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='test_sqrtm_sqrtm',
+          fn=lambda x: matrix_square_root.sqrtm(x)[0],
+          n_tests=10,
+          dim=3,
+          epsilon=1.e-6,
+          atol=1.e-6,
+          rtol=1.e-6,
+      ),
+      dict(
+          testcase_name='test_sqrtm_inv_sqrtm',
+          fn=lambda x: matrix_square_root.sqrtm(x)[1],
+          n_tests=10,
+          dim=3,
+          epsilon=1.e-6,
+          atol=1.e-8,
+          rtol=1.e-8,
+      ),
+      dict(
+          testcase_name='test_sqrtm_sqrtm_plus_inv_sqrtm',
+          fn=_sqrt_plus_inv_sqrt,
+          n_tests=10,
+          dim=3,
+          epsilon=1.e-6,
+          atol=1.e-8,
+          rtol=1.e-8,
+      ),
+      dict(
+          testcase_name='test_sqrtm_only',
+          fn=matrix_square_root.sqrtm_only,
+          n_tests=10,
+          dim=3,
+          epsilon=1.e-6,
+          atol=1.e-8,
+          rtol=1.e-8,
+      ),
+      dict(
+          testcase_name='test_inv_sqrtm_only',
+          fn=matrix_square_root.inv_sqrtm_only,
+          n_tests=10,
+          dim=2,
+          epsilon=1.e-6,
+          atol=1.e-8,
+          rtol=1.e-8,
+      ),
+      )
+  def test_grad(self, fn, n_tests, dim, epsilon, atol, rtol):
+    config.update('jax_enable_x64', True)
+    key = self.rng
+    for _ in range(n_tests):
+      key, subkey = jax.random.split(key)
+      test_fn = _get_test_fn(fn, dim=dim, key=subkey)
+      expected = (test_fn(epsilon) - test_fn(-epsilon)) / (2. * epsilon)
+      actual = jax.grad(test_fn)(0.)
+      self.assertAllClose(expected, actual, atol=atol, rtol=rtol)
 
 if __name__ == '__main__':
   absltest.main()
