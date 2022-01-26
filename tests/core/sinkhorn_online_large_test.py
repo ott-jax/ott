92ed4d435d3f0e75a06e0e46fab6e3725324aa3f
marcocuturi <marcocuturicameto@gmail.com>
2022-01-26 08:51:19 +0100

    Update sinkhorn_online_large_test.py
    Change typo.

--- tests/core/sinkhorn_online_large_test.py
+++ tests/core/sinkhorn_online_large_test.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Lint as: python3
-"""Tests for the Policy."""
+"""Tests Online option for PointCloud geometry."""
 
 from absl.testing import absltest
 from absl.testing import parameterized
