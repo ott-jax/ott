3e59655be4b7ed74ca194bf5e92a3121d54287f0
marcocuturi <marcocuturicameto@gmail.com>
2022-01-30 21:58:40 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ copyright = '2021, the OTT authors'
 author = 'Marco Cuturi, Laetitia Papaxanthos, Olivier Teboul'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
