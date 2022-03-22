bd57d70fde2dce9aea3a18ae501f1bef3b06319f
meyerscetbon <mscetbon@ens-paris-saclay.fr>
2022-03-22 19:42:01 +0100

    test lr-sinkhorn

--- tests/core/sinkhorn_lr_test.py
+++ tests/core/sinkhorn_lr_test.py
@@ -29,7 +29,7 @@ class SinkhornLRTest(parameterized.TestCase):
   def setUp(self):
     super().setUp()
     self.rng = jax.random.PRNGKey(0)
-    self.dim = 4
+    self.dim = 2
     self.n = 19
     self.m = 17
     self.rng, *rngs = jax.random.split(self.rng, 5)
@@ -44,36 +44,41 @@ class SinkhornLRTest(parameterized.TestCase):
     self.a = a / jnp.sum(a)
     self.b = b / jnp.sum(b)
 
+  # @parameterized.product(use_lrcgeom=[True, False], init_type=['random', 'rank_2'])
   @parameterized.parameters([True], [False])
   def test_euclidean_point_cloud(self, use_lrcgeom):
     """Two point clouds, tested with various parameters."""
-    init_type_list = ['random', 'rank_2']
-    for init_type in init_type_list:
-        threshold = 1e-3
+    init_type_arr = ['rank_2','random']
+    for init_type in init_type_arr:
+        threshold = 1e-9
+        gamma = 1000
         geom = pointcloud.PointCloud(self.x, self.y)
         if use_lrcgeom:
           geom = geom.to_LRCGeometry()
         ot_prob = problems.LinearProblem(geom, self.a, self.b)
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, rank=2, init_type=init_type)
+        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=1, epsilon=0.0, init_type=init_type)
         costs = solver(ot_prob).costs
         self.assertTrue(jnp.isclose(costs[-2], costs[-1], rtol=threshold))
         cost_1 = costs[costs > -1][-1]
 
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, rank=10, epsilon=0.0, init_type=init_type)
+        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=10, epsilon=0.0, init_type=init_type)
         out = solver(ot_prob)
         costs = out.costs
         cost_2 = costs[costs > -1][-1]
         self.assertGreater(cost_1, cost_2)
+        print('cost_1: ' + str(cost_1))
+        print('cost_2: ' + str(cost_2))
 
         other_geom = pointcloud.PointCloud(self.x, self.y + 0.3)
         cost_other = out.cost_at_geom(other_geom)
         self.assertGreater(cost_other, 0.0)
 
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, rank=10, epsilon=1e-2, init_type=init_type)
+        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=10, epsilon=1e-3, init_type=init_type)
         out = solver(ot_prob)
         costs = out.costs
         cost_3 = costs[costs > -1][-1]
         self.assertGreater(cost_3, cost_2)
+        print('cost_3: ' + str(cost_3))
 
 
 if __name__ == '__main__':
