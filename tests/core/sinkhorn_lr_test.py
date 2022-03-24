4ea59eb9c71f9845ffe53840b66ec8c79266ce9c
meyerscetbon <mscetbon@ens-paris-saclay.fr>
2022-03-24 11:55:28 +0100

    indentation

--- tests/core/sinkhorn_lr_test.py
+++ tests/core/sinkhorn_lr_test.py
@@ -49,38 +49,37 @@ class SinkhornLRTest(parameterized.TestCase):
     """Two point clouds, tested with various parameters."""
     init_type_arr = ['rank_2','random']
     for init_type in init_type_arr:
-        threshold = 1e-9
-        gamma = 100
-        geom = pointcloud.PointCloud(self.x, self.y)
-        if use_lrcgeom:
-          geom = geom.to_LRCGeometry()
-        ot_prob = problems.LinearProblem(geom, self.a, self.b)
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=2, epsilon=0.0, init_type=init_type)
-        costs = solver(ot_prob).costs
-        self.assertTrue(jnp.isclose(costs[-2], costs[-1], rtol=threshold))
-        cost_1 = costs[costs > -1][-1]
+      threshold = 1e-9
+      gamma = 100
+      geom = pointcloud.PointCloud(self.x, self.y)
+      if use_lrcgeom:
+        geom = geom.to_LRCGeometry()
+      ot_prob = problems.LinearProblem(geom, self.a, self.b)
+      solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=2, epsilon=0.0, init_type=init_type)
+      costs = solver(ot_prob).costs
+      self.assertTrue(jnp.isclose(costs[-2], costs[-1], rtol=threshold))
+      cost_1 = costs[costs > -1][-1]
 
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=10, epsilon=0.0, init_type=init_type)
-        out = solver(ot_prob)
-        costs = out.costs
-        cost_2 = costs[costs > -1][-1]
-        self.assertGreater(cost_1, cost_2)
+      solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=10, epsilon=0.0, init_type=init_type)
+      out = solver(ot_prob)
+      costs = out.costs
+      cost_2 = costs[costs > -1][-1]
+      self.assertGreater(cost_1, cost_2)
 
-        other_geom = pointcloud.PointCloud(self.x, self.y + 0.3)
-        cost_other = out.cost_at_geom(other_geom)
-        self.assertGreater(cost_other, 0.0)
+      other_geom = pointcloud.PointCloud(self.x, self.y + 0.3)
+      cost_other = out.cost_at_geom(other_geom)
+      self.assertGreater(cost_other, 0.0)
 
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=14, epsilon=1e-1, init_type=init_type)
-        out = solver(ot_prob)
-        costs = out.costs
-        cost_3 = costs[costs > -1][-1]
+      solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=14, epsilon=1e-1, init_type=init_type)
+      out = solver(ot_prob)
+      costs = out.costs
+      cost_3 = costs[costs > -1][-1]
 
-        solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=14, epsilon=1e-3, init_type=init_type)
-        out = solver(ot_prob)
-        costs = out.costs
-        cost_4 = costs[costs > -1][-1]
-        self.assertGreater(cost_3, cost_4)
-
+      solver = sinkhorn_lr.LRSinkhorn(threshold=threshold, gamma=gamma, rank=14, epsilon=1e-3, init_type=init_type)
+      out = solver(ot_prob)
+      costs = out.costs
+      cost_4 = costs[costs > -1][-1]
+      self.assertGreater(cost_3, cost_4)
 
 if __name__ == '__main__':
   absltest.main()
