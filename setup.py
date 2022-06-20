9460ca4a3d928cd0f6d06becc5951615c96f7e8f
Michal Klein <michal.klein@protonmail.com>
2022-06-20 23:04:55 +0200

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 
+# simple setup.py for editable installs, see also:
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
 if __name__ == "__main__":
   setup()
