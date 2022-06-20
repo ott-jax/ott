59a1eb244106a8461795c058c3b9cd36e14e2d1a
Michal Klein <michal.klein@protonmail.com>
2022-06-20 23:34:23 +0200

    Switch to setup.cfg

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-# simple setup.py for editable installs, see also:
-# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-if __name__ == "__main__":
-  setup()
