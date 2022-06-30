b4ccc1dfe9661d8934609202262d09ac5bb0dfc7
bunnech <bunnec@inf.ethz.ch>
2022-06-30 11:45:53 +0200

    Correct formatting.

--- ott/core/icnn.py
+++ ott/core/icnn.py
@@ -13,15 +13,16 @@
 # limitations under the License.
 
 # Lint as: python3
-"""Implementation of Amos+(2017) input convex neural networks (ICNN)
-   with initialization schemes introduced in Bunne+(2022)."""
+"""Implementation of Amos+(2017) input convex neural networks (ICNN).
+   Containing initialization schemes introduced in Bunne+(2022).
+"""
 
 from typing import Any, Callable, Sequence, Tuple
 
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
-import jax.nn.initializers as initializers
+from jax.nn import initializers
 from ott.geometry.matrix_square_root import sqrtm, sqrtm_only
 from ott.core.layers import PositiveDense, PosDefPotentials
 
@@ -32,156 +33,155 @@ Array = Any
 
 
 class ICNN(nn.Module):
-    """Input convex neural network (ICNN) architeture with initialization.
+  """Input convex neural network (ICNN) architeture with initialization.
 
-    Args:
-      dim_hidden: sequence specifying size of hidden dimensions. The
-        output dimension of the last layer is 1 by default.
-      init_std: value of standard deviation of weight initialization method.
-      init_fn: choice of initialization method for weight matrices (default:
-        `jax.nn.initializers.normal`).
-      act_fn: choice of activation function used in network architecture
-        (needs to be convex, default: `nn.leaky_relu`).
-      pos_weights: choice to enforce positivity of weight or use regularizer.
-      gaussian_maps: data inputs of source and target measures for
-        initialization scheme based on Gaussian approximation of input and
-        target measure (if None, identity initialization is used).
-    """
+  Args:
+    dim_hidden: sequence specifying size of hidden dimensions. The
+      output dimension of the last layer is 1 by default.
+    init_std: value of standard deviation of weight initialization method.
+    init_fn: choice of initialization method for weight matrices (default:
+      `jax.nn.initializers.normal`).
+    act_fn: choice of activation function used in network architecture
+      (needs to be convex, default: `nn.leaky_relu`).
+    pos_weights: choice to enforce positivity of weight or use regularizer.
+    gaussian_maps: data inputs of source and target measures for
+      initialization scheme based on Gaussian approximation of input and
+      target measure (if None, identity initialization is used).
+  """
 
-    dim_hidden: Sequence[int]
-    init_std: float = 1e-1
-    init_fn: Callable = jax.nn.initializers.normal
-    act_fn: Callable = nn.relu
-    pos_weights: bool = True
-    dim_data: int = 2
-    gaussian_map: Tuple = None
+  dim_hidden: Sequence[int]
+  init_std: float = 1e-1
+  init_fn: Callable = jax.nn.initializers.normal
+  act_fn: Callable = nn.relu
+  pos_weights: bool = True
+  dim_data: int = 2
+  gaussian_map: Tuple = None
 
-    def setup(self):
-        self.num_hidden = len(self.dim_hidden)
+  def setup(self):
+    self.num_hidden = len(self.dim_hidden)
 
-        if self.pos_weights:
-            hid_dense = PositiveDense
-            # this function needs to be the inverse map of function
-            # used in PositiveDense layers
-            rescale = lambda x: jnp.log(jnp.exp(x) - 1)
-        else:
-            hid_dense = nn.Dense
-            rescale = lambda x: x
-        self.use_init = False
-        # check if Gaussian map was provided
-        if self.gaussian_map is not None:
-            factor, mean = self.compute_gaussian_map(self.gaussian_map)
-        else:
-            factor, mean = self.compute_identity_map(self.dim_data)
+    if self.pos_weights:
+      hid_dense = PositiveDense
+      # this function needs to be the inverse map of function
+      # used in PositiveDense layers
+      rescale = lambda x: jnp.log(jnp.exp(x) - 1)
+    else:
+      hid_dense = nn.Dense
+      rescale = lambda x: x
+    self.use_init = False
+    # check if Gaussian map was provided
+    if self.gaussian_map is not None:
+      factor, mean = self.compute_gaussian_map(self.gaussian_map)
+    else:
+      factor, mean = self.compute_identity_map(self.dim_data)
 
-        w_zs = list()
-        # keep track of previous size to normalize accordingly
-        normalization = 1
-        # subsequent layers propagate value of potential provided by
-        # first layer in x normalization factor is rescaled accordingly
-        for i in range(0, self.num_hidden):
-            w_zs.append(
-                hid_dense(
-                    self.dim_hidden[i],
-                    kernel_init=initializers.constant(
-                        rescale(1.0 / normalization)),
-                    use_bias=False,
-                )
-            )
-            normalization = self.dim_hidden[i]
-        # final layer computes average, still with normalized rescaling
-        w_zs.append(
-            hid_dense(
-                1,
-                kernel_init=initializers.constant(
-                    rescale(1.0 / normalization)),
-                use_bias=False,
-            )
+    w_zs = []
+    # keep track of previous size to normalize accordingly
+    normalization = 1
+    # subsequent layers propagate value of potential provided by
+    # first layer in x normalization factor is rescaled accordingly
+    for i in range(0, self.num_hidden):
+      w_zs.append(
+        hid_dense(
+          self.dim_hidden[i],
+          kernel_init=initializers.constant(
+            rescale(1.0 / normalization)),
+          use_bias=False,
         )
-        self.w_zs = w_zs
+      )
+      normalization = self.dim_hidden[i]
+    # final layer computes average, still with normalized rescaling
+    w_zs.append(
+      hid_dense(
+        1,
+        kernel_init=initializers.constant(
+          rescale(1.0 / normalization)),
+        use_bias=False,
+      )
+    )
+    self.w_zs = w_zs
 
-        w_xs = list()
-        # first square layer, initialized to identity
-        w_xs.append(
-            PosDefPotentials(
-                self.dim_data,
-                num_potentials=1,
-                kernel_init=lambda *args, **kwargs: factor,
-                bias_init=lambda *args, **kwargs: mean,
-                use_bias=True,
-            )
+    w_xs = []
+    # first square layer, initialized to identity
+    w_xs.append(
+      PosDefPotentials(
+        self.dim_data,
+        num_potentials=1,
+        kernel_init=lambda *args, **kwargs: factor,
+        bias_init=lambda *args, **kwargs: mean,
+        use_bias=True,
+      )
+    )
+
+    # subsequent layers reinjected into convex functions
+    for i in range(self.num_hidden):
+      w_xs.append(
+        nn.Dense(
+          self.dim_hidden[i],
+          kernel_init=self.init_fn(self.init_std),
+          bias_init=self.init_fn(self.init_std),
+          use_bias=True,
         )
+      )
+    # final layer, to output number
+    w_xs.append(
+      nn.Dense(
+        1,
+        kernel_init=self.init_fn(self.init_std),
+        bias_init=self.init_fn(self.init_std),
+        use_bias=True,
+      )
+    )
+    self.w_xs = w_xs
 
-        # subsequent layers reinjected into convex functions
-        for i in range(self.num_hidden):
-            w_xs.append(
-                nn.Dense(
-                    self.dim_hidden[i],
-                    kernel_init=self.init_fn(self.init_std),
-                    bias_init=self.init_fn(self.init_std),
-                    use_bias=True,
-                )
-            )
-        # final layer, to output number
-        w_xs.append(
-            nn.Dense(
-                1,
-                kernel_init=self.init_fn(self.init_std),
-                bias_init=self.init_fn(self.init_std),
-                use_bias=True,
-            )
-        )
-        self.w_xs = w_xs
+  def compute_gaussian_map(self, inputs):
+    def compute_moments(x, reg=1e-2, sqrt_inv=False):
+      shape = x.shape
+      z = x.reshape(shape[0], -1)
+      mu = jnp.expand_dims(jnp.mean(z, axis=0), 0)
+      z = z - mu
+      sigma = jax.vmap(lambda a, b: jnp.matmul(a, b))(
+        jnp.expand_dims(z, 2), jnp.expand_dims(z, 1))
+      # unbiased estimate
+      sigma = jnp.sum(sigma, axis=0) / (shape[0] - 1)
+      # regularize
+      sigma = sigma + reg * jnp.eye(shape[1])
 
-    def compute_gaussian_map(self, inputs):
-        def compute_moments(x, reg=1e-2, sqrt_inv=False):
-            shape = x.shape
-            z = x.reshape(shape[0], -1)
-            mu = jnp.expand_dims(jnp.mean(z, axis=0), 0)
-            z = z - mu
-            sigma = jax.vmap(lambda a, b: jnp.matmul(a, b))(
-                jnp.expand_dims(z, 2), jnp.expand_dims(z, 1)
-            )
-            # unbiased estimate
-            sigma = jnp.sum(sigma, axis=0) / (shape[0] - 1)
-            # regularize
-            sigma = sigma + reg * jnp.eye(shape[1])
+      if sqrt_inv:
+        sigma_sqrt, sigma_inv_sqrt, _ = sqrtm(sigma)
+        return sigma, sigma_sqrt, sigma_inv_sqrt, mu
+      else:
+        return sigma, mu
 
-            if sqrt_inv:
-                sigma_sqrt, sigma_inv_sqrt, _ = sqrtm(sigma)
-                return sigma, sigma_sqrt, sigma_inv_sqrt, mu
-            else:
-                return sigma, mu
+    source, target = inputs
+    _, covs_sqrt, covs_inv_sqrt, mus = compute_moments(
+      source, sqrt_inv=True)
+    covt, mut = compute_moments(target, sqrt_inv=False)
 
-        source, target = inputs
-        _, covs_sqrt, covs_inv_sqrt, mus = compute_moments(
-            source, sqrt_inv=True)
-        covt, mut = compute_moments(target, sqrt_inv=False)
+    mo = sqrtm_only(jnp.dot(jnp.dot(covs_sqrt, covt), covs_sqrt))
+    A = jnp.dot(jnp.dot(covs_inv_sqrt, mo), covs_inv_sqrt)
+    b = jnp.squeeze(mus) - jnp.linalg.solve(A, jnp.squeeze(mut))
+    A = sqrtm_only(A)
 
-        mo = sqrtm_only(jnp.dot(jnp.dot(covs_sqrt, covt), covs_sqrt))
-        A = jnp.dot(jnp.dot(covs_inv_sqrt, mo), covs_inv_sqrt)
-        b = jnp.squeeze(mus) - jnp.linalg.solve(A, jnp.squeeze(mut))
-        A = sqrtm_only(A)
+    return jnp.expand_dims(A, 0), jnp.expand_dims(b, 0)
 
-        return jnp.expand_dims(A, 0), jnp.expand_dims(b, 0)
+  def compute_identity_map(self, input_dim):
+    A = jnp.eye(input_dim).reshape((1, input_dim, input_dim))
+    b = jnp.zeros((1, input_dim))
 
-    def compute_identity_map(self, input_dim):
-        A = jnp.eye(input_dim).reshape((1, input_dim, input_dim))
-        b = jnp.zeros((1, input_dim))
+    return A, b
 
-        return A, b
-
-    @nn.compact
-    def __call__(self, x):
-        for i in range(self.num_hidden + 2):
-            # applies quadratic transform on data
-            # currently this is only (I x + 0), should be T(x) = 2 A x + 2 b
-            if i == 0:
-                z = self.w_xs[i](x)
-            # apply both transform on hidden state and x
-            # x is one step ahead as there is one more hidden layer for x
-            else:
-                z = jnp.add(self.w_zs[i - 1](z), self.w_xs[i](x))
-            if i != 0 or i != self.num_hidden + 1:
-                z = self.act_fn(z)
-        return jnp.squeeze(z)
+  @nn.compact
+  def __call__(self, x):
+    for i in range(self.num_hidden + 2):
+      # applies quadratic transform on data
+      # currently this is only (I x + 0), should be T(x) = 2 A x + 2 b
+      if i == 0:
+        z = self.w_xs[i](x)
+      # apply both transform on hidden state and x
+      # x is one step ahead as there is one more hidden layer for x
+      else:
+        z = jnp.add(self.w_zs[i - 1](z), self.w_xs[i](x))
+      if i != 0 or i != self.num_hidden + 1:
+        z = self.act_fn(z)
+    return jnp.squeeze(z)
--- ott/core/layers.py
+++ ott/core/layers.py
@@ -74,6 +74,7 @@ class PositiveDense(nn.Module):
 
 class PosDefDense(nn.Module):
   """A layer to output  A^T A x + b.
+
   Args:
     dim_hidden: the number of output dim_hidden.
     use_bias: whether to add a bias to the output (default: True).
@@ -93,80 +94,81 @@ class PosDefDense(nn.Module):
 
   @nn.compact
   def __call__(self, inputs):
-      """Applies a linear transformation to inputs along the last dimension.
-      Args:
-        inputs: The nd-array to be transformed.
-      Returns:
-        The transformed input.
-      """
-      inputs = jnp.asarray(inputs, self.dtype)
-      kernel = self.param(
-          'kernel', self.kernel_init, (inputs.shape[-1], self.dim_hidden))
+    """Applies a linear transformation to inputs along the last dimension.
 
-      y = jax.lax.dot_general(
-          inputs, kernel, (((inputs.ndim - 1,), (0,)), ((), ())),
-          precision=self.precision)
-      y = jax.lax.dot_general(
-          y, kernel.transpose(), (((inputs.ndim - 1,), (0,)), ((), ())),
-          precision=self.precision)
-      if self.use_bias:
-          bias = self.param("bias", self.bias_init, (self.dim_hidden,))
-          bias = jnp.asarray(bias, self.dtype)
-          y = y + bias
-      return y
+    Args:
+      inputs: The nd-array to be transformed.
+    Returns:
+      The transformed input.
+    """
+    inputs = jnp.asarray(inputs, self.dtype)
+    kernel = self.param(
+      'kernel', self.kernel_init, (inputs.shape[-1], self.dim_hidden))
+
+    y = jax.lax.dot_general(
+      inputs, kernel, (((inputs.ndim - 1,), (0,)), ((), ())),
+      precision=self.precision)
+    y = jax.lax.dot_general(
+      y, kernel.transpose(), (((inputs.ndim - 1,), (0,)), ((), ())),
+      precision=self.precision)
+    if self.use_bias:
+      bias = self.param("bias", self.bias_init, (self.dim_hidden,))
+      bias = jnp.asarray(bias, self.dtype)
+      y = y + bias
+    return y
 
 
 class PosDefPotentials(nn.Module):
-    """A layer to output  (0.5 [A_i A_i^T] (x - b_i)_i potentials.
+  """A layer to output  (0.5 [A_i A_i^T] (x - b_i)_i potentials.
+
+  Args:
+    use_bias: whether to add a bias to the output (default: True).
+    dtype: the dtype of the computation (default: float32).
+    precision: numerical precision of computation see `jax.lax.Precision`
+      for details.
+    kernel_init: initializer function for the weight matrix.
+    bias_init: initializer function for the bias.
+  """
+  dim_data: int
+  num_potentials: int
+  use_bias: bool = True
+  dtype: Any = jnp.float32
+  precision: Any = None
+  kernel_init: Callable[
+    [PRNGKey, Shape, Dtype], Array] = nn.initializers.lecun_normal()
+  bias_init: Callable[[PRNGKey, Shape, Dtype], Array] = nn.initializers.zeros
+
+  @nn.compact
+  def __call__(self, inputs):
+    """Applies a few quadratic forms.
 
     Args:
-      use_bias: whether to add a bias to the output (default: True).
-      dtype: the dtype of the computation (default: float32).
-      precision: numerical precision of computation see `jax.lax.Precision`
-        for details.
-      kernel_init: initializer function for the weight matrix.
-      bias_init: initializer function for the bias.
+      inputs: The nd-array to be transformed (possibly batched)
+    Returns:
+      The transformed input.
     """
-    dim_data: int
-    num_potentials: int
-    use_bias: bool = True
-    dtype: Any = jnp.float32
-    precision: Any = None
-    kernel_init: Callable[
-        [PRNGKey, Shape, Dtype], Array] = nn.initializers.lecun_normal()
-    bias_init: Callable[[PRNGKey, Shape, Dtype], Array] = nn.initializers.zeros
+    inputs = jnp.asarray(inputs, self.dtype)
+    kernel = self.param("kernel", self.kernel_init,
+                        (self.num_potentials, inputs.shape[-1],
+                         inputs.shape[-1]))
 
-    @nn.compact
-    def __call__(self, inputs):
-      """Applies a few quadratic forms.
+    if self.use_bias:
+      bias = self.param("bias", self.bias_init,
+                        (self.num_potentials, self.dim_data))
+      bias = jnp.asarray(bias, self.dtype)
 
-      Args:
-        inputs: The nd-array to be transformed (possibly batched)
-      Returns:
-        The transformed input.
-      """
-      inputs = jnp.asarray(inputs, self.dtype)
-      kernel = self.param("kernel", self.kernel_init,
-                          (self.num_potentials, inputs.shape[-1],
-                           inputs.shape[-1]))
+      y = inputs.reshape(
+        (-1, inputs.shape[-1])) if inputs.ndim == 1 else inputs
+      y = y[..., None] - bias.T[None, ...]
+      y = jax.lax.dot_general(
+        y, kernel, (((1,), (1,)), ((2,), (0,))),
+        precision=self.precision)
+    else:
+      y = jax.lax.dot_general(
+        inputs, kernel, (((inputs.ndim - 1,), (0,)), ((), ())),
+        precision=self.precision)
 
-      if self.use_bias:
-          bias = self.param("bias", self.bias_init,
-                            (self.num_potentials, self.dim_data))
-          bias = jnp.asarray(bias, self.dtype)
-
-          y = inputs.reshape(
-              (-1, inputs.shape[-1])) if inputs.ndim == 1 else inputs
-          y = y[..., None] - bias.T[None, ...]
-          y = jax.lax.dot_general(
-              y, kernel, (((1,), (1,)), ((2,), (0,))),
-              precision=self.precision)
-      else:
-          y = jax.lax.dot_general(
-              inputs, kernel, (((inputs.ndim - 1,), (0,)), ((), ())),
-              precision=self.precision)
-
-      y = 0.5 * y * y
-      out = jnp.sum(
-          y.reshape((-1, self.num_potentials, self.dim_data)), axis=2)
-      return out
+    y = 0.5 * y * y
+    out = jnp.sum(
+      y.reshape((-1, self.num_potentials, self.dim_data)), axis=2)
+    return out
