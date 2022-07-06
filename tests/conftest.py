e4273dd0e0d07ec237a9843ba8a817615911f3d6
Michal Klein <michal.klein@protonmail.com>
2022-07-06 12:25:19 +0200

    Update FGW test, fix fixture handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,14 @@ from _pytest.python import Metafunc
 
 
 def pytest_generate_tests(metafunc: Metafunc) -> None:
+  if not hasattr(metafunc.function, "pytestmark"):
+    # no annotation
+    return
+
   fast_marks = [m for m in metafunc.function.pytestmark if m.name == "fast"]
   if fast_marks:
     mark, = fast_marks
+    # TODO(michalk8): handle mark.args?
     selected: Optional[Mapping[str, Any]] = mark.kwargs.pop("only_fast", None)
     argnames = tuple(mark.kwargs.keys())
     argvalues = [(vs,) if not isinstance(vs,
@@ -19,12 +24,18 @@ def pytest_generate_tests(metafunc: Metafunc) -> None:
     argvalues = list(itertools.product(*argvalues))
 
     opt = str(metafunc.config.getoption("-m"))
-    if selected is not None and "fast" in opt:
-      # filter if `-m fast` was passed
-      combinations = []
-      for vs in argvalues:
-        if selected == dict(zip(argnames, vs)):
-          combinations.append(vs)
+    if "fast" in opt:  # filter if `-m fast` was passed
+      if isinstance(selected, dict):
+        combinations = []
+        for vs in argvalues:
+          if selected == dict(zip(argnames, vs)):
+            combinations.append(vs)
+      elif isinstance(selected, int):
+        # TODO(michalk8): limit only when 1 parametrized value
+        # convenient way of specifying by indexing
+        combinations = [argvalues[selected]]
+      else:
+        raise TypeError(f"Invalid fast selection type `{type(selected)}`.")
     else:
       combinations = argvalues
 
