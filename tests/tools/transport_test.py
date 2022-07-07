2a2f697af555588b6c0322314436c618d8b6c6cc
Michal Klein <michal.klein@protonmail.com>
2022-07-07 16:42:31 +0200

    Update soft sort and transport tests

--- tests/tools/transport_test.py
+++ tests/tools/transport_test.py
@@ -16,32 +16,30 @@
 import jax
 import jax.numpy as jnp
 import numpy as np
-from absl.testing import absltest
+import pytest
 
 from ott.core import linear_problems
 from ott.geometry import pointcloud
 from ott.tools import transport
 
 
-class TransportTest(absltest.TestCase):
+@pytest.mark.fast
+class TestTransport:
   """Tests for the Transport class."""
 
-  def setUp(self):
-    super().setUp()
-    self.rng = jax.random.PRNGKey(0)
-
-  def test_transport_from_point(self):
-    rngs = jax.random.split(self.rng, 2)
+  def test_transport_from_point(self, rng: jnp.ndarray):
+    rngs = jax.random.split(rng, 2)
     num_a, num_b = 23, 17
     x = jax.random.uniform(rngs[0], (num_a, 4))
     y = jax.random.uniform(rngs[1], (num_b, 4))
     ot = transport.solve(x, y, threshold=1e-2)
-    self.assertEqual(ot.matrix.shape, (num_a, num_b))
+
+    np.testing.assert_array_equal(ot.matrix.shape, (num_a, num_b))
     np.testing.assert_allclose(jnp.sum(ot.matrix, axis=1), ot.a, atol=1e-3)
     np.testing.assert_allclose(jnp.sum(ot.matrix, axis=0), ot.b, atol=1e-3)
 
-  def test_transport_from_geom(self):
-    rngs = jax.random.split(self.rng, 3)
+  def test_transport_from_geom(self, rng: jnp.ndarray):
+    rngs = jax.random.split(rng, 3)
     num_a, num_b = 23, 17
     x = jax.random.uniform(rngs[0], (num_a, 4))
     y = jax.random.uniform(rngs[1], (num_b, 4))
@@ -49,12 +47,13 @@ class TransportTest(absltest.TestCase):
     b = jax.random.uniform(rngs[2], (num_b,))
     b /= jnp.sum(b)
     ot = transport.solve(geom, b=b, threshold=1e-3)
-    self.assertEqual(ot.matrix.shape, (num_a, num_b))
+
+    np.testing.assert_array_equal(ot.matrix.shape, (num_a, num_b))
     np.testing.assert_allclose(jnp.sum(ot.matrix, axis=1), ot.a, atol=1e-3)
     np.testing.assert_allclose(jnp.sum(ot.matrix, axis=0), ot.b, atol=1e-3)
 
-  def test_transport_from_problem(self):
-    rngs = jax.random.split(self.rng, 3)
+  def test_transport_from_problem(self, rng: jnp.ndarray):
+    rngs = jax.random.split(rng, 3)
     num_a, num_b = 23, 17
     x = jax.random.uniform(rngs[0], (num_a, 4))
     y = jax.random.uniform(rngs[1], (num_b, 4))
@@ -63,22 +62,19 @@ class TransportTest(absltest.TestCase):
     b /= jnp.sum(b)
     pb = linear_problems.LinearProblem(geom, b=b)
     ot = transport.solve(pb)
-    self.assertEqual(ot.matrix.shape, (num_a, num_b))
+
+    np.testing.assert_array_equal(ot.matrix.shape, (num_a, num_b))
     np.testing.assert_allclose(jnp.sum(ot.matrix, axis=1), ot.a, atol=1e-3)
     np.testing.assert_allclose(jnp.sum(ot.matrix, axis=0), ot.b, atol=1e-3)
 
-  def test_transport_wrong_init(self):
-    rngs = jax.random.split(self.rng, 2)
+  def test_transport_wrong_init(self, rng: jnp.ndarray):
+    rngs = jax.random.split(rng, 2)
     num_a, num_b = 23, 17
     x = jax.random.uniform(rngs[0], (num_a, 4))
     y = jax.random.uniform(rngs[1], (num_b, 4))
     geom = pointcloud.PointCloud(x, y, epsilon=1e-2, batch_size=10)
-    with self.assertRaisesRegex(AttributeError, r".*has no attribute.*'"):
+    with pytest.raises(AttributeError, match=r".*has no attribute.*'"):
       transport.solve(geom, x, threshold=1e-3)
 
-    with self.assertRaisesRegex(ValueError, "Cannot instantiate a transport"):
+    with pytest.raises(ValueError, match="Cannot instantiate a transport"):
       transport.solve('pointcloud', threshold=1e-3)
-
-
-if __name__ == '__main__':
-  absltest.main()
