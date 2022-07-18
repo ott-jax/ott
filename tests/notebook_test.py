1cfdce1467efeb6407915f2cd102534c266df4e8
Michal Klein <michal.klein@protonmail.com>
2022-07-18 12:42:48 +0200

    Switch to `testbook`, add notebook tests

---
+++ tests/notebook_test.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import pytest
+from testbook import testbook
+
+ROOT = Path("docs/notebooks")
+
+
+@pytest.mark.notebook
+class TestNotebook:
+
+  @pytest.mark.parametrize(
+      "notebook", [
+          "point_clouds", "introduction_grid", "Hessians", "gromov_wasserstein",
+          "LRSinkhorn", "GWLRSinkhorn", "wasserstein_barycenters_gmms"
+      ]
+  )
+  def test_notebook_regression(self, notebook: str, request):
+    kernel_name = request.config.getoption("--kernel-name")
+    timeout = request.config.getoption("--notebook-cell-timeout")
+
+    if not notebook.endswith(".ipynb"):
+      notebook += ".ipynb"
+
+    with testbook(
+        ROOT / notebook, execute=True, timeout=timeout, kernel_name=kernel_name
+    ) as _:
+      pass
