Fix/padded scaling (#116)
* [ci skip] Fix duplicate bibtex entry and a typo

* [ci skip] Update segment docs

* Add subset ixs, fix `Geometry` scaling

* Update `inv_scale_cost` in PC, privatize methods

* Update PC subsetting, LRC `inv_scale_cost`

* Rename variable, fix LRC subsetting, typing

* Add segmented Sinkhorn divergences to docs

* Minor doc fixes

* Return mask in `segment`, update Sink div

* Fix passing masks in vmap

* Fix LRC unflattening

* Fix prepare divergences in `PointCloud`

* Add masked summary test

* Refactor fixture

* Fix linter and link to PC in README.md

* Fix bug in `test_euclidean_momentum_params`

* Put epsilon-related args to `aux_data`

* Add shape test

* Test inverse scaling of masked geometry

* Add test for permutation

* Refactor scaling to use masks instead of subsets

* Fix `Geometry.inv_scale_cost`, allow `int`

* Add tests, fix some TODOs

* Fix test, do not fail fast on CI

* Update docstrings

* Add test for mask conversion

* Allow for mask in seg. Sink/Sink divergences

* Fix tree flattening

* First stab at fixing cont. barycenters

* Remove passing segmented arrays to bar problem

Also polish the documentation

* Update FGW barycenters

* Reintroduce the option of pre-segmented measures

* Update segment_point_cloud

* Fix typo in the docstring, start FGW bary tests

* Better division impl.

* Clean `GWBarycenter` impl., enable masks

* Fix segment sinkhorn, divergences

* Add GW barycenter test, fix KL loss

* Add FGW barycenter test, clean impl.

* Fix unused variables in tests, lexer and badge

* Add shape assertions, update docs

ott/core/continuous_barycenter.py
@@ -56,14 +56,21 @@ class BarycenterState(NamedTuple):
       self, iteration: int, bar_prob: bar_problems.BarycenterProblem,
       linear_ot_solver: Any, store_errors: bool
   ) -> 'BarycenterState':
-    segmented_y, segmented_b = bar_prob.segmented_y_b
+    seg_y, seg_b = bar_prob.segmented_y_b
 
     @functools.partial(jax.vmap, in_axes=[None, None, 0, 0])
-    def solve_linear_ot(a, x, b, y):
+    def solve_linear_ot(
+        a: Optional[jnp.ndarray], x: jnp.ndarray, b: jnp.ndarray, y: jnp.ndarray
+    ):
       out = linear_ot_solver(
           linear_problems.LinearProblem(
               pointcloud.PointCloud(
-                  x, y, cost_fn=bar_prob.cost_fn, epsilon=bar_prob.epsilon
+                  x,
+                  y,
+                  src_mask=a > 0.,
+                  tgt_mask=b > 0.,
+                  cost_fn=bar_prob.cost_fn,
+                  epsilon=bar_prob.epsilon
               ), a, b
           )
       )
@@ -79,7 +86,7 @@ class BarycenterState(NamedTuple):
       )
 
     reg_ot_costs, convergeds, matrices, errors = solve_linear_ot(
-        self.a, self.x, segmented_b, segmented_y
+        self.a, self.x, seg_b, seg_y
     )
 
     cost = jnp.sum(reg_ot_costs * bar_prob.weights)
@@ -95,7 +102,7 @@ class BarycenterState(NamedTuple):
     # Approximation of barycenter as barycenter of barycenters per measure.
 
     barycenters_per_measure = bar_problems.barycentric_projection(
-        matrices, segmented_y, bar_prob.cost_fn
+        matrices, seg_y, bar_prob.cost_fn
     )
 
     x_new = jax.vmap(
@@ -130,9 +137,23 @@ class WassersteinBarycenter(was_solver.WassersteinSolver):
       bar_prob: bar_problems.BarycenterProblem,
       bar_size: int,
       x_init: Optional[jnp.ndarray] = None,
+      # TODO(michalk8): change the API to pass the PRNG key directly
       rng: int = 0,
   ) -> BarycenterState:
-    """Initialize the state of the Wasserstein barycenter iterations."""
+    """Initialize the state of the Wasserstein barycenter iterations.
+
+    Args:
+      bar_prob: The barycenter problem.
+      bar_size: Size of the barycenter.
+      x_init: Initial barycenter estimate of shape ``[bar_size, ndim]``.
+        If `None`, ``bar_size`` points will be sampled from the input
+        measures according to their weights
+        :attr:`~ott.core.bar_problems.BarycenterProblem.flattened_y`.
+      rng: Seed for :func:`jax.random.PRNGKey`.
+
+    Returns:
+      The initial barycenter state.
+    """
     if x_init is not None:
       assert bar_size == x_init.shape[0]
       x = x_init
@@ -152,7 +173,7 @@ class WassersteinBarycenter(was_solver.WassersteinSolver):
     num_iter = self.max_iterations
     if self.store_inner_errors:
       errors = -jnp.ones((
-          num_iter, bar_prob.num_segments,
+          num_iter, bar_prob.num_measures,
           self.linear_ot_solver.outer_iterations
       ))
     else:

ott/core/segment.py
@@ -11,8 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Prepare point clouds for parallel computations."""
-from types import MappingProxyType
-from typing import Any, Callable, Mapping, Optional, Sequence, Tuple
+from typing import Callable, Optional, Tuple
 
 import jax
 from jax import numpy as jnp
@@ -20,51 +19,64 @@ from jax import numpy as jnp
 
 def segment_point_cloud(
     x: jnp.ndarray,
-    num_segments: int,
-    max_measure_size: int,
     a: Optional[jnp.ndarray] = None,
+    num_segments: Optional[int] = None,
+    max_measure_size: Optional[int] = None,
     segment_ids: Optional[jnp.ndarray] = None,
     indices_are_sorted: bool = False,
-    num_per_segment: Optional[Tuple[int]] = None,
+    num_per_segment: Optional[Tuple[int, ...]] = None,
     padding_vector: Optional[jnp.ndarray] = None
-) -> Tuple[jnp.ndarray, jnp.ndarray, int]:
+) -> Tuple[jnp.ndarray, jnp.ndarray]:
   """Segment and pad as needed the entries of a point cloud.
 
-  There are two interfaces: either use `segment_ids`, and optionally
-  `num_segments` and `indices_are_sorted`, to describe for each
-  data point in the matrix to which segment each point corresponds to,
-  OR use `num_per_segment`, which describes contiguous segments.
+  There are two interfaces:
 
-  If using the first interface, `num_segments` is required for JIT compilation.
-  Assumes range(0, `num_segments`) are the segment ids.
+  1. use ``segment_ids``, and optionally ``indices_are_sorted`` to describe
+     for each data point in the matrix to which segment it belongs to.
+  2. use ``num_per_segment`` which describes contiguous segments.
 
-  In both cases, jitting requires defining a max_measure_size, the
+  If using the 1st interface, ``num_segments`` is required for JIT compilation.
+  Assumes ``range(0, num_segments)`` are the segment ids.
+
+  In both cases, jitting requires defining a ``max_measure_size``, the
   upper bound on the maximal size of measures, which will be used for padding.
 
   Args:
-    x: Array of input points, of shape [num_x, feature]. Multiple segments are
-      held in this single array.
-    num_segments: Number of segments. This is required for JIT compilation
-      to work.
-    max_measure_size: Overall size of padding. This argument is needed
-      when jitting.
-    segment_ids: (1st interface) The segment ID for which each row of x
+    x: Array of input points, of shape ``[num_x, ndim]``.
+      Multiple segments are held in this single array.
+    a: Array of shape ``[num_x,]`` containing the weights (within each measure)
+      of all the points.
+    num_segments: Number of segments. Required for jitting.
+      If `None` and using the 2nd interface, it will be computed as
+      ``len(num_per_segment)``.
+    max_measure_size: Overall size of padding. Required for jitting.
+      If `None` and using the 2nd interface, it will be computed as
+      ``max(num_per_segment)``.
+    segment_ids: **1st interface** The segment ids for which each row of ``x``
       belongs. This is a similar interface to :func:`jax.ops.segment_sum`.
-    num_per_segment: (2nd interface) Number of points in each segment in `x`.
-      For example, [100, 20, 30] would imply that `x` is segmented into three
-      arrays of length `[100]`, `[20]`, and `[30]` respectively. Must be a tuple
-      and not a `jnp.ndarray` to allow jitting. This means changes in
-      `num_per_segment` will re-trigger compilation.
+    indices_are_sorted: **1st interface** Whether ``segment_ids`` are sorted.
+    num_per_segment: **2nd interface** Number of points in each segment.
+      For example, `[100, 20, 30]` would imply that ``x`` is segmented into 3
+      arrays of length `[100]`, `[20]`, and `[30]`, respectively.
+      Must be a tuple and not a :class:`jax.numpy.ndarray` to allow jitting.
+      This means changes in ``num_per_segment`` will re-trigger compilation.
     padding_vector: vector to be used to pad point cloud matrices. Most likely
       to be zero, but can be adjusted to be other values to avoid errors or
       over/underflow in cost matrix that could be problematic (even these values
       are not supposed to be taken given their corresponding masses are 0).
+      See also :func:`ott.geometry.costs.CostFn.padder`.
+      If ``None``, vector of 0s of shape ``[1, ndim]`` is used.
+
   Returns:
-    Segmented ``x``, `a`` and the number segments.
+    Segmented ``x`` as an array of shape
+    ``[num_measures, max_measure_size, ndim]`` and ``a`` as an array of shape
+    ``[num_measures, max_measure_size]``.
   """
   num, dim = x.shape
   use_segment_ids = segment_ids is not None
   if use_segment_ids:
+    assert num_segments is not None, "Please specify `num_segments`."
+    assert max_measure_size is not None, "Please specify `max_measure_size`."
     num_per_segment = jax.ops.segment_sum(
         jnp.ones_like(segment_ids),
         segment_ids,
@@ -72,8 +84,13 @@ def segment_point_cloud(
         indices_are_sorted=indices_are_sorted
     )
   else:
-    assert num_per_segment is not None
-    assert num_segments == len(num_per_segment)
+    assert num_per_segment is not None, "Please specify `num_per_segment`."
+    if max_measure_size is None:
+      max_measure_size = max(num_per_segment)
+    if num_segments is None:
+      num_segments = len(num_per_segment)
+    else:
+      assert num_segments == len(num_per_segment)
     # conversion to facilitate computation of default weight below.
     num_per_segment = jnp.array(num_per_segment)
     segment_ids = jnp.arange(num_segments).repeat(
@@ -89,23 +106,18 @@ def segment_point_cloud(
   if padding_vector is None:
     padding_vector = jnp.zeros((1, dim))
 
-  segmented_a = []
-  segmented_x = []
-
   x = jnp.concatenate((x, padding_vector))
   a = jnp.concatenate((a, jnp.zeros((1,))))
+  segmented_a, segmented_x = [], []
 
   for i in range(num_segments):
     idx = jnp.where(segment_ids == i, jnp.arange(num), num + 1)
     idx = jax.lax.dynamic_slice(jnp.sort(idx), (0,), (max_measure_size,))
 
     # segment the weights
-    z = a.at[idx].get()
-    segmented_a.append(z)
-
+    segmented_a.append(a.at[idx].get())
     # segment the positions
-    z = x.at[idx].get()
-    segmented_x.append(z)
+    segmented_x.append(x.at[idx].get())
 
   segmented_a = jnp.stack(segmented_a)
   segmented_x = jnp.stack(segmented_x)
@@ -116,10 +128,10 @@ def segment_point_cloud(
 def _segment_interface(
     x: jnp.ndarray,
     y: jnp.ndarray,
-    num_segments: int,
-    max_measure_size: int,
     eval_fn: Callable[[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray],
                       jnp.ndarray],
+    num_segments: Optional[int] = None,
+    max_measure_size: Optional[int] = None,
     segment_ids_x: Optional[jnp.ndarray] = None,
     segment_ids_y: Optional[jnp.ndarray] = None,
     indices_are_sorted: bool = False,
@@ -144,9 +156,9 @@ def _segment_interface(
 
   segmented_x, segmented_weights_x = segment_point_cloud(
       x,
-      num_segments,
-      max_measure_size,
-      weights_x,
+      a=weights_x,
+      num_segments=num_segments,
+      max_measure_size=max_measure_size,
       segment_ids=segment_ids_x,
       indices_are_sorted=indices_are_sorted,
       num_per_segment=num_per_segment_x,
@@ -155,60 +167,19 @@ def _segment_interface(
 
   segmented_y, segmented_weights_y = segment_point_cloud(
       y,
-      num_segments,
-      max_measure_size,
-      weights_y,
+      a=weights_y,
+      num_segments=num_segments,
+      max_measure_size=max_measure_size,
       segment_ids=segment_ids_y,
       indices_are_sorted=indices_are_sorted,
       num_per_segment=num_per_segment_y,
       padding_vector=padding_vector
   )
 
-  v_eval = jax.vmap(eval_fn, in_axes=[0, 0, 0, 0])
-
+  v_eval = jax.vmap(eval_fn, in_axes=[0] * 4)
   return v_eval(
-      segmented_x, segmented_y, segmented_weights_x, segmented_weights_y
+      segmented_x,
+      segmented_y,
+      segmented_weights_x,
+      segmented_weights_y,
   )
-
-
-def pad_along_axis(
-    x: Sequence[jnp.ndarray],
-    max_pad_size: Mapping[int, Optional[int]] = MappingProxyType({}),
-    constant_values: Any = 0.0,
-    **kwargs: Any,
-) -> jnp.ndarray:
-  """Pad and stack sequence of arrays.
-
-  Args:
-    x: Sequence of arrays to pad.
-    max_pad_size: Maximum padding size along each axis. Always pads after.
-      Each key specifies an axis to pad, value corresponds to its new size.
-      If the value is ``None``, maximum value across all arrays is used.
-    constant_values: Value to pad with.
-    kwargs: Keyword arguments for :func:`jax.numpy.pad`.
-
-  Returns:
-    The padded array.
-  """
-  shapes = jnp.asarray([arr.shape for arr in x])
-  res = []
-
-  for arr in x:
-    pad_width = []
-    # TODO(michalk8): handle negative axes
-    for dim in range(arr.ndim):
-      max_size = max_pad_size.get(dim, arr.shape[dim])
-      if max_size is None:
-        max_size = jnp.max(shapes[:, dim])
-      # if negative, `jnp.pad` will raise
-      pad_width.append((0, max_size - arr.shape[dim]))
-    padded = jnp.pad(
-        arr,
-        pad_width=pad_width,
-        mode='constant',
-        constant_values=constant_values,
-        **kwargs
-    )
-    res.append(padded)
-
-  return jnp.asarray(res)

ott/geometry/geometry.py
@@ -62,13 +62,17 @@ class Geometry:
       'median', 'mean' and 'max_cost'. Alternatively, a float factor can be
       given to rescale the cost such that ``cost_matrix /= scale_cost``.
       If `True`, use 'mean'.
+    tgt_mask: Mask specifying valid rows when computing some statistics of
+      :attr:`cost_matrix`, see :attr:`src_mask`.
+    tgt_mask: Mask specifying valid columns when computing some statistics of
+      :attr:`cost_matrix`, see :attr:`tgt_mask`.
     kwargs: additional kwargs to epsilon scheduler.
 
   Note:
     When defining a ``Geometry`` through a ``cost_matrix``, it is important to
     select an ``epsilon`` regularization parameter that is meaningful. That
     parameter can be provided by the user, or assigned a default value through
-    a simple rule, using the mean cost value implied by the ``cost_matrix``.
+    a simple rule, using the :attr:`mean_cost_matrix`.
   """
 
   def __init__(
@@ -78,8 +82,10 @@ class Geometry:
       epsilon: Union[epsilon_scheduler.Epsilon, float, None] = None,
       relative_epsilon: Optional[bool] = None,
       scale_epsilon: Optional[float] = None,
-      scale_cost: Optional[Union[Literal['mean', 'max_cost', 'median'], bool,
-                                 float]] = None,
+      src_mask: Optional[jnp.ndarray] = None,
+      tgt_mask: Optional[jnp.ndarray] = None,
+      scale_cost: Union[bool, int, float, Literal['mean', 'max_cost',
+                                                  'median']] = 1.0,
       **kwargs: Any,
   ):
     self._cost_matrix = cost_matrix
@@ -88,6 +94,8 @@ class Geometry:
     self._relative_epsilon = relative_epsilon
     self._scale_epsilon = scale_epsilon
     self._scale_cost = "mean" if scale_cost is True else scale_cost
+    self._src_mask = src_mask
+    self._tgt_mask = tgt_mask
     # Define default dictionary and update it with user's values.
     self._kwargs = {**{'init': None, 'decay': None}, **kwargs}
 
@@ -136,22 +144,20 @@ class Geometry:
 
   @property
   def median_cost_matrix(self) -> float:
-    """Median of cost matrix."""
-    return jnp.median(self.cost_matrix)
+    """Median of the :attr:`cost_matrix`."""
+    geom = self._masked_geom(mask_value=jnp.nan)
+    return jnp.nanmedian(geom.cost_matrix)  # will fail for online PC
 
   @property
   def mean_cost_matrix(self) -> float:
-    """Mean of cost matrix."""
-    if isinstance(self.shape[0], int) and (self.shape[0] > 0):
-      return jnp.sum(self.apply_cost(jnp.ones((self.shape[0],)))) / (
-          self.shape[0] * self.shape[1]
-      )
-    else:
-      return 1.0
+    """Mean of the :attr:`cost_matrix`."""
+    tmp = self._masked_geom().apply_cost(self._n_normed_ones).squeeze()
+    return jnp.sum(tmp * self._m_normed_ones)
 
   @property
   def kernel_matrix(self) -> jnp.ndarray:
-    """Kernel matrix, either provided by user or recomputed from cost."""
+    """Kernel matrix, either provided by user or recomputed from \
+     :attr:`cost_matrix`."""
     if self._kernel_matrix is None:
       return jnp.exp(-(self._cost_matrix * self.inv_scale_cost / self.epsilon))
     return self._kernel_matrix ** self.inv_scale_cost
@@ -163,13 +169,13 @@ class Geometry:
 
   @property
   def shape(self) -> Tuple[int, int]:
-    """Shape of cost or kernel matrix."""
+    """Shape of the geometry."""
     mat = (
         self._kernel_matrix if self._cost_matrix is None else self._cost_matrix
     )
     if mat is not None:
       return mat.shape
-    return (0, 0)
+    return 0, 0
 
   @property
   def is_squared_euclidean(self) -> bool:
@@ -192,18 +198,16 @@ class Geometry:
   @property
   def inv_scale_cost(self) -> float:
     """Compute and return inverse of scaling factor for cost matrix."""
-    if isinstance(self._scale_cost, float):
+    if isinstance(self._scale_cost, (int, float)):
       return 1.0 / self._scale_cost
-    elif self._scale_cost == 'max_cost':
-      return 1.0 / jnp.max(self._cost_matrix)
-    elif self._scale_cost == 'mean':
-      return 1.0 / jnp.mean(self._cost_matrix)
-    elif self._scale_cost == 'median':
-      return 1.0 / jnp.median(self._cost_matrix)
-    elif isinstance(self._scale_cost, str):
-      raise ValueError(f'Scaling {self._scale_cost} not implemented.')
-    else:
-      return 1.0
+    self = self._masked_geom(mask_value=jnp.nan)
+    if self._scale_cost == 'max_cost':
+      return 1.0 / jnp.nanmax(self._cost_matrix)
+    if self._scale_cost == 'mean':
+      return 1.0 / jnp.nanmean(self._cost_matrix)
+    if self._scale_cost == 'median':
+      return 1.0 / jnp.nanmedian(self._cost_matrix)
+    raise ValueError(f'Scaling {self._scale_cost} not implemented.')
 
   def _set_scale_cost(
       self, scale_cost: Optional[Union[bool, float, str]]
@@ -236,7 +240,7 @@ class Geometry:
       vec: jnp.ndarray = None,
       axis: int = 0
   ) -> jnp.ndarray:
-    r"""Apply kernel in log domain on pair of dual potential variables.
+    r"""Apply :attr:`kernel_matrix` in log domain on a pair of dual potential variables.
 
     This function applies the ground geometry's kernel in log domain, using
     a stabilized formulation. At a high level, this iteration performs either:
@@ -275,7 +279,7 @@ class Geometry:
       eps: Optional[float] = None,
       axis: int = 0,
   ) -> jnp.ndarray:
-    """Apply kernel on positive scaling vector.
+    """Apply :attr:`kernel_matrix` on positive scaling vector.
 
     This function applies the ground geometry's kernel, to perform either
     output = K v    (1)
@@ -311,6 +315,7 @@ class Geometry:
     correction used to stabilise computations, and lifts this with an exp to
     recover either of the marginals corresponding to the transport map induced
     by potentials.
+
     Args:
       f: jnp.ndarray [num_a,] , potential of size num_rows of cost_matrix
       g: jnp.ndarray [num_b,] , potential of size num_cols of cost_matrix
@@ -504,7 +509,7 @@ class Geometry:
     """Compute scaling vector from dual potential.
 
     Args:
-      scaling: vector.
+      potential: vector.
 
     Returns:
       a vector of the same size.
@@ -540,7 +545,7 @@ class Geometry:
       fn: Optional[Callable[[jnp.ndarray], jnp.ndarray]] = None,
       **kwargs: Any
   ) -> jnp.ndarray:
-    """Apply cost matrix to array (vector or matrix).
+    """Apply :attr:`cost_matrix` to array (vector or matrix).
 
     This function applies the ground geometry's cost matrix, to perform either
     output = C arr (if axis=1)
@@ -563,21 +568,6 @@ class Geometry:
     app = functools.partial(self._apply_cost_to_vec, axis=axis, fn=fn, **kwargs)
     return jax.vmap(app, in_axes=1, out_axes=1)(arr)
 
-  def rescale_cost_fn(self, factor: float) -> None:
-    """Rescale the cost or kernel matrix using a factor.
-
-    Args:
-      factor: used to multiply the cost matrix, or, alternatively, used to
-        exponentiate (using its inverse) the kernel matrix.
-
-    Returns:
-      Nothing.
-    """
-    if self._cost_matrix is not None:
-      self._cost_matrix *= factor
-    if self._kernel_matrix is not None:
-      self._kernel_matrix **= 1 / factor
-
   def _apply_cost_to_vec(
       self,
       vec: jnp.ndarray,
@@ -585,7 +575,7 @@ class Geometry:
       fn=None,
       **_: Any,
   ) -> jnp.ndarray:
-    """Apply [num_a, num_b] fn(cost) (or transpose) to vector.
+    """Apply ``[num_a, num_b]`` fn(cost) (or transpose) to vector.
 
     Args:
       vec: jnp.ndarray [num_a,] ([num_b,] if axis=1) vector
@@ -704,52 +694,181 @@ class Geometry:
     )
 
   def subset(
-      self,
-      src_ixs: Optional[jnp.ndarray],
-      tgt_ixs: Optional[jnp.ndarray],
-      **kwargs: Any,
+      self, src_ixs: Optional[jnp.ndarray], tgt_ixs: Optional[jnp.ndarray],
+      **kwargs: Any
   ) -> "Geometry":
-    """Subset rows and/or columns of a geometry.
+    """Subset rows or columns of a geometry.
 
     Args:
-      src_ixs: Source indices. If ``None``, use all rows.
-      tgt_ixs: Target indices. If ``None``, use all columns.
-      kwargs: Keyword arguments for :class:`ott.geometry.geometry.Geometry`.
+      src_ixs: Row indices. If ``None``, use all rows.
+      tgt_ixs: Column indices. If ``None``, use all columns.
+      kwargs: Keyword arguments to override the initialization.
 
     Returns:
-      Subset of a geometry.
+      The subsetted geometry.
     """
 
-    def sub(
-        arr: jnp.ndarray, src_ixs: Optional[jnp.ndarray],
-        tgt_ixs: Optional[jnp.ndarray]
-    ) -> jnp.ndarray:
+    def subset_fn(
+        arr: Optional[jnp.ndarray],
+        src_ixs: Optional[jnp.ndarray],
+        tgt_ixs: Optional[jnp.ndarray],
+    ) -> Optional[jnp.ndarray]:
+      if arr is None:
+        return None
       if src_ixs is not None:
-        arr = arr[jnp.atleast_1d(src_ixs), :]
+        arr = arr[jnp.atleast_1d(src_ixs)]
       if tgt_ixs is not None:
         arr = arr[:, jnp.atleast_1d(tgt_ixs)]
       return arr
 
-    (cost, kernel, *children), aux_data = self.tree_flatten()
-    if cost is not None:
-      cost = sub(cost, src_ixs, tgt_ixs)
-    if kernel is not None:
-      kernel = sub(kernel, src_ixs, tgt_ixs)
+    return self._mask_subset_helper(
+        src_ixs, tgt_ixs, fn=subset_fn, propagate_mask=True, **kwargs
+    )
+
+  def mask(
+      self,
+      src_mask: Optional[jnp.ndarray],
+      tgt_mask: Optional[jnp.ndarray],
+      mask_value: float = 0.,
+  ) -> "Geometry":
+    """Mask rows or columns of a geometry.
+
+    The mask is used only when computing some statistics of the
+    :attr:`cost_matrix`.
+
+        - :attr:`mean_cost_matrix`
+        - :attr:`median_cost_matrix`
+        - :attr:`inv_scale_cost`
+
+    Args:
+      src_mask: Row mask. Can be specified either as a boolean array of shape
+        ``[num_a,]`` or as an array of indices. If ``None``, no mask is applied.
+      tgt_mask: Column mask. Can be specified either as a boolean array of shape
+        ``[num_b,]`` or as an array of indices. If ``None``, no mask is applied.
+      mask_value: Value to use for masking.
+
+    Returns:
+      The masked geometry.
+    """
+
+    def mask_fn(
+        arr: Optional[jnp.ndarray],
+        src_mask: Optional[jnp.ndarray],
+        tgt_mask: Optional[jnp.ndarray],
+    ) -> Optional[jnp.ndarray]:
+      if arr is None:
+        return arr
+      assert arr.ndim == 2, arr.ndim
+      if src_mask is not None:
+        arr = jnp.where(src_mask[:, None], arr, mask_value)
+      if tgt_mask is not None:
+        arr = jnp.where(tgt_mask[None, :], arr, mask_value)
+      return arr
+
+    src_mask = self._normalize_mask(src_mask, self.shape[0])
+    tgt_mask = self._normalize_mask(tgt_mask, self.shape[1])
+    return self._mask_subset_helper(
+        src_mask, tgt_mask, fn=mask_fn, propagate_mask=False
+    )
+
+  def _mask_subset_helper(
+      self,
+      src_ixs: Optional[jnp.ndarray],
+      tgt_ixs: Optional[jnp.ndarray],
+      *,
+      fn: Callable[
+          [Optional[jnp.ndarray], Optional[jnp.ndarray], Optional[jnp.ndarray]],
+          Optional[jnp.ndarray]],
+      propagate_mask: bool,
+      **kwargs: Any,
+  ) -> "Geometry":
+    (cost, kernel, *children, src_mask, tgt_mask,
+     kws), aux_data = self.tree_flatten()
+    cost = fn(cost, src_ixs, tgt_ixs)
+    kernel = fn(kernel, src_ixs, tgt_ixs)
+    if propagate_mask:
+      src_mask = self._normalize_mask(src_mask, self.shape[0])
+      tgt_mask = self._normalize_mask(tgt_mask, self.shape[1])
+      src_mask = fn(src_mask, src_ixs, None)
+      tgt_mask = fn(tgt_mask, tgt_ixs, None)
 
     aux_data = {**aux_data, **kwargs}
-    return type(self).tree_unflatten(aux_data, [cost, kernel] + children)
+    return type(self).tree_unflatten(
+        aux_data, [cost, kernel] + children + [src_mask, tgt_mask, kws]
+    )
+
+  @property
+  def src_mask(self) -> Optional[jnp.ndarray]:
+    """Mask of shape ``[num_a,]`` to compute :attr:`cost_matrix` statistics.
+
+    Specifically, it is used when computing:
+
+      - :attr:`mean_cost_matrix`
+      - :attr:`median_cost_matrix`
+      - :attr:`inv_scale_cost`
+    """
+    return self._normalize_mask(self._src_mask, self.shape[0])
+
+  @property
+  def tgt_mask(self) -> Optional[jnp.ndarray]:
+    """Mask of shape ``[num_b,]`` to compute :attr:`cost_matrix` statistics.
+
+    Specifically, it is used when computing:
+
+      - :attr:`mean_cost_matrix`
+      - :attr:`median_cost_matrix`
+      - :attr:`inv_scale_cost`
+    """
+    return self._normalize_mask(self._tgt_mask, self.shape[1])
+
+  def _masked_geom(self, mask_value: float = 0.) -> "Geometry":
+    """Mask geometry based on :attr:`src_mask` and :attr:`tgt_mask`."""
+    src_mask, tgt_mask = self.src_mask, self.tgt_mask
+    if src_mask is None and tgt_mask is None:
+      return self
+    return self.mask(src_mask, tgt_mask, mask_value=mask_value)
+
+  @property
+  def _n_normed_ones(self) -> jnp.ndarray:
+    """Normalized array of shape ``[num_a,]`` \
+    taking into account :attr:`src_mask`."""
+    mask = self.src_mask
+    arr = jnp.ones(self.shape[0]) if mask is None else mask
+    return arr / jnp.sum(arr)
+
+  @property
+  def _m_normed_ones(self) -> jnp.ndarray:
+    """Normalized array of shape ``[num_b,]`` \
+    taking into account :attr:`tgt_mask`."""
+    mask = self.tgt_mask
+    arr = jnp.ones(self.shape[1]) if mask is None else mask
+    return arr / jnp.sum(arr)
+
+  @staticmethod
+  def _normalize_mask(mask: Optional[Union[int, jnp.ndarray]],
+                      size: int) -> Optional[jnp.ndarray]:
+    """Convert array of indices to a boolean mask."""
+    if mask is None:
+      return None
+    mask = jnp.atleast_1d(mask)
+    if not jnp.issubdtype(mask, (bool, jnp.bool_)):
+      mask = jnp.isin(jnp.arange(size), mask)
+    assert mask.shape == (size,)
+    return mask
 
   def tree_flatten(self):
     return (
         self._cost_matrix, self._kernel_matrix, self._epsilon_init,
-        self._relative_epsilon, self._kwargs
+        self._relative_epsilon, self._scale_epsilon, self._src_mask,
+        self._tgt_mask, self._kwargs
     ), {
         'scale_cost': self._scale_cost
     }
 
   @classmethod
   def tree_unflatten(cls, aux_data, children):
-    return cls(*children[:-1], **children[-1], **aux_data)
+    *args, kwargs = children
+    return cls(*args, **kwargs, **aux_data)
 
 
 def is_affine(fn) -> bool:

ott/geometry/grid.py
@@ -113,7 +113,7 @@ class Grid(geometry.Geometry):
 
   @property
   def cost_matrices(self) -> List[jnp.ndarray]:
-    # computes cost matrices along each dimension of the grid
+    """Cost matrices along each dimension of the grid."""
     cost_matrices = []
     for dimension, cost_fn in itertools.zip_longest(
         range(self.grid_dimension), self.cost_fns, fillvalue=self.cost_fns[-1]
@@ -126,7 +126,7 @@ class Grid(geometry.Geometry):
 
   @property
   def kernel_matrices(self) -> List[jnp.ndarray]:
-    # computes kernel matrices from cost matrices grid
+    """Kernel matrices along each dimension of the grid."""
     kernel_matrices = []
     for cost_matrix in self.cost_matrices:
       kernel_matrices.append(jnp.exp(-cost_matrix / self.epsilon))
@@ -134,7 +134,8 @@ class Grid(geometry.Geometry):
 
   @property
   def median_cost_matrix(self) -> NoReturn:
-    raise NotImplementedError('Median cost not implemented for grids')
+    """Not implemented."""
+    raise NotImplementedError('Median cost not implemented for grids.')
 
   @property
   def shape(self) -> Tuple[int, int]:
@@ -290,6 +291,7 @@ class Grid(geometry.Geometry):
   def transport_from_potentials(
       self, f: jnp.ndarray, g: jnp.ndarray, axis: int = 0
   ) -> NoReturn:
+    """Not implemented, use :meth:`apply_transport_from_potentials` instead."""
     raise ValueError(
         'Grid geometry cannot instantiate a transport matrix, use',
         ' apply_transport_from_potentials(...) if you wish to ',
@@ -300,6 +302,7 @@ class Grid(geometry.Geometry):
   def transport_from_scalings(
       self, f: jnp.ndarray, g: jnp.ndarray, axis: int = 0
   ) -> NoReturn:
+    """Not implemented, use :meth:`apply_transport_from_scalings` instead."""
     raise ValueError(
         'Grid geometry cannot instantiate a transport matrix, use',
         ' apply_transport_from_scalings(...) if you wish to ',
@@ -310,7 +313,17 @@ class Grid(geometry.Geometry):
   def subset(
       self, src_ixs: Optional[jnp.ndarray], tgt_ixs: Optional[jnp.ndarray]
   ) -> NoReturn:
-    raise NotImplementedError("Subsetting grid is not implemented.")
+    """Not implemented."""
+    raise NotImplementedError("Subsetting is not implemented for grids.")
+
+  def mask(
+      self,
+      src_mask: Optional[jnp.ndarray],
+      tgt_mask: Optional[jnp.ndarray],
+      mask_value: float = 0.,
+  ) -> NoReturn:
+    """Not implemented."""
+    raise NotImplementedError("Masking is not implemented for grids.")
 
   @classmethod
   def prepare_divergences(

ott/tools/segment_sinkhorn.py
@@ -24,8 +24,8 @@ from ott.geometry import costs, pointcloud
 def segment_sinkhorn(
     x: jnp.ndarray,
     y: jnp.ndarray,
-    num_segments: int,
-    max_measure_size: int,
+    num_segments: Optional[int] = None,
+    max_measure_size: Optional[int] = None,
     cost_fn: Optional[costs.CostFn] = None,
     segment_ids_x: Optional[jnp.ndarray] = None,
     segment_ids_y: Optional[jnp.ndarray] = None,
@@ -52,28 +52,31 @@ def segment_sinkhorn(
 
   For both interfaces, both `x` and `y` should contain the same total number of
   segments. Each segment will be padded as necessary, all segments rearranged as
-  a tensor, and `vmap` used to evaluate sinkhorn divergences in parallel.
+  a tensor, and :func:`jax.vmap` used to evaluate sinkhorn divergences in
+  parallel.
 
   Args:
     x: Array of input points, of shape [num_x, feature]. Multiple segments are
       held in this single array.
     y: Array of target points, of shape [num_y, feature].
     num_segments: Number of segments contained in x and y. Providing this number
-      is required for JIT compilation to work.
+      is required for JIT compilation to work, see also
+      :func:`~ott.core.segment.segment_point_cloud`.
     max_measure_size: Total size of measures after padding. Should ideally be
       set to an upper bound on points clouds processed with the segment
-      interface. Providing this number is required for JIT
-      compilation to work.
-    segment_ids_x: (1st interface) The segment ID for which each row of x
+      interface. Providing this number is required for JIT compilation to work.
+    cost_fn: Cost function, defaults to :class:`~ott.core.costs.Euclidean`.
+    segment_ids_x: **1st interface** The segment ID for which each row of x
       belongs. This is a similar interface to `jax.ops.segment_sum`.
-    segment_ids_y: (1st interface) The segment ID for which each row of y
+    segment_ids_y: **1st interface** The segment ID for which each row of y
       belongs.
-    indices_are_sorted: (1st interface) Whether `segment_ids_x` and
+    indices_are_sorted: **1st interface** Whether `segment_ids_x` and
       `segment_ids_y` are sorted. Default false.
-    num_per_segment_x: (2nd interface) Number of points in each segment in `x`.
-      For example, [100, 20, 30] would imply that `x` is segmented into three
-      arrays of length `[100]`, `[20]`, and `[30]` respectively.
-    num_per_segment_y: (2nd interface) Number of points in each segment in `y`.
+    num_per_segment_x: **2nd interface** Number of points in each segment in
+      `x`. For example, [100, 20, 30] would imply that `x` is segmented into
+      three arrays of length `[100]`, `[20]`, and `[30]` respectively.
+    num_per_segment_y: **2nd interface** Number of points in each segment in
+      `y`.
     weights_x: Weights of each input points, arranged in the same segmented
       order as `x`.
     weights_y: Weights of each input points, arranged in the same segmented
@@ -86,23 +89,36 @@ def segment_sinkhorn(
     kwargs: keywords arguments passed to form
       :class:`ott.geometry.pointcloud.PointCloud` geometry objects from the
       subsets of points and masses selected in `x` and `y`, possibly a
-      :class:`ott.geometry.costs.CostFn` or and entropy regularizer.
+      :class:`ott.geometry.costs.CostFn` or an entropy regularizer.
+
   Returns:
     An array of sinkhorn reg_ot_cost for each segment.
   """
-  # Instatiate padding vector.
+  # instantiate padding vector
   dim = x.shape[1]
   if cost_fn is None:
-    # Default padder.
+    # default padder
     padding_vector = costs.CostFn.padder(dim=dim)
   else:
     padding_vector = cost_fn.padder(dim=dim)
 
-  sinkhorn_kwargs = {} if sinkhorn_kwargs is None else sinkhorn_kwargs
-
-  def eval_fn(padded_x, padded_y, padded_weight_x, padded_weight_y):
+  def eval_fn(
+      padded_x: jnp.ndarray,
+      padded_y: jnp.ndarray,
+      padded_weight_x: jnp.ndarray,
+      padded_weight_y: jnp.ndarray,
+  ) -> float:
+    mask_x = padded_weight_x > 0.
+    mask_y = padded_weight_y > 0.
     return sinkhorn.sinkhorn(
-        pointcloud.PointCloud(padded_x, padded_y, cost_fn=cost_fn, **kwargs),
+        pointcloud.PointCloud(
+            padded_x,
+            padded_y,
+            cost_fn=cost_fn,
+            src_mask=mask_x,
+            tgt_mask=mask_y,
+            **kwargs
+        ),
         a=padded_weight_x,
         b=padded_weight_y,
         **sinkhorn_kwargs
@@ -111,9 +127,9 @@ def segment_sinkhorn(
   return segment._segment_interface(
       x,
       y,
-      num_segments,
-      max_measure_size,
       eval_fn,
+      num_segments=num_segments,
+      max_measure_size=max_measure_size,
       segment_ids_x=segment_ids_x,
       segment_ids_y=segment_ids_y,
       indices_are_sorted=indices_are_sorted,

ott/tools/sinkhorn_divergence.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Implements the sinkhorn divergence."""
-
-from typing import Any, Dict, List, Mapping, NamedTuple, Optional, Tuple
+from types import MappingProxyType
+from typing import Any, List, Mapping, NamedTuple, Optional, Tuple, Type
 
 from jax import numpy as jnp
 
@@ -31,11 +31,11 @@ class SinkhornDivergenceOutput(NamedTuple):
 
 
 def sinkhorn_divergence(
-    geom: geometry.Geometry,
+    geom: Type[geometry.Geometry],
     *args: Any,
     a: Optional[jnp.ndarray] = None,
     b: Optional[jnp.ndarray] = None,
-    sinkhorn_kwargs: Optional[Dict[str, Any]] = None,
+    sinkhorn_kwargs: Mapping[str, Any] = MappingProxyType({}),
     static_b: bool = False,
     share_epsilon: bool = True,
     **kwargs: Any,
@@ -43,18 +43,18 @@ def sinkhorn_divergence(
   """Compute Sinkhorn divergence defined by a geometry, weights, parameters.
 
   Args:
-    geom: a geometry class.
-    args: arguments to
-      :meth:`ott.geometry.geometry.Geometry.prepare_divergences` that is
+    geom: Type of the geometry.
+    args: Positional arguments to
+      :meth:`~ott.geometry.geometry.Geometry.prepare_divergences` that is
       specific to each geometry.
-    a: jnp.ndarray<float>[n]: the weight of each input point. The sum of
-      all elements of b must match that of a to converge.
-    b: jnp.ndarray<float>[m]: the weight of each target point. The sum of
-      all elements of b must match that of a to converge.
-    sinkhorn_kwargs: Optionally a dict containing the keywords arguments for
-      calls to the `sinkhorn` function, that is called twice if static_b else
-      three times.
-    static_b: if True, divergence of measure b against itself is NOT computed
+    a: the weight of each input point. The sum of all elements of `a` must
+      match that of `b` to converge.
+    b: the weight of each target point. The sum of all elements of `b` must
+      match that of `a` to converge.
+    sinkhorn_kwargs: keywords arguments for :func:`~ott.core.sinkhorn.sinkhorn`
+      that is called twice if ``static_b = True`` else 3 times.
+    static_b: if True, divergence of measure `b` against itself is **not**
+      computed.
     share_epsilon: if True, enforces that the same epsilon regularizer is shared
       for all 2 or 3 terms of the Sinkhorn divergence. In that case, the epsilon
       will be by default that used when comparing x to y (contained in the first
@@ -65,7 +65,7 @@ def sinkhorn_divergence(
       geometry.
 
   Returns:
-    tuple: (sinkhorn divergence value, three pairs of potentials, three costs)
+    Sinkhorn divergence value, three pairs of potentials, three costs.
   """
   geometries = geom.prepare_divergences(*args, static_b=static_b, **kwargs)
   num_a, num_b = geometries[0].shape
@@ -79,8 +79,7 @@ def sinkhorn_divergence(
 
   a = jnp.ones((num_a,)) / num_a if a is None else a
   b = jnp.ones((num_b,)) / num_b if b is None else b
-  div_kwargs = {} if sinkhorn_kwargs is None else sinkhorn_kwargs
-  return _sinkhorn_divergence(*geometries, a, b, **div_kwargs)
+  return _sinkhorn_divergence(*geometries, a, b, **sinkhorn_kwargs)
 
 
 def _sinkhorn_divergence(
@@ -107,7 +106,7 @@ def _sinkhorn_divergence(
      all elements of b must match that of a to converge.
     b: jnp.ndarray<float>[m]: the weight of each target point. The sum of
      all elements of b must match that of a to converge.
-    kwargs: Keyword arguments to sinkhorn.
+    kwargs: Keyword arguments to :func:`ott.core.sinkhorn.sinkhorn`.
 
   Returns:
     SinkhornDivergenceOutput named tuple.
@@ -153,8 +152,8 @@ def _sinkhorn_divergence(
 def segment_sinkhorn_divergence(
     x: jnp.ndarray,
     y: jnp.ndarray,
-    num_segments: int,
-    max_measure_size: int,
+    num_segments: Optional[int] = None,
+    max_measure_size: Optional[int] = None,
     cost_fn: Optional[costs.CostFn] = None,
     segment_ids_x: Optional[jnp.ndarray] = None,
     segment_ids_y: Optional[jnp.ndarray] = None,
@@ -163,7 +162,7 @@ def segment_sinkhorn_divergence(
     num_per_segment_y: Optional[jnp.ndarray] = None,
     weights_x: Optional[jnp.ndarray] = None,
     weights_y: Optional[jnp.ndarray] = None,
-    sinkhorn_kwargs: Optional[Mapping[str, Any]] = None,
+    sinkhorn_kwargs: Mapping[str, Any] = MappingProxyType({}),
     static_b: bool = False,
     share_epsilon: bool = True,
     **kwargs: Any
@@ -190,22 +189,24 @@ def segment_sinkhorn_divergence(
       held in this single array.
     y: Array of target points, of shape [num_y, feature].
     num_segments: Number of segments contained in x and y. Providing this number
-      is required for JIT compilation to work.
+      is required for JIT compilation to work, see also
+      :func:`~ott.core.segment.segment_point_cloud`.
     max_measure_size: Total size of measures after padding. Should ideally be
       set to an upper bound on points clouds processed with the segment
       interface. Should also be smaller than total length of `x` or `y`.
       Providing this number is required for JIT compilation to work.
-    cost_fn: a :class:`ott.geometry.costs.CostFn` Cost. Defaults to sqEuclidean.
-    segment_ids_x: (1st interface) The segment ID for which each row of x
-      belongs. This is a similar interface to `jax.ops.segment_sum`.
-    segment_ids_y: (1st interface) The segment ID for which each row of y
+    cost_fn: Cost function, defaults to :class:`~ott.core.costs.Euclidean`.
+    segment_ids_x: **1st interface** The segment ID for which each row of x
+      belongs. This is a similar interface to :func:`jax.ops.segment_sum`.
+    segment_ids_y: **1st interface** The segment ID for which each row of y
       belongs.
-    indices_are_sorted: (1st interface) Whether `segment_ids_x` and
+    indices_are_sorted: **1st interface** Whether `segment_ids_x` and
       `segment_ids_y` are sorted. Default false.
-    num_per_segment_x: (2nd interface) Number of points in each segment in `x`.
-      For example, [100, 20, 30] would imply that `x` is segmented into three
-      arrays of length `[100]`, `[20]`, and `[30]` respectively.
-    num_per_segment_y: (2nd interface) Number of points in each segment in `y`.
+    num_per_segment_x: **2nd interface** Number of points in each segment in
+      `x`. For example, [100, 20, 30] would imply that `x` is segmented into
+      three arrays of length `[100]`, `[20]`, and `[30]` respectively.
+    num_per_segment_y: **2nd interface** Number of points in each segment in
+      `y`.
     weights_x: Weights of each input points, arranged in the same segmented
       order as `x`.
     weights_y: Weights of each input points, arranged in the same segmented
@@ -229,15 +230,22 @@ def segment_sinkhorn_divergence(
   Returns:
     An array of sinkhorn divergence values for each segment.
   """
-  # Instatiate padding vector.
+  # instantiate padding vector
   dim = x.shape[1]
   if cost_fn is None:
-    # Default padder.
+    # default padder
     padding_vector = costs.CostFn.padder(dim=dim)
   else:
     padding_vector = cost_fn.padder(dim=dim)
 
-  def eval_fn(padded_x, padded_y, padded_weight_x, padded_weight_y):
+  def eval_fn(
+      padded_x: jnp.ndarray,
+      padded_y: jnp.ndarray,
+      padded_weight_x: jnp.ndarray,
+      padded_weight_y: jnp.ndarray,
+  ) -> float:
+    mask_x = padded_weight_x > 0.
+    mask_y = padded_weight_y > 0.
     return sinkhorn_divergence(
         pointcloud.PointCloud,
         padded_x,
@@ -248,15 +256,17 @@ def segment_sinkhorn_divergence(
         static_b=static_b,
         share_epsilon=share_epsilon,
         cost_fn=cost_fn,
+        src_mask=mask_x,
+        tgt_mask=mask_y,
         **kwargs
     ).divergence
 
   return segment._segment_interface(
       x,
       y,
-      num_segments,
-      max_measure_size,
       eval_fn,
+      num_segments=num_segments,
+      max_measure_size=max_measure_size,
       segment_ids_x=segment_ids_x,
       segment_ids_y=segment_ids_y,
       indices_are_sorted=indices_are_sorted,

tests/core/continuous_barycenter_test.py
@@ -13,14 +13,16 @@
 
 # Lint as: python3
 """Tests for Continuous barycenters."""
+import functools
+from typing import Any, Optional, Sequence, Tuple
 
 import jax
 import jax.numpy as jnp
 import numpy as np
 import pytest
 
-from ott.core import bar_problems, continuous_barycenter
-from ott.geometry import costs
+from ott.core import bar_problems, continuous_barycenter, gw_barycenter, segment
+from ott.geometry import costs, pointcloud
 from ott.tools.gaussian_mixture import gaussian_mixture
 
 means_and_covs_to_x = jax.vmap(costs.mean_and_cov_to_x, in_axes=[0, 0, None])
@@ -68,15 +70,17 @@ class TestBarycenter:
       b.append(c / jnp.sum(c))
     b = jnp.concatenate(b, axis=0)
     # Set a barycenter problem with 8 measures, of irregular sizes.
-
     bar_prob = bar_problems.BarycenterProblem(
         y,
-        num_measures=8,
+        b,
+        epsilon=epsilon,
+        num_segments=8,
         max_measure_size=35,
-        b=b,
-        num_per_segment=num_per_segment,
-        epsilon=epsilon
+        num_per_segment=num_per_segment
     )
+    assert bar_prob.num_measures == 8
+    assert bar_prob.max_measure_size == 35
+    assert bar_prob.ndim == self.DIM
 
     # Define solver
     threshold = 1e-3
@@ -100,6 +104,57 @@ class TestBarycenter:
     # Check shape is as expected
     np.testing.assert_array_equal(out.x.shape, (bar_size, self.DIM))
 
+    # Check convergence by looking at cost evolution.
+    c = out.costs[out.costs > -1]
+    assert jnp.isclose(c[-2], c[-1], rtol=threshold)
+
+    # Check barycenter has all points roughly in [1,2]^4.
+    # (this is because sampled points were equally set in either [0,1]^4
+    # or [2,3]^4)
+    assert jnp.all(out.x.ravel() < 2.3)
+    assert jnp.all(out.x.ravel() > .7)
+
+  @pytest.mark.parametrize("segment_before", [False, True])
+  def test_barycenter_jit(self, rng: jnp.ndarray, segment_before: bool):
+
+    @functools.partial(jax.jit, static_argnums=(2, 3))
+    def barycenter(
+        y: jnp.ndarray, b: jnp.ndarray, segment_before: bool,
+        num_per_segment: int
+    ) -> continuous_barycenter.BarycenterState:
+      if segment_before:
+        y, b = segment.segment_point_cloud(
+            x=y, a=b, num_per_segment=num_per_segment
+        )
+        bar_prob = bar_problems.BarycenterProblem(y, b, epsilon=1e-1)
+      else:
+        bar_prob = bar_problems.BarycenterProblem(
+            y, b, epsilon=1e-1, num_per_segment=num_per_segment
+        )
+      solver = continuous_barycenter.WassersteinBarycenter(threshold=threshold)
+      return solver(bar_prob)
+
+    rngs = jax.random.split(rng, 20)
+    # Sample 2 point clouds, each of size 113, the first around [0,1]^4,
+    # Second around [2,3]^4.
+    y1 = jax.random.uniform(rngs[0], (self.N_POINTS, self.DIM))
+    y2 = jax.random.uniform(rngs[1], (self.N_POINTS, self.DIM)) + 2
+    # Merge them
+    y = jnp.concatenate((y1, y2))
+
+    # Define segments
+    num_per_segment = (33, 29, 24, 27, 27, 31, 30, 25)
+    # Set weights for each segment that sum to 1.
+    b = []
+    for rng, n in zip(rngs, num_per_segment):
+      c = jax.random.uniform(rng, (n,))
+      b.append(c / jnp.sum(c))
+    b = jnp.concatenate(b, axis=0)
+
+    threshold = 1e-3
+    out = barycenter(
+        y, b, segment_before=segment_before, num_per_segment=num_per_segment
+    )
     # Check convergence by looking at cost evolution.
     c = out.costs[out.costs > -1]
     assert jnp.isclose(c[-2], c[-1], rtol=threshold)
@@ -134,10 +189,10 @@ class TestBarycenter:
     means2 = jnp.array([[1., 1.], [1., -1.]])
     sigma = 0.01
     covs1 = sigma * jnp.asarray([
-        jnp.eye(dimension) for i in range(num_components)
+        jnp.eye(dimension) for _ in range(num_components)
     ])
     covs2 = sigma * jnp.asarray([
-        jnp.eye(dimension) for i in range(num_components)
+        jnp.eye(dimension) for _ in range(num_components)
     ])
 
     y1 = means_and_covs_to_x(means1, covs1, dimension)
@@ -157,16 +212,24 @@ class TestBarycenter:
 
     x_init = means_and_covs_to_x(x_init_means, x_init_covs, dimension)
 
-    bar_p = bar_problems.BarycenterProblem(
-        y,
-        num_measures=num_measures,
+    seg_y, seg_b = segment.segment_point_cloud(
+        x=y,
+        a=b,
+        num_segments=num_measures,
         max_measure_size=num_components,
-        b=b,
+        num_per_segment=(num_components, num_components),
+        padding_vector=bures_cost.padder(y.shape[1]),
+    )
+    bar_p = bar_problems.BarycenterProblem(
+        seg_y,
+        seg_b,
         weights=barycentric_weights,
-        num_per_segment=jnp.asarray([num_components, num_components]),
         cost_fn=bures_cost,
         epsilon=epsilon
     )
+    assert bar_p.num_measures == seg_y.shape[0]
+    assert bar_p.max_measure_size == seg_y.shape[1]
+    assert bar_p.ndim == seg_y.shape[2]
 
     solver = continuous_barycenter.WassersteinBarycenter(
         lse_mode=lse_mode, jit=jit
@@ -192,7 +255,7 @@ class TestBarycenter:
         )
     )
 
-    assert jnp.allclose(
+    np.testing.assert_allclose(
         covs_bary,
         jnp.array([sigma * jnp.eye(dimension) for i in range(bar_size)]),
         rtol=1e-05,
@@ -217,7 +280,7 @@ class TestBarycenter:
     n_components = jnp.array([3, 4])  # the number of components of the GMMs
     num_measures = n_components.size
     bar_size = 5  # the size of the barycenter
-    int(jnp.max(n_components))
+    max_measure_size = int(jnp.max(n_components))
 
     # Create an instance of the Bures cost class.
     b_cost = costs.Bures(dimension=dim)
@@ -278,15 +341,18 @@ class TestBarycenter:
     # test second interface for segmentation
     seg_ids = jnp.repeat(jnp.arange(num_measures), n_components)
     bar_p = bar_problems.BarycenterProblem(
-        ys,
-        num_measures=num_measures,
-        max_measure_size=4,
+        y=ys,
         b=bs,
         weights=barycentric_weights,
-        segment_ids=seg_ids,
         cost_fn=b_cost,
-        epsilon=epsilon
+        epsilon=epsilon,
+        num_segments=num_measures,
+        max_measure_size=max_measure_size,
+        segment_ids=seg_ids,
     )
+    assert bar_p.max_measure_size == 4
+    assert bar_p.num_measures == num_measures
+    assert bar_p.ndim == ys.shape[-1]
 
     solver = continuous_barycenter.WassersteinBarycenter(lse_mode=True, jit=jit)
 
@@ -311,3 +377,153 @@ class TestBarycenter:
         jax.vmap(is_positive_semidefinite, in_axes=0, out_axes=0)(covs_bary),
         True
     )
+
+
+class TestGWBarycenter:
+  NDIM = 3
+  NDIM_F = 4
+
+  @staticmethod
+  def random_pc(
+      n: int,
+      d: int,
+      rng: jnp.ndarray,
+      m: Optional[int] = None,
+      **kwargs: Any
+  ) -> pointcloud.PointCloud:
+    key1, key2 = jax.random.split(rng, 2)
+    x = jax.random.normal(key1, (n, d))
+    y = x if m is None else jax.random.normal(key2, (m, d))
+    return pointcloud.PointCloud(x, y, batch_size=None, **kwargs)
+
+  @staticmethod
+  def pad_cost_matrices(
+      costs: Sequence[jnp.ndarray],
+      shape: Optional[Tuple[int, int]] = None
+  ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+    if shape is None:
+      shape = jnp.asarray([arr.shape for arr in costs]).max()
+      shape = (shape, shape)
+    else:
+      assert shape[0] == shape[1], shape
+
+    cs, weights = [], []
+    for cost in costs:
+      r, c = cost.shape
+      cs.append(jnp.zeros(shape).at[:r, :c].set(cost))
+      w = jnp.ones(r) / r
+      weights.append(jnp.concatenate([w, jnp.zeros(shape[0] - r)]))
+    return jnp.stack(cs), jnp.stack(weights)
+
+  @pytest.mark.parametrize(
+      "gw_loss,bar_size,epsilon", [("sqeucl", 17, None), ("kl", 22, 1e-2)]
+  )
+  def test_gw_barycenter(
+      self, rng: jnp.ndarray, gw_loss: str, bar_size: int,
+      epsilon: Optional[float]
+  ):
+    tol = 1e-2 if gw_loss == 'kl' else 1e-3
+    num_per_segment = (13, 15, 21)
+    rngs = jax.random.split(rng, len(num_per_segment))
+    pcs = [
+        self.random_pc(n, d=self.NDIM, rng=rng, epsilon=epsilon)
+        for n, rng in zip(num_per_segment, rngs)
+    ]
+    costs = [pc._compute_cost_matrix() for pc, n in zip(pcs, num_per_segment)]
+    costs, cbs = self.pad_cost_matrices(costs)
+    ys = jnp.concatenate([pc.x for pc in pcs])
+    bs = jnp.concatenate([jnp.ones(n) / n for n in num_per_segment])
+    kwargs = {
+        "gw_loss": gw_loss,
+        "num_per_segment": num_per_segment,
+        "epsilon": epsilon
+    }
+
+    problem_pc = bar_problems.GWBarycenterProblem(y=ys, b=bs, **kwargs)
+    problem_cost = bar_problems.GWBarycenterProblem(
+        costs=costs,
+        b=cbs,
+        **kwargs,
+    )
+    for prob in [problem_pc, problem_cost]:
+      assert not prob.is_fused
+      assert prob.ndim_fused is None
+      assert prob.num_measures == len(num_per_segment)
+      assert prob.max_measure_size == max(num_per_segment)
+      assert prob._loss_name == gw_loss
+    assert problem_pc.ndim == self.NDIM
+    assert problem_cost.ndim is None
+
+    solver = gw_barycenter.GromovWassersteinBarycenter(jit=True)
+    out_pc = solver(problem_pc, bar_size=bar_size)
+    out_cost = solver(problem_cost, bar_size=bar_size)
+
+    assert out_pc.x is None
+    assert out_cost.x is None
+    assert out_pc.cost.shape == (bar_size, bar_size)
+    np.testing.assert_allclose(out_pc.cost, out_cost.cost, rtol=tol, atol=tol)
+    np.testing.assert_allclose(out_pc.costs, out_cost.costs, rtol=tol, atol=tol)
+
+  @pytest.mark.fast(
+      "jit,fused_penalty,scale_cost", [(False, 1.5, "mean"),
+                                       (True, 3.1, "max_cost")],
+      only_fast=0
+  )
+  def test_fgw_barycenter(
+      self,
+      rng: jnp.ndarray,
+      jit: bool,
+      fused_penalty: float,
+      scale_cost: str,
+  ):
+
+    def barycenter(
+        y: jnp.ndim, y_fused: jnp.ndarray, num_per_segment: Tuple[int, ...]
+    ) -> gw_barycenter.GWBarycenterState:
+      prob = bar_problems.GWBarycenterProblem(
+          y=y,
+          y_fused=y_fused,
+          num_per_segment=num_per_segment,
+          fused_penalty=fused_penalty,
+          scale_cost=scale_cost,
+      )
+      assert prob.is_fused
+      assert prob.fused_penalty == fused_penalty
+      assert not prob._y_as_costs
+      assert prob.max_measure_size == max(num_per_segment)
+      assert prob.num_measures == len(num_per_segment)
+      assert prob.ndim == self.NDIM
+      assert prob.ndim_fused == self.NDIM_F
+
+      solver = gw_barycenter.GromovWassersteinBarycenter(
+          jit=False, store_inner_errors=True
+      )
+
+      x_init = jax.random.normal(rng, (bar_size, self.NDIM_F))
+      cost_init = pointcloud.PointCloud(x_init).cost_matrix
+
+      return solver(prob, bar_size=bar_size, bar_init=(cost_init, x_init))
+
+    bar_size, epsilon, = 10, 1e-1
+    num_per_segment = (7, 12)
+
+    key1, *rngs = jax.random.split(rng, len(num_per_segment) + 1)
+    y = jnp.concatenate([
+        self.random_pc(n, d=self.NDIM, rng=rng, epsilon=epsilon).x
+        for n, rng in zip(num_per_segment, rngs)
+    ])
+    rngs = jax.random.split(key1, len(num_per_segment))
+    y_fused = jnp.concatenate([
+        self.random_pc(n, d=self.NDIM_F, rng=rng, epsilon=epsilon).x
+        for n, rng in zip(num_per_segment, rngs)
+    ])
+
+    fn = jax.jit(barycenter, static_argnums=2) if jit else barycenter
+    out = fn(y, y_fused, num_per_segment)
+
+    assert out.cost.shape == (bar_size, bar_size)
+    assert out.x.shape == (bar_size, self.NDIM_F)
+    np.testing.assert_array_equal(jnp.isfinite(out.cost), True)
+    np.testing.assert_array_equal(jnp.isfinite(out.x), True)
+    np.testing.assert_array_equal(jnp.isfinite(out.costs), True)
+    np.testing.assert_array_equal(jnp.isfinite(out.errors), True)

tests/geometry/geometry_lr_test.py
@@ -206,7 +206,8 @@ class TestCostMatrixFactorization:
       self.assert_upper_bound(geom, geom_lr, rank=rank, tol=tol)
 
   @pytest.mark.fast.with_args(
-      "batch_size,scale_cost", [(None, "mean"), (32, None)], only_fast=1
+      "batch_size,scale_cost", [(None, "mean"), (32, "max_cost"), (None, 1.0)],
+      only_fast=1
   )
   def test_point_cloud_to_lr(
       self, rng: jnp.ndarray, batch_size: Optional[int],

tests/tools/sinkhorn_divergence_test.py
@@ -255,14 +255,15 @@ class TestSinkhornDivergence:
 
     sink_div = jax.jit(
         sinkhorn_divergence.segment_sinkhorn_divergence,
-        static_argnames=['num_segments', 'max_measure_size'],
+        static_argnames=['num_per_segment_x', 'num_per_segment_y'],
     )
 
     segmented_divergences = sink_div(
         jnp.concatenate((x1, x2)),
         jnp.concatenate((y1, y2)),
-        num_segments=2,
-        max_measure_size=15,
+        # these 2 arguments are not necessary for jitting:
+        # num_segments=2,
+        # max_measure_size=15,
         num_per_segment_x=(10, 12),
         num_per_segment_y=(11, 13),
         epsilon=0.01
@@ -354,6 +355,8 @@ class TestSinkhornDivergence:
     threshold = 3.2e-3
     cloud_a = jax.random.uniform(rngs[0], (self._num_points[0], self._dim))
     cloud_b = jax.random.uniform(rngs[1], (self._num_points[1], self._dim))
+    sinkhorn_kwargs["threshold"] = threshold
+
     div = sinkhorn_divergence.sinkhorn_divergence(
         pointcloud.PointCloud,
         cloud_a,
@@ -361,7 +364,7 @@ class TestSinkhornDivergence:
         epsilon=epsilon,
         a=self._a,
         b=self._b,
-        sinkhorn_kwargs=sinkhorn_kwargs.update({'threshold': threshold})
+        sinkhorn_kwargs=sinkhorn_kwargs,
     )
     assert div.divergence > 0.0
     assert threshold > div.errors[0][-1]