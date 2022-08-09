e99b6f07ce38c9ecd315363d71e69ba630aba350
Michal Klein <michal.klein@protonmail.com>
2022-08-09 11:41:39 +0200

    Add skeleton tests

---
+++ tests/tools/k_means_test.py
@@ -0,0 +1,31 @@
+class TestKmeansPlusPlus:
+
+  def test_n_local_trials(self):
+    pass
+
+  def test_matches_sklearn(self):
+    pass
+
+
+class TestKmeans:
+
+  def test_init_method(self):
+    pass
+
+  def test_n_init(self):
+    pass
+
+  def test_weights(self):
+    pass
+
+  def test_cosine_cost_fn(self):
+    pass
+
+  def test_jitting(self):
+    pass
+
+  def test_differentiability(self):
+    pass
+
+  def test_matches_sklearn(self):
+    pass
