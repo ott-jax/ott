Fix old code in test scaling

tests/core/gromov_wasserstein_test.py
@@ -367,9 +367,9 @@ class TestGromovWasserstein:
   def test_gw_fused_scale_cost(self, scale_cost: Union[bool, str]):
     epsilon = 0.1
     fused_penalty = 1
-    geom_x = pointcloud.PointCloud(self.x, scale_cost=None)
-    geom_y = pointcloud.PointCloud(self.y, scale_cost=None)
-    geom_xy = pointcloud.PointCloud(self.xx, self.yy, scale_cost=None)
+    geom_x = pointcloud.PointCloud(self.x, scale_cost=1.)
+    geom_y = pointcloud.PointCloud(self.y, scale_cost=1.)
+    geom_xy = pointcloud.PointCloud(self.xx, self.yy, scale_cost=1.)
     geom_x_scaled = pointcloud.PointCloud(self.x, scale_cost=scale_cost)
     geom_y_scaled = pointcloud.PointCloud(self.y, scale_cost=scale_cost)
     geom_xy_scaled = pointcloud.PointCloud(
@@ -549,6 +549,3 @@ class TestGromovWassersteinUnbalanced:
     np.testing.assert_allclose(
         grad_matrices[0][1], grad_matrices[1][1], rtol=1e-02, atol=1e-02
     )
-
-  def test_gw_lr_initializer(self):
-    pass