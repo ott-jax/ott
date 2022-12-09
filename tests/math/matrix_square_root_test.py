119dc3471d666557bde3ac5e0456f7cf2686aaf5
marcocuturi <marcocuturicameto@gmail.com>
2022-12-09 19:27:33 +0100

    lint

--- tests/math/matrix_square_root_test.py
+++ tests/math/matrix_square_root_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for matrix square roots."""
-from typing import Callable, Any
+from typing import Any, Callable
 
 import jax
 import jax.numpy as jnp
