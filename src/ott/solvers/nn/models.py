3041eeae7daa01511c1ce6ce38e0fee896746488
Brandon Amos <brandon.amos.cs@gmail.com>
2023-02-10 11:30:39 -0800

    update docstring

--- src/ott/solvers/nn/models.py
+++ src/ott/solvers/nn/models.py
@@ -9,7 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Base module for potentials."""
+"""Neural potential models."""
 
 import abc
 from typing import Callable, Optional, Sequence, Tuple, Union
