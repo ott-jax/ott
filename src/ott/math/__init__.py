1c63892978149974dd009dee94edd74fa42fd7ed
michalk8 <46717574+michalk8@users.noreply.github.com>
2023-03-08 12:18:44 +0100

    Remove sparse option in `Graph` geometry (#329)
    * Remove sparse option from `Graph`
    
    * Fix typos
    
    * Update graph tests
    
    * Udpate docs, remove suitesparse from CI
    
    * Update references, fix docs linter

--- src/ott/math/__init__.py
+++ src/ott/math/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from . import (
-    decomposition,
     fixed_point_loop,
     matrix_square_root,
     unbalanced_functions,
