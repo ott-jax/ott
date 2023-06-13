e243d69a4238bcd994ed2ff55f604bb6aca2690c
marcocuturi <marcocuturicameto@gmail.com>
2023-06-13 19:41:43 +0200

    doc

--- src/ott/solvers/linear/implicit_differentiation.py
+++ src/ott/solvers/linear/implicit_differentiation.py
@@ -164,15 +164,15 @@ class ImplicitDiff:
           rtol=1e-6, atol=1e-6
       ) if self.solver is None else self.solver
 
-    # elementwise vmap apply of derivative of precondition_fun. No vmapping
-    # can be problematic here.
-    if self.precondition_fun is None:
       precond_fun = lambda x: geom.epsilon * jnp.log(x)
       symmetric = False
     else:
       precond_fun = self.precondition_fun
       symmetric = self.symmetric
     derivative = jax.vmap(jax.grad(precond_fun))
+    # elementwise vmap apply of derivative of precondition_fun. No vmapping
+    # can be problematic here.
+    if self.precondition_fun is None:
 
     n, m = geom.shape
     # pylint: disable=g-long-lambda
