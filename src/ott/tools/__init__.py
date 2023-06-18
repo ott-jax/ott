3d8ca61c5cfa7fae8bc4756448d65ed4e3046349
Théo Uscidda <theo.uscidda@gmail.com>
2023-06-18 18:37:29 +0200

    fix linter issues

--- src/ott/tools/__init__.py
+++ src/ott/tools/__init__.py
@@ -11,4 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from . import gaussian_mixture, k_means, plot, sinkhorn_divergence, soft_sort
+from . import (
+    gaussian_mixture,
+    k_means,
+    map_estimator,
+    plot,
+    sinkhorn_divergence,
+    soft_sort,
+)
