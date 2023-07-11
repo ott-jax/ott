414f40192f0b828625c4afe5737471f4aebe4695
Pierre Ablin <pierreablin@gmail.com>
2023-07-11 16:21:29 +0200

    rm types

--- src/ott/solvers/linear/sinkhorn.py
+++ src/ott/solvers/linear/sinkhorn.py
@@ -279,27 +279,26 @@ def ent_reg_cost(
 
 
 class SinkhornOutput(NamedTuple):
-  r"""Implements the problems.Transport interface, for a Sinkhorn solution.
+  """Implements the problems.Transport interface, for a Sinkhorn solution.
 
   Attributes:
-    f: jnp.ndarray, potential
-    g: jnp.ndarray, potential
-    errors: jnp.ndarray, the list of errors of the Sinkhorn iterations
-    reg_ot_cost: float, the final cost value in the primal space
-    ot_prob: linear_problem.LinearProblem, the problem Sinkhorn was fit on
-    dual_cost: float, the final unregularized cost value in the dual space
-    primal_cost: float, the final unregularized cost value in the primal space
-    linear: bool, whether the problem is linear
-    geom: geometry.Geometry, the geometry used in the problem
-    a: jnp.ndarray, marginals of the problem
-    b: jnp.ndarray, marginals of the problem
-    linear_output: bool, True
-    converged: bool, whether the Sinkhorn solver has converged
-    n_iters: int, the number of iterations of the solver
-    scalings: tuple of jnp.ndarray, the scaling vectors
-    matrix: jnp.ndarray, the transport plan matrix
-    transport_mass: float, sum of transport matrix.
-    
+    f: potential
+    g: potential
+    errors: the list of errors of the Sinkhorn iterations
+    reg_ot_cost: the final cost value in the primal space
+    ot_prob: the problem Sinkhorn was fit on
+    dual_cost: the final unregularized cost value in the dual space
+    primal_cost: the final unregularized cost value in the primal space
+    linear: whether the problem is linear
+    geom: the geometry used in the problem
+    a: marginals of the problem
+    b: marginals of the problem
+    linear_output: True
+    converged: whether the Sinkhorn solver has converged
+    n_iters: the number of iterations of the solver
+    scalings: tuple of the scaling vectors
+    matrix: the transport plan matrix
+    transport_mass: sum of transport matrix.
   """
 
   f: Optional[jnp.ndarray] = None
