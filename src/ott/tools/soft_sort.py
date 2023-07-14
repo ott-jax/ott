fix normalization of target_weights

src/ott/tools/soft_sort.py
@@ -212,63 +212,6 @@ def _ranks(
   return jnp.reshape(out, inputs.shape)
 
 
-def topk_mask(
-    inputs: jnp.ndarray,
-    axis: int = -1,
-    k: int = 1,
-    **kwargs: Any,
-) -> jnp.ndarray:
-  r"""Soft top-$k$ selection mask.
-
-  For instance:
-
-  .. code-block:: python
-
-    k = 5
-    x = jax.random.uniform(rng, (100,))
-    mask = top_k_mask(x, k=k)
-
-  will output a vector of shape ``x.shape``, with values in :math:`[0,1]`, that
-  are differentiable approximations to the binary mask selecting the top $k$
-  entries in ``x``. These should be compared to the non-differentiable mask
-  obtained with :func:`jax.numpy.argsort`, which can be obtained as:
-
-  .. code-block:: python
-
-    mask = x >= jax.numpy.sort(x).flip()[k-1]
-
-  Args:
-    inputs: Array of any shape.
-    axis: the axis on which to apply the soft-sorting operator.
-    k : topk parameter. Should be smaller than ``inputs.shape[axis]``.
-    kwargs: keyword arguments passed on to lower level functions. Of interest
-      to the user are ``squashing_fun``, which will redistribute the values in
-      ``inputs`` to lie in :math:`[0,1]` (sigmoid of whitened values by default)
-      to solve the optimal transport problem;
-      :class:`cost_fn <ott.geometry.costs.CostFn>` object of
-      :class:`~ott.geometry.pointcloud.PointCloud`, which defines the ground
-      1D cost function to transport from ``inputs`` to the ``num_targets``
-      target values ; ``epsilon`` regularization parameter. Remaining ``kwargs``
-      are passed on to parameterize the
-      :class:`~ott.solvers.linear.sinkhorn.Sinkhorn` solver.
-
-  """
-  num_points = inputs.shape[axis]
-  assert k < num_points, (
-      f"`k` must be smaller than `inputs.shape[axis]`, yet {k} >= {num_points}."
-  )
-  target_weights = jnp.array([1.0 - k / num_points, k / num_points])
-  out = apply_on_axis(
-      _ranks,
-      inputs,
-      axis,
-      num_targets=None,
-      target_weights=target_weights,
-      **kwargs
-  )
-  return out / (num_points - 1)
-
-
 def ranks(
     inputs: jnp.ndarray,
     axis: int = -1,
@@ -325,6 +268,63 @@ def ranks(
   return apply_on_axis(
       _ranks, inputs, axis, num_targets, target_weights, **kwargs
   )
+
+
+def topk_mask(
+    inputs: jnp.ndarray,
+    axis: int = -1,
+    k: int = 1,
+    **kwargs: Any,
+) -> jnp.ndarray:
+  r"""Soft top-$k$ selection mask.
+
+  For instance:
+
+  .. code-block:: python
+
+    k = 5
+    x = jax.random.uniform(rng, (100,))
+    mask = top_k_mask(x, k=k)
+
+  will output a vector of shape ``x.shape``, with values in :math:`[0,1]`, that
+  are differentiable approximations to the binary mask selecting the top $k$
+  entries in ``x``. These should be compared to the non-differentiable mask
+  obtained with :func:`jax.numpy.argsort`, which can be obtained as:
+
+  .. code-block:: python
+
+    mask = x >= jax.numpy.sort(x).flip()[k-1]
+
+  Args:
+    inputs: Array of any shape.
+    axis: the axis on which to apply the soft-sorting operator.
+    k : topk parameter. Should be smaller than ``inputs.shape[axis]``.
+    kwargs: keyword arguments passed on to lower level functions. Of interest
+      to the user are ``squashing_fun``, which will redistribute the values in
+      ``inputs`` to lie in :math:`[0,1]` (sigmoid of whitened values by default)
+      to solve the optimal transport problem;
+      :class:`cost_fn <ott.geometry.costs.CostFn>` object of
+      :class:`~ott.geometry.pointcloud.PointCloud`, which defines the ground
+      1D cost function to transport from ``inputs`` to the ``num_targets``
+      target values ; ``epsilon`` regularization parameter. Remaining ``kwargs``
+      are passed on to parameterize the
+      :class:`~ott.solvers.linear.sinkhorn.Sinkhorn` solver.
+
+  """
+  num_points = inputs.shape[axis]
+  assert k < num_points, (
+      f"`k` must be smaller than `inputs.shape[axis]`, yet {k} >= {num_points}."
+  )
+  target_weights = jnp.array([1.0 - k / num_points, k / num_points])
+  out = apply_on_axis(
+      _ranks,
+      inputs,
+      axis,
+      num_targets=None,
+      target_weights=target_weights,
+      **kwargs
+  )
+  return out / (num_points - 1)
 
 
 def quantile(

tests/tools/soft_sort_test.py
@@ -99,7 +99,7 @@ class TestSoftSort:
         squashing_fun=lambda x: x,
         epsilon=1e-4,
         axis=axis,
-        max_iterations=10000
+        max_iterations=5000
     )
     expected_ranks = jnp.argsort(
         jnp.argsort(x, axis=axis), axis=axis
@@ -113,6 +113,7 @@ class TestSoftSort:
     np.testing.assert_allclose(ranks, expected_ranks, atol=0.3, rtol=0.1)
 
     target_weights = jax.random.uniform(rng2, (num_targets,))
+    target_weights /= jnp.sum(target_weights)
     ranks = my_ranks(x, target_weights=target_weights)
     np.testing.assert_array_equal(x.shape, ranks.shape)
     np.testing.assert_allclose(ranks, expected_ranks, atol=0.3, rtol=0.1)
@@ -125,7 +126,7 @@ class TestSoftSort:
         soft_sort.topk_mask,
         squashing_fun=lambda x: x,
         epsilon=1e-4,  # needed to recover a sharp mask given close ties
-        max_iterations=20000,  # needed to recover a sharp mask given close ties
+        max_iterations=15000,  # needed to recover a sharp mask given close ties
         axis=axis
     )
     mask = my_topk_mask(x, k=k, axis=axis)