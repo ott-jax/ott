fb815f894c9f08385d5d46558fdbaa05428489f0
Daniel Packer <packer.61@osu.edu>
2023-10-24 11:13:39 -0400

    removed `FixedCouplingInitializer` class

--- src/ott/initializers/quadratic/initializers.py
+++ src/ott/initializers/quadratic/initializers.py
@@ -23,11 +23,7 @@ if TYPE_CHECKING:
   from ott.problems.linear import linear_problem
   from ott.problems.quadratic import quadratic_problem
 
-__all__ = [
-    "BaseQuadraticInitializer",
-    "QuadraticInitializer",
-    "FixedCouplingInitializer",
-]
+__all__ = ["BaseQuadraticInitializer", "QuadraticInitializer"]
 
 
 @jax.tree_util.register_pytree_node_class
@@ -123,7 +119,15 @@ class QuadraticInitializer(BaseQuadraticInitializer):
 
   When working with the fused problem, a linear term is added to the cost
   matrix: `cost_matrix` += `fused_penalty` * `geom_xy.cost_matrix`
+
+  Args:
+    init_coupling: The coupling to use for initialization. Set to `None` for
+    the uniform coupling.
   """
+
+  def __init__(self, init_coupling: Optional[jnp.ndarray] = None, **kwargs):
+    super().__init__(**kwargs)
+    self.init_coupling = init_coupling
 
   def _create_geometry(
       self,
@@ -152,70 +156,14 @@ class QuadraticInitializer(BaseQuadraticInitializer):
     geom_xx, geom_yy = quad_prob.geom_xx, quad_prob.geom_yy
 
     h1, h2 = quad_prob.quad_loss
-    tmp1 = quadratic_problem.apply_cost(geom_xx, quad_prob.a, axis=1, fn=h1)
-    tmp2 = quadratic_problem.apply_cost(geom_yy, quad_prob.b, axis=1, fn=h2)
-    tmp = jnp.outer(tmp1, tmp2)
-
-    if quad_prob.is_balanced:
-      cost_matrix = marginal_cost.cost_matrix - tmp
+    if self.init_coupling is None:
+      tmp1 = quadratic_problem.apply_cost(geom_xx, quad_prob.a, axis=1, fn=h1)
+      tmp2 = quadratic_problem.apply_cost(geom_yy, quad_prob.b, axis=1, fn=h2)
+      tmp = jnp.outer(tmp1, tmp2)
     else:
-      # initialize epsilon for Unbalanced GW according to Sejourne et. al (2021)
-      init_transport = jnp.outer(quad_prob.a, quad_prob.b)
-      marginal_1, marginal_2 = init_transport.sum(1), init_transport.sum(0)
-
-      epsilon = quadratic_problem.update_epsilon_unbalanced(
-          epsilon=epsilon, transport_mass=marginal_1.sum()
-      )
-      unbalanced_correction = quad_prob.cost_unbalanced_correction(
-          init_transport, marginal_1, marginal_2, epsilon=epsilon
-      )
-      cost_matrix = marginal_cost.cost_matrix - tmp + unbalanced_correction
-
-    cost_matrix += quad_prob.fused_penalty * quad_prob._fused_cost_matrix
-    return geometry.Geometry(
-        cost_matrix=cost_matrix,
-        epsilon=epsilon,
-        relative_epsilon=relative_epsilon
-    )
-
-
-class FixedCouplingInitializer(BaseQuadraticInitializer):
-  r"""Initialize a linear problem locally around a given coupling."""
-
-  def __init__(self, init_coupling: jnp.ndarray, **kwargs):
-    super().__init__(**kwargs)
-    self.init_coupling = init_coupling
-
-  def _create_geometry(
-      self,
-      quad_prob: "quadratic_problem.QuadraticProblem",
-      *,
-      epsilon: float,
-      relative_epsilon: Optional[bool] = None,
-      **kwargs: Any,
-  ) -> geometry.Geometry:
-    """Compute initial geometry for linearization using the given coupling.
-
-    Args:
-      quad_prob: Quadratic OT problem.
-      epsilon: Epsilon regularization.
-      relative_epsilon: Flag, use `relative_epsilon` or not in geometry.
-      kwargs: Keyword arguments for :class:`~ott.geometry.geometry.Geometry`.
-
-    Returns:
-      The initial geometry used to initialize the linearized problem.
-    """
-    from ott.problems.quadratic import quadratic_problem
-
-    del kwargs
-
-    marginal_cost = quad_prob.marginal_dependent_cost(quad_prob.a, quad_prob.b)
-    geom_xx, geom_yy = quad_prob.geom_xx, quad_prob.geom_yy
-
-    h1, h2 = quad_prob.quad_loss
-    tmp1 = h1.func(geom_xx.cost_matrix)
-    tmp2 = h2.func(geom_yy.cost_matrix)
-    tmp = tmp1 @ self.init_coupling @ tmp2.T
+      tmp1 = h1.func(geom_xx.cost_matrix)
+      tmp2 = h2.func(geom_yy.cost_matrix)
+      tmp = tmp1 @ self.init_coupling @ tmp2.T
 
     if quad_prob.is_balanced:
       cost_matrix = marginal_cost.cost_matrix - tmp
