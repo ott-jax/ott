1717f55d831fd7a2f5292629c7dd4b369f3afb83
theouscidda6 <theo.uscidda@gmail.com>
2023-11-21 11:55:33 +0100

    move_sklearn_datasets

--- src/ott/datasets.py
+++ src/ott/datasets.py
@@ -12,13 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import dataclasses
-from typing import Iterator, Literal, NamedTuple, Optional, Tuple
+from types import MappingProxyType
+from typing import Any, Iterator, Literal, Mapping, NamedTuple, Optional, Tuple
 
 import jax
 import jax.numpy as jnp
 import numpy as np
+import sklearn.datasets
 
-__all__ = ["create_gaussian_mixture_samplers", "Dataset", "GaussianMixture"]
+__all__ = [
+    "Dataset", "create_gaussian_mixture_samplers", "GaussianMixture",
+    "create_sklearn_samplers", "SklearnDistribution"
+]
 
 from ott import utils
 
@@ -105,6 +110,79 @@ class GaussianMixture:
       yield samples
 
 
+@dataclasses.dataclass
+class SklearnDistribution:
+  """A class to define toy probability 2-dimensional distributions.
+
+  Args:
+    name: the name specifying the centers of the mixture components:
+
+      - ``moon`` - lower moon Sklearn dataset,
+      - ``s_curve`` - s curve Sklearn dataset.
+
+    init_rng: initial PRNG key
+    theta_rotation: angle to define the rotation matrix to rotate samples
+    offset: offset added to the Sklearn samples
+    scale: scaling factor to scale the Sklearn samples
+    std_noise: standard deviation of the gaussian additive noise
+    batch_size: batch size of the samples
+  """
+
+  name: Literal["moon", "s_curve"]
+  init_rng: jax.Array
+  theta_rotation: float = 0.0
+  offset: Optional[jnp.ndarray] = None
+  scale: float = 1.0
+  std_noise: float = 0.01
+  batch_size: int = 1024
+
+  def __iter__(self) -> Iterator[jnp.ndarray]:
+    """Random sample generator from a Sklearn distribution.
+
+    Returns:
+    A generator of samples from the Sklearn distribution.
+    """
+    return self._create_sample_generators()
+
+  def __post_init__(self):
+    # define rotation matrix to rotate samples
+    self.rotation = jnp.array([
+        [jnp.cos(self.theta_rotation), -jnp.sin(self.theta_rotation)],
+        [jnp.sin(self.theta_rotation),
+         jnp.cos(self.theta_rotation)],
+    ])
+
+  def _create_sample_generators(self) -> Iterator[jnp.ndarray]:
+    rng = jax.random.PRNGKey(0) if self.init_rng is None else self.init_rng
+
+    while True:
+      rng, _ = jax.random.split(rng)
+      seed = jax.random.randint(rng, [], minval=0, maxval=1e5).item()
+      if self.name == "moon":
+        samples, _ = sklearn.datasets.make_moons(
+            n_samples=(self.batch_size, 0),
+            random_state=seed,
+            noise=self.std_noise,
+        )
+      elif self.name == "s_curve":
+        x, _ = sklearn.datasets.make_s_curve(
+            n_samples=self.batch_size,
+            random_state=seed,
+            noise=self.std_noise,
+        )
+        samples = x[:, [2, 0]]
+      else:
+        raise NotImplementedError(
+            f"SklearnDistribution `{self.name}` not implemented."
+        )
+
+      samples = jnp.asarray(samples, dtype=jnp.float32)
+      samples = jnp.squeeze(jnp.matmul(self.rotation[None, :], samples.T).T)
+      offset = jnp.zeros(2) if self.offset is None else self.offset
+      samples = offset + self.scale * samples
+      yield samples
+
+
 def create_gaussian_mixture_samplers(
     name_source: Name_t,
     name_target: Name_t,
@@ -149,6 +227,55 @@ def create_gaussian_mixture_samplers(
               name_target, batch_size=valid_batch_size, init_rng=rng4
           )
       )
+  )
+  dim_data = 2
+  return train_dataset, valid_dataset, dim_data
+
+
+def create_sklearn_samplers(
+    source_kwargs: Mapping[str, Any] = MappingProxyType({}),
+    target_kwargs: Mapping[str, Any] = MappingProxyType({}),
+    train_batch_size: int = 256,
+    valid_batch_size: int = 256,
+    rng: Optional[jax.Array] = None,
+) -> Tuple[Dataset, Dataset, int]:
+  """Sklearn samplers for :class:`~ott.solvers.nn.neuraldual.W2NeuralDual`.
+
+  Args:
+  source_kwargs: kwargs to initialize source sampler
+  target_kwargs: kwargs to initialize source sampler
+  train_batch_size: the training batch size
+  valid_batch_size: the validation batch size
+  rng: initial PRNG key
+
+  Returns:
+  The dataset and dimension of the data.
+  """
+  rng = jax.random.PRNGKey(0) if rng is None else rng
+  rng1, rng2, rng3, rng4 = jax.random.split(rng, 4)
+  train_dataset = Dataset(
+      source_iter=iter(
+          SklearnDistribution(
+              init_rng=rng1, batch_size=train_batch_size, **source_kwargs
+          )
+      ),
+      target_iter=iter(
+          SklearnDistribution(
+              init_rng=rng2, batch_size=train_batch_size, **target_kwargs
+          )
+      ),
+  )
+  valid_dataset = Dataset(
+      source_iter=iter(
+          SklearnDistribution(
+              init_rng=rng3, batch_size=valid_batch_size, **source_kwargs
+          )
+      ),
+      target_iter=iter(
+          SklearnDistribution(
+              init_rng=rng4, batch_size=valid_batch_size, **target_kwargs
+          )
+      ),
   )
   dim_data = 2
   return train_dataset, valid_dataset, dim_data
