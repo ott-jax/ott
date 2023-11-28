8fa3683a5b91d5848231236cb97d5b1632d1538d
Dominik Klein <dominik.klein@helmholtz-munich.de>
2023-11-28 14:53:21 +0100

    move dataloader from tests to module

--- src/ott/neural/data/dataloaders.py
+++ src/ott/neural/data/dataloaders.py
@@ -11,22 +11,123 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#import tensorflow as tf
+
+from typing import Dict, Iterator, Mapping, Optional
+
+import numpy as np
 
 
-class ConditionalDataLoader:  #TODO(@MUCDK) uncomment, resolve installation issues with TF
-  pass
+__all__ =[ "OTDataLoader", "ConditionalDataLoader"]
 
-  #def __init__(
-  #    self, rng: jax.random.KeyArray, dataloaders: Dict[str, tf.Dataloader],
-  #    p: jnp.ndarray
-  #) -> None:
-  #  super().__init__()
-  #  self.rng = rng
-  #  self.conditions = dataloaders.keys()
-  #  self.p = p
+class OTDataLoader:
+  """Data loader for OT problems.
 
-  #def __next__(self) -> jnp.ndarray:
-  #  self.rng, rng = jax.random.split(self.rng, 2)
-  #  condition = jax.random.choice(rng, self.conditions, p=self.p)
-  #  return next(self.dataloaders[condition])
+  Args:
+    batch_size: Number of samples per batch.
+    source_lin: Linear part of the source measure.
+    source_quad: Quadratic part of the source measure.
+    target_lin: Linear part of the target measure.
+    target_quad: Quadratic part of the target measure.
+    source_conditions: Conditions of the source measure.
+    target_conditions: Conditions of the target measure.
+    seed: Random seed.
+  """
+
+  def __init__(
+      self,
+      batch_size: int = 64,
+      source_lin: Optional[np.ndarray] = None,
+      source_quad: Optional[np.ndarray] = None,
+      target_lin: Optional[np.ndarray] = None,
+      target_quad: Optional[np.ndarray] = None,
+      source_conditions: Optional[np.ndarray] = None,
+      target_conditions: Optional[np.ndarray] = None,
+      seed: int = 0,
+  ) -> None:
+    super().__init__()
+    if source_lin is not None:
+      if source_quad is not None:
+        assert len(source_lin) == len(source_quad)
+        self.n_source = len(source_lin)
+      else:
+        self.n_source = len(source_lin)
+    else:
+      self.n_source = len(source_quad)
+    if source_conditions is not None:
+      assert len(source_conditions) == self.n_source
+    if target_lin is not None:
+      if target_quad is not None:
+        assert len(target_lin) == len(target_quad)
+        self.n_target = len(target_lin)
+      else:
+        self.n_target = len(target_lin)
+    else:
+      self.n_target = len(target_quad)
+    if target_conditions is not None:
+      assert len(target_conditions) == self.n_target
+
+    self.source_lin = source_lin
+    self.target_lin = target_lin
+    self.source_quad = source_quad
+    self.target_quad = target_quad
+    self.source_conditions = source_conditions
+    self.target_conditions = target_conditions
+    self.batch_size = batch_size
+    self.rng = np.random.default_rng(seed=seed)
+
+  def __next__(self) -> Mapping[str, np.ndarray]:
+    inds_source = self.rng.choice(self.n_source, size=[self.batch_size])
+    inds_target = self.rng.choice(self.n_target, size=[self.batch_size])
+    return {
+        "source_lin":
+            self.source_lin[inds_source, :]
+            if self.source_lin is not None else None,
+        "source_quad":
+            self.source_quad[inds_source, :]
+            if self.source_quad is not None else None,
+        "target_lin":
+            self.target_lin[inds_target, :]
+            if self.target_lin is not None else None,
+        "target_quad":
+            self.target_quad[inds_target, :]
+            if self.target_quad is not None else None,
+        "source_conditions":
+            self.source_conditions[inds_source, :]
+            if self.source_conditions is not None else None,
+        "target_conditions":
+            self.target_conditions[inds_target, :]
+            if self.target_conditions is not None else None,
+    }
+
+
+class ConditionalDataLoader:
+  """Data loader for OT problems with conditions.
+
+  This data loader wraps several data loaders and samples from them according to their conditions.
+
+  Args:
+    dataloaders: Dictionary of data loaders with keys corresponding to conditions.
+    p: Probability of sampling from each data loader.
+    seed: Random seed.
+
+  """
+
+  def __init__(
+      self,
+      dataloaders: Dict[str, Iterator],
+      p: np.ndarray,
+      seed: int = 0
+  ) -> None:
+    super().__init__()
+    self.dataloaders = dataloaders
+    self.conditions = list(dataloaders.keys())
+    self.p = p
+    self.rng = np.random.default_rng(seed=seed)
+
+  def __next__(self, cond: str = None) -> Mapping[str, np.ndarray]:
+    if cond is not None:
+      if cond not in self.conditions:
+        raise ValueError(f"Condition {cond} not in {self.conditions}")
+      return next(self.dataloaders[cond])
+    idx = self.rng.choice(len(self.conditions), p=self.p)
+    return next(self.dataloaders[self.conditions[idx]])
--- tests/neural/conftest.py
+++ tests/neural/conftest.py
@@ -1,60 +1,7 @@
-from typing import Dict, Iterator, Mapping, Optional
-
 import numpy as np
 import pytest
 
-
-class DataLoader:
-
-  def __init__(
-      self,
-      source_data: np.ndarray,
-      target_data: np.ndarray,
-      batch_size: int = 64,
-      source_conditions: Optional[np.ndarray] = None,
-      target_conditions: Optional[np.ndarray] = None,
-  ) -> None:
-    super().__init__()
-    self.source_data = source_data
-    self.target_data = target_data
-    self.source_conditions = source_conditions
-    self.target_conditions = target_conditions
-    self.batch_size = batch_size
-    self.rng = np.random.default_rng(seed=0)
-
-  def __next__(self) -> Mapping[str, np.ndarray]:
-    inds_source = self.rng.choice(len(self.source_data), size=[self.batch_size])
-    inds_target = self.rng.choice(len(self.target_data), size=[self.batch_size])
-    return {
-        "source_lin":
-            self.source_data[inds_source, :],
-        "target_lin":
-            self.target_data[inds_target, :],
-        "source_conditions":
-            self.source_conditions[inds_source, :]
-            if self.source_conditions is not None else None,
-        "target_conditions":
-            self.target_conditions[inds_target, :]
-            if self.target_conditions is not None else None,
-    }
-
-
-class ConditionalDataLoader:
-
-  def __init__(self, dataloaders: Dict[str, Iterator], p: np.ndarray) -> None:
-    super().__init__()
-    self.dataloaders = dataloaders
-    self.conditions = list(dataloaders.keys())
-    self.p = p
-    self.rng = np.random.default_rng(seed=0)
-
-  def __next__(self, cond: str = None) -> Mapping[str, np.ndarray]:
-    if cond is not None:
-      if cond not in self.conditions:
-        raise ValueError(f"Condition {cond} not in {self.conditions}")
-      return next(self.dataloaders[cond])
-    idx = self.rng.choice(len(self.conditions), p=self.p)
-    return next(self.dataloaders[self.conditions[idx]])
+from ott.neural.data.dataloaders import ConditionalDataLoader, OTDataLoader
 
 
 @pytest.fixture(scope="module")
@@ -63,7 +10,7 @@ def data_loader_gaussian():
   rng = np.random.default_rng(seed=0)
   source = rng.normal(size=(100, 2))
   target = rng.normal(size=(100, 2)) + 1.0
-  return DataLoader(source, target, 16)
+  return OTDataLoader(source, target, 16)
 
 
 @pytest.fixture(scope="module")
@@ -75,10 +22,10 @@ def data_loader_gaussian_conditional():
 
   source_1 = rng.normal(size=(100, 2))
   target_1 = rng.normal(size=(100, 2)) - 2.0
-  dl0 = DataLoader(
+  dl0 = OTDataLoader(
       source_0, target_0, 16, source_conditions=np.zeros_like(source_0) * 0.0
   )
-  dl1 = DataLoader(
+  dl1 = OTDataLoader(
       source_1, target_1, 16, source_conditions=np.ones_like(source_1) * 1.0
   )
 
@@ -93,75 +40,7 @@ def data_loader_gaussian_with_conditions():
   target = rng.normal(size=(100, 2)) + 1.0
   source_conditions = rng.normal(size=(100, 1))
   target_conditions = rng.normal(size=(100, 1)) - 1.0
-  return DataLoader(source, target, 16, source_conditions, target_conditions)
-
-
-class GENOTDataLoader:
-
-  def __init__(
-      self,
-      batch_size: int = 64,
-      source_lin: Optional[np.ndarray] = None,
-      source_quad: Optional[np.ndarray] = None,
-      target_lin: Optional[np.ndarray] = None,
-      target_quad: Optional[np.ndarray] = None,
-      source_conditions: Optional[np.ndarray] = None,
-      target_conditions: Optional[np.ndarray] = None,
-  ) -> None:
-    super().__init__()
-    if source_lin is not None:
-      if source_quad is not None:
-        assert len(source_lin) == len(source_quad)
-        self.n_source = len(source_lin)
-      else:
-        self.n_source = len(source_lin)
-    else:
-      self.n_source = len(source_quad)
-    if source_conditions is not None:
-      assert len(source_conditions) == self.n_source
-    if target_lin is not None:
-      if target_quad is not None:
-        assert len(target_lin) == len(target_quad)
-        self.n_target = len(target_lin)
-      else:
-        self.n_target = len(target_lin)
-    else:
-      self.n_target = len(target_quad)
-    if target_conditions is not None:
-      assert len(target_conditions) == self.n_target
-
-    self.source_lin = source_lin
-    self.target_lin = target_lin
-    self.source_quad = source_quad
-    self.target_quad = target_quad
-    self.source_conditions = source_conditions
-    self.target_conditions = target_conditions
-    self.batch_size = batch_size
-    self.rng = np.random.default_rng(seed=0)
-
-  def __next__(self) -> Mapping[str, np.ndarray]:
-    inds_source = self.rng.choice(self.n_source, size=[self.batch_size])
-    inds_target = self.rng.choice(self.n_target, size=[self.batch_size])
-    return {
-        "source_lin":
-            self.source_lin[inds_source, :]
-            if self.source_lin is not None else None,
-        "source_quad":
-            self.source_quad[inds_source, :]
-            if self.source_quad is not None else None,
-        "target_lin":
-            self.target_lin[inds_target, :]
-            if self.target_lin is not None else None,
-        "target_quad":
-            self.target_quad[inds_target, :]
-            if self.target_quad is not None else None,
-        "source_conditions":
-            self.source_conditions[inds_source, :]
-            if self.source_conditions is not None else None,
-        "target_conditions":
-            self.target_conditions[inds_target, :]
-            if self.target_conditions is not None else None,
-    }
+  return OTDataLoader(source, target, 16, source_conditions, target_conditions)
 
 
 @pytest.fixture(scope="module")
@@ -170,7 +49,7 @@ def genot_data_loader_linear():
   rng = np.random.default_rng(seed=0)
   source = rng.normal(size=(100, 2))
   target = rng.normal(size=(100, 2)) + 1.0
-  return GENOTDataLoader(16, source_lin=source, target_lin=target)
+  return OTDataLoader(16, source_lin=source, target_lin=target)
 
 
 @pytest.fixture(scope="module")
@@ -181,7 +60,7 @@ def genot_data_loader_linear_conditional():
   target = rng.normal(size=(100, 2)) + 1.0
   conditions_source = rng.normal(size=(100, 4))
   conditions_target = rng.normal(size=(100, 4)) - 1.0
-  return GENOTDataLoader(
+  return OTDataLoader(
       16,
       source_lin=source,
       target_lin=target,
@@ -196,7 +75,7 @@ def genot_data_loader_quad():
   rng = np.random.default_rng(seed=0)
   source = rng.normal(size=(100, 2))
   target = rng.normal(size=(100, 1)) + 1.0
-  return GENOTDataLoader(16, source_quad=source, target_quad=target)
+  return OTDataLoader(16, source_quad=source, target_quad=target)
 
 
 @pytest.fixture(scope="module")
@@ -206,7 +85,7 @@ def genot_data_loader_quad_conditional():
   source = rng.normal(size=(100, 2))
   target = rng.normal(size=(100, 1)) + 1.0
   conditions = rng.normal(size=(100, 7))
-  return GENOTDataLoader(
+  return OTDataLoader(
       16,
       source_quad=source,
       target_quad=target,
@@ -223,7 +102,7 @@ def genot_data_loader_fused():
   target_q = rng.normal(size=(100, 1)) + 1.0
   source_lin = rng.normal(size=(100, 2))
   target_lin = rng.normal(size=(100, 2)) + 1.0
-  return GENOTDataLoader(
+  return OTDataLoader(
       16,
       source_lin=source_lin,
       source_quad=source_q,
@@ -241,7 +120,7 @@ def genot_data_loader_fused_conditional():
   source_lin = rng.normal(size=(100, 2))
   target_lin = rng.normal(size=(100, 2)) + 1.0
   conditions = rng.normal(size=(100, 7))
-  return GENOTDataLoader(
+  return OTDataLoader(
       16,
       source_lin=source_lin,
       source_quad=source_q,
