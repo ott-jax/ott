542c05bd799b42b4ce57f6271031f52a4fa8a3a5
Michal Klein <46717574+michalk8@users.noreply.github.com>
2023-12-21 17:17:07 +0100

    Add test skeletons

---
+++ tests/geometry/lr_kernel_test.py
@@ -0,0 +1,18 @@
+import jax
+import pytest
+
+
+@pytest.mark.fast()
+class TestLRCGeometry:
+
+  def test_positive_features(self, rng: jax.Array):
+    pass
+
+  def test_gaussian_approximation(self, rng: jax.Array):
+    pass
+
+  def test_arccos_approximation(self, rng: jax.Array):
+    pass
+
+  def test_sinkhorn_differiability(self, rng: jax.Array):
+    pass
