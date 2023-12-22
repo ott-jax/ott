5681245325d7c604c14f0f3996f7e4aeed1d16ee
Michal Klein <46717574+michalk8@users.noreply.github.com>
2023-12-22 02:38:25 +0100

    Fix LRK tests

--- tests/geometry/lr_kernel_test.py
+++ tests/geometry/lr_kernel_test.py
@@ -3,7 +3,7 @@ from typing import Literal
 import jax
 import numpy as np
 import pytest
-from ott.geometry import low_rank, pointcloud
+from ott.geometry import costs, low_rank, pointcloud
 
 
 @pytest.mark.fast()
@@ -23,34 +23,38 @@ class TestLRCGeometry:
         x, y, kernel=kernel, std=std, rank=rank
     )
 
-    assert geom.rank == rank
+    if kernel == "gaussian":
+      assert geom.rank == rank
+    else:
+      assert geom.rank == rank + 1
     np.testing.assert_array_equal(geom.k1 >= 0.0, True)
     np.testing.assert_array_equal(geom.k2 >= 0.0, True)
 
-  @pytest.mark.parametrize("eps", [1e-3, 1e-2, 1e-1, 5e-1, 1.0])
-  def test_gaussian_approximation(self, rng: jax.Array, eps: float):
+  @pytest.mark.parametrize("std", [1e-2, 1e-1, 5e-1, 1.0])
+  @pytest.mark.parametrize("kernel", ["gaussian", "arccos"])
+  def test_kernel_approximation(
+      self, rng: jax.Array, kernel: Literal["gaussian", "arccos"], std: float
+  ):
     rng, rng1, rng2 = jax.random.split(rng, 3)
+    s = 1
     x = jax.random.normal(rng1, (230, 5))
     y = jax.random.normal(rng2, (260, 5))
 
-    pc = pointcloud.PointCloud(x, y, epsilon=eps, scale_cost=1.0)
+    cost_fn = costs.SqEuclidean() if kernel == "gaussian" else costs.Arccos(s)
+    pc = pointcloud.PointCloud(x, y, epsilon=std, cost_fn=cost_fn)
     gt_cost = pc.cost_matrix
 
     max_abs_diff = []
-    for rank in [25, 50, 100, 200]:
+    for rank in [10, 50, 100, 200]:
       rng, rng_approx = jax.random.split(rng, 2)
       geom = low_rank.LRKGeometry.from_pointcloud(
-          x, y, rank=rank, kernel="gaussian", std=eps, rng=rng_approx
+          x, y, rank=rank, kernel=kernel, std=std, s=s, rng=rng_approx
       )
       pred_cost = geom.cost_matrix
-
       max_abs_diff.append(np.max(np.abs(gt_cost - pred_cost)))
 
     # test that higher rank better approximates the cost
     np.testing.assert_array_equal(np.diff(max_abs_diff) <= 0.0, True)
-
-  def test_arccos_approximation(self, rng: jax.Array):
-    pass
 
   def test_sinkhorn_approximation(self, rng: jax.Array):
     pass
