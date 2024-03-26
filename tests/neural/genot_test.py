9b89fd7f8a934135f09f8ffad658b26a10bb36a0
Michal Klein <46717574+michalk8@users.noreply.github.com>
2024-03-26 18:23:53 +0100

    Simplify GENOT test

--- tests/neural/genot_test.py
+++ tests/neural/genot_test.py
@@ -12,12 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import functools
-from typing import Literal
+from typing import Literal, Optional
 
 import pytest
 
 import jax
 import jax.numpy as jnp
+import jax.tree_util as jtu
 
 import optax
 
@@ -25,54 +26,35 @@ from ott.neural.flow_models import flows, genot, models, utils
 
 
 def data_match_fn(
-    src_lin: jnp.ndarray, tgt_lin: jnp.ndarray, src_quad: jnp.ndarray,
-    tgt_quad: jnp.ndarray, *, type: Literal["linear", "quadratic", "fused"]
-):
-  if type == "linear":
+    src_lin: Optional[jnp.ndarray], tgt_lin: Optional[jnp.ndarray],
+    src_quad: Optional[jnp.ndarray], tgt_quad: Optional[jnp.ndarray], *,
+    typ: Literal["lin", "quad", "fused"]
+) -> jnp.ndarray:
+  if typ == "lin":
     return utils.match_linear(x=src_lin, y=tgt_lin)
-  if type == "quadratic":
+  if typ == "quad":
     return utils.match_quadratic(xx=src_quad, yy=tgt_quad)
-  if type == "fused":
+  if typ == "fused":
     return utils.match_quadratic(xx=src_quad, yy=tgt_quad, x=src_lin, y=tgt_lin)
-  raise NotImplementedError(f"Unknown type: {type}")
+  raise NotImplementedError(f"Unknown type: {typ}.")
 
 
 class TestGENOT:
 
-  @pytest.mark.parametrize(
-      "dl", [
-          "lin_dl", "conditional_lin_dl", "quad_dl", "conditional_quad_dl",
-          "fused_dl", "conditional_fused_dl"
-      ]
-  )
+  # TODO(michalk8): add conds
+  @pytest.mark.parametrize("dl", ["lin_dl", "quad_dl", "fused_dl"])
   def test_genot(self, rng: jax.Array, dl: str, request):
-    rng_init, rng_call = jax.random.split(rng, 2)
+    rng_init, rng_call, rng_data = jax.random.split(rng, 3)
+    problem_type = dl.split("_")[0]
     dl = request.getfixturevalue(dl)
 
     batch = next(iter(dl))
-    src_lin = batch.get("src_lin")
-    if src_lin is not None:
-      src_lin = jnp.asarray(src_lin)
-    src_quad = batch.get("src_quad")
-    if src_quad is not None:
-      src_quad = jnp.asarray(src_quad)
-    tgt_lin = batch.get("tgt_lin")
-    if tgt_lin is not None:
-      tgt_lin = jnp.asarray(batch["tgt_lin"])
-    tgt_quad = batch.get("tgt_quad")
-    if tgt_quad is not None:
-      tgt_quad = jnp.asarray(batch["tgt_quad"])
+    batch = jtu.tree_map(jnp.asarray, batch)
     src_cond = batch.get("src_condition")
-    if src_cond is not None:
-      src_cond = jnp.asarray(src_cond)
 
-    src_lin_dim = src_lin.shape[-1] if src_lin is not None else 0
-    src_quad_dim = src_quad.shape[-1] if src_quad is not None else 0
-    tgt_lin_shape = tgt_lin.shape[-1] if tgt_lin is not None else 0
-    tgt_quad_shape = tgt_quad.shape[-1] if tgt_quad is not None else 0
-    src_dim = src_lin_dim + src_quad_dim
-    tgt_dim = tgt_lin_shape + tgt_quad_shape
-    cond_dim = src_cond.shape[-1] if src_cond is not None else 0
+    dims = jtu.tree_map(lambda x: x.shape[-1], batch)
+    src_dim = dims.get("src_lin", 0) + dims.get("src_quad", 0)
+    tgt_dim = dims.get("tgt_lin", 0) + dims.get("tgt_quad", 0)
 
     vf = models.VelocityField(
         tgt_dim,
@@ -80,31 +62,20 @@ class TestGENOT:
         condition_dims=[7, 7, 7],
     )
 
-    if src_lin_dim > 0 and src_quad_dim == 0:
-      problem_type = "linear"
-    elif src_lin_dim == 0 and src_quad_dim > 0:
-      problem_type = "quadratic"
-    elif src_lin_dim > 0 and src_quad_dim > 0:
-      problem_type = "fused"
-    else:
-      raise ValueError("Unknown problem type")
-
-    data_mfn = functools.partial(data_match_fn, type=problem_type)
-
     model = genot.GENOT(
         vf,
         flow=flows.ConstantNoiseFlow(0.0),
-        data_match_fn=data_mfn,
+        data_match_fn=functools.partial(data_match_fn, typ=problem_type),
         source_dim=src_dim,
         target_dim=tgt_dim,
-        condition_dim=cond_dim,
+        condition_dim=None if src_cond is None else src_cond.shape[-1],
         rng=rng_init,
         optimizer=optax.adam(learning_rate=1e-4),
     )
 
     _logs = model(dl, n_iters=3, rng=rng_call)
-    src_terms = [term for term in [src_lin, src_quad] if term is not None]
-    src = jnp.concatenate(src_terms, axis=-1)
+
+    src = jax.random.normal(rng_data, (3, src_dim))
     res = model.transport(src, condition=src_cond)
 
     assert jnp.sum(jnp.isnan(res)) == 0
