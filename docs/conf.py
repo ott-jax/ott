05f7ebb04d13f2a9b890dca3f3bb27acd1078a48
Marco Cuturi <marcocuturicameto@gmail.com>
2024-04-26 15:58:15 +0200

    Update icnn.py (#524)
    * Update icnn.py
    
    add bibliographic ref
    
    * Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -130,6 +130,7 @@ linkcheck_ignore = [
     "https://doi.org/10.1145/2516971.2516977",
     "https://doi.org/10.1145/2766963",
     "https://keras.io/examples/nlp/pretrained_word_embeddings/",
+    "https://www.jstor.org/stable/3647580",
 ]
 
 # List of patterns, relative to source directory, that match files and
