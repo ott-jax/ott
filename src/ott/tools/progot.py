8ebb7015b6b2c7aec95dcf6432b4a130c70290d7
Michal Klein <46717574+michalk8@users.noreply.github.com>
2024-08-06 20:31:20 +0200

    Update more docs

--- src/ott/tools/progot.py
+++ src/ott/tools/progot.py
@@ -38,14 +38,14 @@ class ProgOTState(NamedTuple):
 
 
 class ProgOTOutput(NamedTuple):
-  """:class:`ProgOT` solver output.
+  """Output of the :class:`ProgOT` solver.
 
   Args:
     prob: Linear problem.
     alphas: Stepsize schedule of shape ``[num_steps,]``.
     epsilons: Entropy regularization of shape ``[num_steps,]``.
     outputs: Solver outputs at every step.
-    xs: Intermediate interpolations of shape ``[num_steps, n, d]``.
+    xs: Intermediate interpolations of shape ``[num_steps, n, d]``, if present.
   """
   prob: linear_problem.LinearProblem
   alphas: jnp.ndarray
@@ -100,10 +100,10 @@ class ProgOTOutput(NamedTuple):
     r"""Get the solver output at a specific step.
 
     Args:
-      step: Iteration step in :math:`[0, num_steps)`.
+      step: Iteration step in :math:`[0, \text{num_steps})`.
 
     Returns:
-      The output.
+      The solver output at ``step``.
     """
     return jtu.tree_map(lambda x: x[step], self.outputs)
 
@@ -111,20 +111,22 @@ class ProgOTOutput(NamedTuple):
   def converged(
       self
   ) -> Union[jnp.ndarray, Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]]:
-    """Converged flag at each step.
+    """Convergence at each step.
 
-    If :attr`is_debiased`, return an array of shape ``[num_steps, 3]`` with
-    values corresponding to ``(x, y)``, ``(x, x)`` and ``(y, y)`` problems.
+    If :attr:`is_debiased`, return an array of shape ``[num_steps, 3]`` with
+    values corresponding to the convergence of the  ``(x, y)``, ``(x, x)`` and
+    ``(y, y)`` problems.
     Otherwise, return an array of shape ``[num_steps,]``.
     """
-    return jnp.stack(self.outputs.converged, axis=1)
+    return jnp.stack(self.outputs.converged, axis=-1)
 
   @property
   def num_iters(self) -> jnp.ndarray:
-    """Number of iterations at each steps.
+    """Number of iterations at each step.
 
-    If :attr`is_debiased`, return an array of shape ``[num_steps, 3]`` with
-    values corresponding to ``(x, y)``, ``(x, x)`` and ``(y, y)`` problems.
+    If :attr:`is_debiased`, return an array of shape ``[num_steps, 3]`` with
+    values corresponding to the number of iterations for the ``(x, y)``,
+    ``(x, x)`` and ``(y, y)`` problems.
     Otherwise, return an array of shape ``[num_steps,]``.
     """
     return jnp.array([
@@ -144,13 +146,15 @@ class ProgOTOutput(NamedTuple):
 
 @jtu.register_pytree_node_class
 class ProgOT:
-  """Progressive Entropic Optimal Transport :cite:`kassraie:24`.
+  """Progressive Entropic Optimal Transport solver :cite:`kassraie:24`.
 
   Args:
-    alphas: Stepsize schedule.
-    epsilons: Epsilon regularization schedule. If :obj:`None`, use the default
-      epsilon at each step.
-    epsilon_scales: TODO.
+    alphas: Stepsize schedule of shape ``[num_steps,]``.
+    epsilons: Epsilon regularization schedule of shape ``[num_steps,]``.
+      If :obj:`None`, use the default epsilon at each step.
+    epsilon_scales: Scale for the default epsilon of shape ``[num_steps,]``.
+      If :obj:`None`, don't scale the epsilons. Note that only one of
+      ``epsilons`` and ``epsilon_scales`` can be passed.
     is_debiased: Whether to use
       :func:`~ott.tools.sinkhorn_divergence.sinkhorn_divergence` or
       :class:`~ott.solvers.linear.sinkhorn.Sinkhorn`.
@@ -165,10 +169,12 @@ class ProgOT:
       is_debiased: bool = False,
   ):
     if epsilons is not None:
+      assert epsilon_scales is None, "TODO"
       assert len(alphas) == len(
           epsilons
       ), "Epsilons have different length than alphas."
     if epsilon_scales is not None:
+      assert epsilons is None, "TODO"
       assert len(alphas) == len(
           epsilon_scales
       ), "Epsilon scales have different length than alphas."
@@ -185,7 +191,7 @@ class ProgOT:
       warm_start: bool = False,
       **kwargs: Any,
   ) -> ProgOTOutput:
-    """Run the estimator.
+    """Run the solver.
 
     Args:
       prob: Linear problem.
@@ -194,10 +200,10 @@ class ProgOT:
       kwargs: Keyword arguments for
         :class:`~ott.solvers.linear.sinkhorn.Sinkhorn` or
         :func:`~ott.tools.sinkhorn_divergence.sinkhorn_divergence`, depending
-        on :attr:`debiased`.
+        on :attr:`is_debiased`.
 
     Returns:
-      The output.
+      The solver output.
     """
 
     def body_fn(
@@ -213,7 +219,7 @@ class ProgOT:
       if self.is_debiased:
         assert state.init_potentials == (
             None, None
-        ), "Warm start is not implemented for debiased."
+        ), "Warm star is not implemented for debiased."
         out = _sinkhorn_divergence(
             state.x, y, cost_fn=cost_fn, eps=eps, **kwargs
         )
@@ -282,6 +288,99 @@ class ProgOT:
     return cls(alphas=alphas, epsilons=epsilons, **aux_data)
 
 
+def get_epsilon_schedule(
+    geom: pointcloud.PointCloud,
+    *,
+    alphas: jnp.ndarray,
+    epsilon_scales: jnp.ndarray,
+    y_eval: jnp.ndarray,
+    start_epsilon_scale: float = 1.0,
+    **kwargs: Any,
+) -> jnp.ndarray:
+  """Get the epsilon regularization schedule.
+
+  See Algorithm 4 in :cite:`kassraie:24` for more information.
+
+  Args:
+    geom: Point cloud geometry.
+    alphas: Stepsize schedule of shape ``[num_steps,]``.
+    epsilon_scales: scales of shape ``[num_,]`` used to scale
+      the TODO.
+    y_eval: Array of shape ``[k, d]`` from the target distribution used to
+      compute the error.
+    start_epsilon_scale: Constant by which to scale the initial epsilon.
+    kwargs: Keyword arguments for
+      :class:`~ott.solvers.linear.sinkhorn.Sinkhorn`.
+
+  Returns:
+    The epsilon regularization schedule of shape ``[num_steps,]``.
+  """
+
+  def error(epsilon_scale: float) -> float:
+    epsilon = epsilon_scale * geom_end.epsilon
+
+    out = _sinkhorn(y, y, cost_fn=cost_fn, eps=epsilon, **kwargs)
+    dp = out.to_dual_potentials()
+    y_hat = dp.transport(y_eval, forward=True)
+
+    return jnp.linalg.norm(y_eval - y_hat)
+
+  y, cost_fn = geom.y, geom.cost_fn
+
+  start_eps = start_epsilon_scale * geom.epsilon
+
+  geom_end = pointcloud.PointCloud(y, y, cost_fn=cost_fn)
+  errors = jax.vmap(error)(epsilon_scales)
+  end_epsilon = epsilon_scales[jnp.argmin(errors)] * geom_end.epsilon
+
+  mod_alpha = jnp.concatenate([jnp.array([0.0]), alphas])
+  no_ending_1 = mod_alpha[-1] != 1.0  # e.g. the exp schedule
+  # TODO(michalk8): not jittable
+  if no_ending_1:
+    mod_alpha = jnp.concatenate([mod_alpha, jnp.array([1.0])])
+
+  tk = 1.0 - jnp.cumprod(1.0 - mod_alpha)
+  epsilons = end_epsilon * tk + (1.0 - tk) * start_eps
+
+  epsilons = epsilons[:-1]
+  if no_ending_1:
+    epsilons = epsilons[:-1]
+
+  return epsilons
+
+
+def get_alpha_schedule(
+    kind: Literal["lin", "exp", "quad"], *, num_steps: int
+) -> jnp.ndarray:
+  """Get the stepsize schedule.
+
+  See Section 4 in :cite:`kassraie:24` for more information.
+
+  Args:
+    kind: The schedule to create:
+
+      - ``'lin'`` - constant speed schedule.
+      - ``'exp'`` - decelerating schedule.
+      - ``'quad'`` - accelerating schedule.
+    num_steps: Total number of steps.
+
+  Returns:
+    The stepsize schedule, array of shape ``[num_steps,]``.
+  """
+  if kind == "lin":
+    arr = jnp.arange(2, num_steps + 2)
+    arr = 1.0 / (num_steps - arr + 2)
+  elif kind == "exp":
+    arr = jnp.full(num_steps, fill_value=1.0 / jnp.e)
+  elif kind == "quad":
+    arr = jnp.arange(2, num_steps + 2)
+    arr = (2.0 * arr - 1.0) / ((num_steps + 1) ** 2 - (arr - 1) ** 2)
+  else:
+    raise ValueError(f"Invalid stepsize schedule `{kind}`.")
+
+  return arr
+
+
 def _sinkhorn(
     x: jnp.ndarray,
     y: jnp.ndarray,
@@ -312,75 +411,6 @@ def _sinkhorn_divergence(
       share_epsilon=False,
       sinkhorn_kwargs=kwargs,
   )
-
-
-def get_epsilon_schedule(
-    geom: pointcloud.PointCloud,
-    *,
-    alphas: jnp.ndarray,
-    epsilon_scales: jnp.ndarray,
-    y_eval: jnp.ndarray,
-    start_epsilon_scale: float = 1.0,
-    **kwargs: Any,
-) -> jnp.ndarray:
-  """TODO."""
-
-  def error(epsilon_scale: float) -> float:
-    epsilon = epsilon_scale * geom_end.epsilon
-
-    out = _sinkhorn(y, y, cost_fn=cost_fn, eps=epsilon, **kwargs)
-    dp = out.to_dual_potentials()
-    y_hat = dp.transport(y_eval, forward=True)
-
-    return jnp.linalg.norm(y_eval - y_hat)
-
-  y, cost_fn = geom.y, geom.cost_fn
-
-  start_eps = start_epsilon_scale * geom.epsilon
-
-  geom_end = pointcloud.PointCloud(y, y, cost_fn=cost_fn)
-  errors = jax.vmap(error)(epsilon_scales)
-  end_epsilon = epsilon_scales[jnp.argmin(errors)] * geom_end.epsilon
-
-  mod_alpha = jnp.concatenate([jnp.array([0.0]), alphas])
-  no_ending_1 = mod_alpha[-1] != 1.0  # e.g. the exp schedule
-  if no_ending_1:
-    mod_alpha = jnp.concatenate([mod_alpha, jnp.array([1.0])])
-
-  tk = 1.0 - jnp.cumprod(1.0 - mod_alpha)
-  epsilons = end_epsilon * tk + (1.0 - tk) * start_eps
-
-  epsilons = epsilons[:-1]
-  if no_ending_1:
-    epsilons = epsilons[:-1]
-
-  return epsilons
-
-
-def get_alpha_schedule(
-    kind: Literal["lin", "exp", "quad"], *, num_steps: int
-) -> jnp.ndarray:
-  """Get the stepsize schedule.
-
-  Args:
-    kind: Kind of the schedule.
-    num_steps: Total number of steps.
-
-  Returns:
-    The schedule.
-  """
-  if kind == "lin":
-    arr = jnp.arange(2, num_steps + 2)
-    arr = 1.0 / (num_steps - arr + 2)
-  elif kind == "exp":
-    arr = jnp.full(num_steps, fill_value=1.0 / jnp.e)
-  elif kind == "quad":
-    arr = jnp.arange(2, num_steps + 2)
-    arr = (2.0 * arr - 1.0) / ((num_steps + 1) ** 2 - (arr - 1) ** 2)
-  else:
-    raise ValueError(kind)
-
-  return arr
 
 
 def _interpolate(
