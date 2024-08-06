55d779e4f004df32f94066d611228d6d17f76949
Michal Klein <46717574+michalk8@users.noreply.github.com>
2024-08-06 17:49:23 +0200

    Simplify API

--- src/ott/tools/progot.py
+++ src/ott/tools/progot.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Callable, Literal, NamedTuple, Optional, Union
+from typing import Any, Literal, NamedTuple, Optional, Tuple, Union
 
 import jax
 import jax.numpy as jnp
@@ -22,28 +22,10 @@ from ott.problems.linear import linear_problem, potentials
 from ott.solvers.linear import sinkhorn
 from ott.tools import sinkhorn_divergence as sd
 
-# TODO(michalk8): simplify
-Output = Union[sinkhorn.SinkhornOutput, sd.SinkhornDivergenceOutput]
-Initializer = tuple[Optional[jnp.ndarray], Optional[jnp.ndarray]]
-SolveFn = Callable[
-    [
-        jnp.ndarray,
-        jnp.ndarray,
-        costs.TICost,
-        Optional[float],
-        Optional[float],
-        Initializer,
-        Optional[float],
-    ],
-    Output,
-]
-
 __all__ = [
     "ProgOT",
     "ProgOTState",
     "ProgOTOutput",
-    "solve_sinkhorn",
-    "solve_sinkdiv",
     "get_epsilon_schedule",
     "get_alpha_schedule",
 ]
@@ -51,21 +33,21 @@ __all__ = [
 
 class ProgOTState(NamedTuple):
   x: jnp.ndarray  # [n, d]
-  xs: jnp.ndarray  # [k, n, d]
+  xs: Optional[jnp.ndarray]  # [k, n, d]
   epsilons: jnp.ndarray  # [k,]
   alphas: jnp.ndarray  # [k,]
-  init_potentials: Initializer
+  init_potentials: Tuple[Optional[jnp.ndarray], Optional[jnp.ndarray]]
 
   def set(self, **kwargs: Any) -> "ProgOTState":
     return self._replace(**kwargs)
 
 
-# TODO(michalk8): docs
 class ProgOTOutput(NamedTuple):
   prob: linear_problem.LinearProblem
   alphas: jnp.ndarray  # [k,]
   epsilons: jnp.ndarray  # [k,]
-  outputs: Output  # [k, Output]
+  outputs: Union[sinkhorn.SinkhornOutput,
+                 sd.SinkhornDivergenceOutput]  # [k, ...]
   xs: Optional[jnp.ndarray] = None  # [k, n, d]
 
   def transport(
@@ -100,13 +82,15 @@ class ProgOTOutput(NamedTuple):
   def get_entropic_map(self, it: int) -> potentials.EntropicPotentials:
     return self.get_output(it).to_dual_potentials()
 
-  def get_output(self, it: int) -> Output:
+  def get_output(
+      self, it: int
+  ) -> Union[sinkhorn.SinkhornOutput, sd.SinkhornDivergenceOutput]:
     return jtu.tree_map(lambda x: x[it], self.outputs)
 
   def get_plan(self, it: int) -> jnp.ndarray:
     out = self.get_output(it)
     if isinstance(out, sd.SinkhornDivergenceOutput):
-      out = _sink_out_from_debiased(out)
+      out = _sink_out_from_debiased(out, idx=0)
     return out.matrix
 
   @property
@@ -135,23 +119,23 @@ class ProgOT:
 
   def __init__(
       self,
-      # TODO(michalk8): pass `debiased` + kwargs?
-      solve_fn: SolveFn,
-      *,
       alphas: jnp.ndarray,
+      *,
       # if `None`, all epsilons will be `None`
-      epsilons: Optional[jnp.ndarray],
+      epsilons: Optional[jnp.ndarray] = None,
       epsilon_scales: Optional[jnp.ndarray] = None,
+      debiased: bool = False,
   ):
     if epsilons is not None:
       assert len(alphas) == len(
           epsilons
-      ), "Epsilon schedule has different length than alpha schedule."
+      ), "Epsilons have different length than alphas."
     if epsilon_scales is not None:
       assert len(alphas) == len(
           epsilon_scales
-      ), "Epsilon scales has different length than alpha schedule."
-    self.solve_fn = solve_fn
+      ), "Epsilon scales have different length than alphas."
+
+    self.debiased = debiased
     self.alphas = alphas
     self.epsilons = epsilons
     self.epsilon_scales = epsilon_scales
@@ -161,28 +145,33 @@ class ProgOT:
       prob: linear_problem.LinearProblem,
       store_intermediate: bool = False,
       warm_start: bool = False,
+      **kwargs: Any,
   ) -> ProgOTOutput:
 
     def body_fn(state: ProgOTState, it: int) -> tuple[ProgOTState, jnp.ndarray]:
-      # TODO(michalk8): check only one is being passed
-      eps = None if self.epsilons is None else self.epsilons[it]
-      eps_scale = None if self.epsilon_scales is None else self.epsilon_scales[
-          it]
       alpha = self.alphas[it]
-      thr = 1e-3  # TODO(michalk8)
+      eps = None if self.epsilons is None else self.epsilons[it]
+      if self.epsilon_scales is not None:
+        assert eps is None, "TODO"
+        geom = pointcloud.PointCloud(state.x, y, cost_fn=cost_fn)
+        eps = self.epsilon_scales[it] * geom.epsilon
 
-      out = self.solve_fn(
-          state.x, y, cost_fn, eps, eps_scale, state.init_potentials, thr
-      )
-
-      # TODO(michalk8): simplify
-      if is_debiased:
-        # out_xx = _sink_out_from_debiased(out, idx=1)
-        out_xy = _sink_out_from_debiased(out, idx=0)
-        eps = out_xy.geom.epsilon
+      if self.debiased:
+        assert init == (None, None), "TODO"
+        out = _sinkhorn_divergence(
+            state.x, y, cost_fn=cost_fn, eps=eps, **kwargs
+        )
+        eps = _sink_out_from_debiased(out, idx=0).geom.epsilon
       else:
-        # out_xx = None
-        out_xy, eps = out, out.geom.epsilon
+        out = _sinkhorn(
+            state.x,
+            y,
+            cost_fn=cost_fn,
+            eps=eps,
+            init=state.init_potentials,
+            **kwargs
+        )
+        eps = out.geom.epsilon
 
       t_x = out.to_dual_potentials().transport(state.x, forward=True)
       next_x = _interpolate(x=state.x, t_x=t_x, alpha=alpha, cost_fn=cost_fn)
@@ -200,26 +189,16 @@ class ProgOT:
 
       return next_state, out
 
-    assert isinstance(
-        prob.geom, pointcloud.PointCloud
-    ), "Geometry must be a point cloud."
-    assert isinstance(
-        prob.geom.cost_fn, costs.TICost
-    ), "Cost must be a TI cost."
+    lse_mode = kwargs.get("lse_mode", True)
 
     num_steps = len(self.alphas)
-
     n, m = prob.geom.shape
     x, y, cost_fn = prob.geom.x, prob.geom.y, prob.geom.cost_fn
     _, d = x.shape
 
-    tree = jax.eval_shape(self.solve_fn, x, y, cost_fn, 1.0, None, (None, None))
-    is_debiased = isinstance(tree, sd.SinkhornDivergenceOutput)
     if warm_start:
-      assert not is_debiased, "Warm-start is only available for Sinkhorn."
-      # FIXME(michalk8)
-      # assume always LSE mode for now
-      init_potentials = (jnp.zeros(n), jnp.zeros(m))
+      init_potentials = (jnp.zeros(n), jnp.zeros(m)
+                        ) if lse_mode else (jnp.ones(n), jnp.ones(m))
     else:
       init_potentials = (None, None)
 
@@ -246,59 +225,39 @@ class ProgOT:
     )
 
   def tree_flatten(self):
-    return [], {
-        "solve_fn": self.solve_fn,
+    return (self.epsilons,), {
+        "debiased": self.debiased,
         "alphas": self.alphas,
-        "epsilons": self.epsilons,
         "epsilon_scales": self.epsilon_scales,
     }
 
   @classmethod
   def tree_unflatten(cls, aux_data: dict[str, Any], children: Any) -> "ProgOT":
-    del children
-    return cls(**aux_data)
+    epsilons, = children
+    return cls(epsilons=epsilons, **aux_data)
 
 
-def solve_sinkhorn(
+def _sinkhorn(
     x: jnp.ndarray,
     y: jnp.ndarray,
     cost_fn: costs.TICost,
     eps: Optional[float],
-    eps_scale: Optional[float],
-    init: Initializer = (None, None),
-    thr: float = 1e-3,
+    init: Tuple[Optional[jnp.ndarray], Optional[jnp.ndarray]] = (None, None),
     **kwargs: Any,
 ) -> sinkhorn.SinkhornOutput:
-  if eps_scale is not None:
-    geom = pointcloud.PointCloud(x, y, cost_fn=cost_fn, epsilon=eps)
-    eps = eps_scale * geom.epsilon
-
   geom = pointcloud.PointCloud(x, y, cost_fn=cost_fn, epsilon=eps)
   prob = linear_problem.LinearProblem(geom)
-  solver = sinkhorn.Sinkhorn(threshold=thr, **kwargs)
+  solver = sinkhorn.Sinkhorn(**kwargs)
   return solver(prob, init=init)
 
 
-def solve_sinkdiv(
+def _sinkhorn_divergence(
     x: jnp.ndarray,
     y: jnp.ndarray,
     cost_fn: costs.TICost,
     eps: Optional[float],
-    eps_scale: Optional[float],
-    init: Initializer = (None, None),
-    thr: float = 1e-3,
-    sinkhorn_kwargs: Optional[dict[str, Any]] = None,
     **kwargs: Any,
 ) -> sd.SinkhornDivergenceOutput:
-  del init  # not implemented
-  if eps_scale is not None:
-    geom = pointcloud.PointCloud(x, y, cost_fn=cost_fn, epsilon=eps)
-    eps = eps_scale * geom.epsilon
-
-  if sinkhorn_kwargs is None:
-    sinkhorn_kwargs = {}
-  sinkhorn_kwargs["threshold"] = thr
-
   return sd.sinkhorn_divergence(
       pointcloud.PointCloud,
       x,
@@ -306,41 +265,36 @@ def solve_sinkdiv(
       cost_fn=cost_fn,
       epsilon=eps,
       share_epsilon=False,
-      sinkhorn_kwargs=sinkhorn_kwargs,
-      **kwargs,
+      sinkhorn_kwargs=kwargs,
   )
 
 
 def get_epsilon_schedule(
+    geom: pointcloud.PointCloud,
     *,
-    x: jnp.ndarray,
-    y: jnp.ndarray,
     alphas: jnp.ndarray,
     epsilon_scales: jnp.ndarray,
-    # TODO(michalk8): remove this
-    solve_fn: SolveFn,
-    eps0_multiplier: float = 1.0,
-    y_eval: Optional[jnp.ndarray] = None,
-    cost_fn: Optional[costs.TICost] = None,
+    y_eval: jnp.ndarray,
+    start_epsilon_scale: float = 1.0,
+    **kwargs: Any,
 ) -> jnp.ndarray:
 
-  def error(es: float) -> float:
-    init, epsilon = (None, None), None
-    dp = solve_fn(y, y, cost_fn, epsilon, es, init).to_dual_potentials()
+  def error(epsilon_scale: float) -> float:
+    epsilon = epsilon_scale * geom_end.epsilon
+
+    out = _sinkhorn(y, y, cost_fn=cost_fn, eps=epsilon, **kwargs)
+    dp = out.to_dual_potentials()
     y_hat = dp.transport(y_eval, forward=True)
+
     return jnp.linalg.norm(y_eval - y_hat)
 
-  if cost_fn is None:
-    cost_fn = costs.SqEuclidean()
+  y, cost_fn = geom.y, geom.cost_fn
 
-  geom0 = pointcloud.PointCloud(x, y, cost_fn=cost_fn)
-  geom1 = pointcloud.PointCloud(y, y, cost_fn=cost_fn)
+  start_eps = start_epsilon_scale * geom.epsilon
 
-  eps0 = eps0_multiplier * geom0.epsilon
-
+  geom_end = pointcloud.PointCloud(y, y, cost_fn=cost_fn)
   errors = jax.vmap(error)(epsilon_scales)
-  eps1_scale = epsilon_scales[jnp.argmin(errors)]
-  eps1 = eps1_scale * geom1.epsilon
+  end_epsilon = epsilon_scales[jnp.argmin(errors)] * geom_end.epsilon
 
   mod_alpha = jnp.concatenate([jnp.array([0.0]), alphas])
   no_ending_1 = mod_alpha[-1] != 1.0  # e.g. the exp schedule
@@ -348,7 +302,7 @@ def get_epsilon_schedule(
     mod_alpha = jnp.concatenate([mod_alpha, jnp.array([1.0])])
 
   tk = 1.0 - jnp.cumprod(1.0 - mod_alpha)
-  epsilons = eps1 * tk + (1.0 - tk) * eps0
+  epsilons = end_epsilon * tk + (1.0 - tk) * start_eps
 
   epsilons = epsilons[:-1]
   if no_ending_1:
@@ -368,8 +322,6 @@ def get_alpha_schedule(
   elif kind == "quad":
     arr = jnp.arange(2, num_steps + 2)
     arr = (2.0 * arr - 1.0) / ((num_steps + 1) ** 2 - (arr - 1) ** 2)
-  elif kind == "exp2":
-    arr = jnp.full(num_steps, fill_value=0.5)
   else:
     raise ValueError(kind)
 
