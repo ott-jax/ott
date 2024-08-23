3efe83422669f7a9875b6688c5ecd2343cb116a0
Michal Klein <46717574+michalk8@users.noreply.github.com>
2024-08-23 14:51:27 +0200

    Cleaner docs

--- src/ott/geometry/costs.py
+++ src/ott/geometry/costs.py
@@ -398,6 +398,7 @@ class RegTICost(TICost):
   Args:
     regularizer: Regularization function.
     lam: Strength of the regularization.
+    rho: Scale of the quadratic part.
   """
 
   def __init__(
@@ -513,7 +514,7 @@ class RegTICost(TICost):
 
   @property
   def rho(self) -> float:
-    r"""Strength of the quadratic part \frac{1}{2}\|\cdot\|_2^2."""
+    r"""Scale of the quadratic part \frac{\rho}{2}\|\cdot\|_2^2."""
     return self._h.rho
 
   def tree_flatten(self):  # noqa: D102
