71e908c941c2f55eda7907c7bb82027f74073ec1
Michal Klein <46717574+michalk8@users.noreply.github.com>
2024-10-02 17:44:56 +0200

    Update `quadratic.solve`

--- src/ott/solvers/was_solver.py
+++ src/ott/solvers/was_solver.py
@@ -11,17 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Dict, Sequence, Tuple, Union
+from typing import TYPE_CHECKING, Any, Dict, Sequence, Tuple, Union
 
 import jax
 import jax.numpy as jnp
 
-from ott.solvers.linear import continuous_barycenter, sinkhorn, sinkhorn_lr
+from ott.solvers.linear import sinkhorn, sinkhorn_lr
+
+if TYPE_CHECKING:
+  from ott.solvers.linear import continuous_barycenter
 
 __all__ = ["WassersteinSolver"]
 
 State = Union[sinkhorn.SinkhornState, sinkhorn_lr.LRSinkhornState,
-              continuous_barycenter.FreeBarycenterState]
+              "continuous_barycenter.FreeBarycenterState"]
 
 
 @jax.tree_util.register_pytree_node_class
