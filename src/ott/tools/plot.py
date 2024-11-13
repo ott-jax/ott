b70e9aba210f6b5a085634dde3c55fd3d42a0fe7
marcocuturi <marcocuturicameto@gmail.com>
2024-11-13 22:43:02 +0100

    add terms in point cloud demo, fixes

--- src/ott/tools/plot.py
+++ src/ott/tools/plot.py
@@ -249,7 +249,7 @@ class Plot:
     """Make an animation from several transports."""
     _ = self(transports[0])
     if titles is None:
-      titles = ["tt" for _ in np.arange(0, len(transports))]
+      titles = [None for _ in np.arange(0, len(transports))]
     assert len(titles) == len(transports), (
         f"titles/transports lengths differ `{len(titles)}`/`{len(transports)}`."
     )
