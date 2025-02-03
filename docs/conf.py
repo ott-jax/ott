a4cbf8a5fb1f8840615d09125afff0df087858ca
Michal Klein <46717574+michalk8@users.noreply.github.com>
2025-02-03 16:42:58 +0100

    [ci skip] Fix JMLR link

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,6 @@ linkcheck_ignore = [
     "https://keras.io/examples/nlp/pretrained_word_embeddings/",
     "https://proceedings.neurips.cc/",
     "https://www.gnu.org/software/gsl/doc/html/statistics.html#weighted-samples",
-    "https://www.jmlr.org/papers/volume23/19-843/19-843.pdf"
 ]
 linkcheck_report_timeouts_as_broken = False
 
