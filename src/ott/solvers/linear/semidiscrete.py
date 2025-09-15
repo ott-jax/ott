Fix gradient computation when `b` is not uniform

src/ott/solvers/linear/semidiscrete.py
@@ -375,7 +375,8 @@ def _semidiscrete_loss(
     is_soft: bool,
 ) -> jax.Array:
   f, _ = _soft_c_transform(g, prob) if is_soft else _hard_c_transform(g, prob)
-  return -jnp.mean(f) - jnp.dot(g, prob.b)
+  # we assume uniform weights for `prob.a`
+  return -(jnp.mean(f) + jnp.dot(g, prob.b))
 
 
 def _semidiscrete_loss_fwd(
@@ -384,7 +385,8 @@ def _semidiscrete_loss_fwd(
     is_soft: bool,
 ) -> Tuple[jax.Array, Tuple[jax.Array, linear_problem.LinearProblem]]:
   f, z = _soft_c_transform(g, prob) if is_soft else _hard_c_transform(g, prob)
-  return -jnp.mean(f) - jnp.dot(g, prob.b), (z, prob)
+  # we assume uniform weights for `prob.a`
+  return -(jnp.mean(f) + jnp.dot(g, prob.b)), (z, prob)
 
 
 def _semidiscrete_loss_bwd(
@@ -395,17 +397,30 @@ def _semidiscrete_loss_bwd(
   z, prob = res
   n, m = prob.geom.shape
   if is_soft:
-    grad = jsp.special.softmax(z, axis=-1).sum(0)
+    b = None if prob._b is None else prob.b[None, :]
+    grad = _weighted_softmax(z, b, axis=-1).sum(0)
   else:
     ixs = jnp.argmax(z, axis=-1)
     grad = jax.ops.segment_sum(
         jnp.ones(n, dtype=z.dtype), segment_ids=ixs, num_segments=m
     )
   assert grad.shape == (m,), (grad.shape, (m,))
-  # TODO(michalk8): double-check
   grad = grad * (1.0 / n) - prob.b
-  grad = jnp.where(prob.b > 0.0, grad, 0.0)
   return g * grad, None
+
+
+def _weighted_softmax(
+    x: jax.Array, b: Optional[jax.Array], axis: int = -1
+) -> jax.Array:
+  if b is None:
+    return jsp.special.softmax(x, axis=axis)
+  where = b > 0.0
+  x_max = jnp.max(x, axis=axis, keepdims=True, where=where, initial=-jnp.inf)
+  unnormalized = b * jnp.exp(x - x_max)
+  softmax = unnormalized / unnormalized.sum(
+      axis=axis, keepdims=True, where=where
+  )
+  return jnp.where(where, softmax, 0.0)
 
 
 _semidiscrete_loss.defvjp(_semidiscrete_loss_fwd, _semidiscrete_loss_bwd)

tests/solvers/linear/semidiscrete_test.py
@@ -42,7 +42,7 @@ def _random_problem(
 class TestSemidiscreteSolver:
 
   @pytest.mark.parametrize("n", [20, 31])
-  @pytest.mark.parametrize("epsilon", [None, 0.01, 0.1])
+  @pytest.mark.parametrize("epsilon", [0.0, 1e-3, 1e-2, 1e-1, None])
   def test_c_transform_gradient(
       self, rng: jax.Array, n: int, epsilon: Optional[float]
   ):
@@ -57,11 +57,11 @@ class TestSemidiscreteSolver:
       return -jnp.mean(f) - jnp.dot(g, prob.b)
 
     rng_prob, rng_potential, rng_sample = jr.split(rng, 3)
-    m, d = 7, 5
-    b = jnp.full((m,), fill_value=1.0 / (m - 2))
-    # TODO(michalk8): this causes the test to fail
+    m, d = 17, 5
+    b = jr.uniform(rng, (m,))
     b = b.at[np.array([0, 2])].set(0.0)
-    prob = _random_problem(rng, m=m, d=d, b=b)
+    b /= b.sum()
+    prob = _random_problem(rng, m=m, d=d, b=b, epsilon=epsilon)
 
     g = jr.normal(rng_potential, (m,))
     sampled_prob = prob.sample(rng_sample, n)
@@ -76,10 +76,12 @@ class TestSemidiscreteSolver:
     )
 
     gt_val, gt_grad_g = gt_fn(g, sampled_prob, is_soft)
+    # for low epsilon, where `b=0`, this can be NaN
+    gt_grad_g = jnp.where(jnp.isnan(gt_grad_g), 0.0, gt_grad_g)
     prev_val, pred_grad_g = pred_fn(g, sampled_prob, is_soft)
 
-    np.testing.assert_allclose(prev_val, gt_val, rtol=1e-6, atol=1e-6)
-    np.testing.assert_allclose(pred_grad_g, gt_grad_g, rtol=1e-6, atol=1e-6)
+    np.testing.assert_allclose(prev_val, gt_val, rtol=1e-5, atol=1e-5)
+    np.testing.assert_allclose(pred_grad_g, gt_grad_g, rtol=1e-4, atol=1e-4)
 
   def test_dtype(self):
     pass