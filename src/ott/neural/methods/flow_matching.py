3eba76342d1fa026664a19d7dd89b52e5a24471d
Michal Klein <46717574+michalk8@users.noreply.github.com>
2025-10-16 15:06:21 +0200

    Add EMA

--- src/ott/neural/methods/flow_matching.py
+++ src/ott/neural/methods/flow_matching.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Callable, Dict, Literal
+from typing import Callable, Dict, Literal, Optional
 
 import jax
 
@@ -22,11 +22,13 @@ __all__ = ["flow_matching_step"]
 
 
 def flow_matching_step(
-    rngs: nnx.Rngs,
     model: nnx.Module,
     optimizer: nnx.Optimizer,
     batch: Dict[Literal["t", "x_t", "v_t", "cond"], jax.Array],
+    *,
     loss_fn: Callable[[jax.Array, jax.Array], jax.Array] = optax.squared_error,
+    ema_update_fn: Optional[Callable[[nnx.Module], None]] = None,
+    dropout_rngs: nnx.Rngs,
 ) -> Dict[Literal["loss", "grad_norm"], jax.Array]:
   """TODO."""
 
@@ -36,12 +38,20 @@ def flow_matching_step(
     v_pred = model(t, x_t, cond, rngs=rngs)
     return loss_fn(v_pred, v_t).mean()
 
-  loss, grads = nnx.value_and_grad(compute_loss)(model, rngs)
+  loss, grads = nnx.value_and_grad(compute_loss)(model, dropout_rngs)
   optimizer.update(model, grads)
+  if ema_update_fn is not None:
+    ema_update_fn(model)
+
   grad_norm = optax.global_norm(grads)
+
   return {"loss": loss, "grad_norm": grad_norm}
 
 
-def evaluate_velocity_field():
+def evaluate_velocity_field(
+    model: nnx.Module,
+    x0: jax.Array,
+    cond: Optional[jax.Array] = None,
+):
   """TODO."""
   pass
---
+++ src/ott/neural/networks/velocity_field/ema.py
@@ -0,0 +1,36 @@
+import jax.numpy as jnp
+import jax.tree_util as jtu
+
+from flax import nnx
+
+__all__ = ["EMA"]
+
+
+class EMA(nnx.Module):
+
+  def __init__(self, net: nnx.Module, *, decay: float):
+    super().__init__()
+    self.ema = init_ema(net)
+    self.decay = decay
+
+  def update(self, net: nnx.Module) -> None:
+    update_ema(net, self.ema, decay=self.decay)
+
+
+def init_ema(net: nnx.Module) -> nnx.Module:
+  graphdef, state, rest = nnx.split(net, nnx.Param, ...)
+  ema_state = jtu.tree_map(jnp.zeros_like, state)
+  rest = jtu.tree_map(lambda r: r.copy(), rest)
+  return nnx.merge(graphdef, ema_state, rest)
+
+
+def update_ema(net: nnx.Module, ema: nnx.Module, *, decay: float) -> None:
+
+  def update_fn(p_net: nnx.Param, p_ema: nnx.Param) -> nnx.Param:
+    return p_ema * decay + p_net * (1.0 - decay)
+
+  state, rest = nnx.state(net, nnx.Param, ...)
+  graphdef, ema_state, _ = nnx.split(ema, nnx.Param, ...)
+  rest = jtu.tree_map(lambda r: r.copy(), rest)
+  ema_state = jtu.tree_map(update_fn, state, ema_state)
+  nnx.update(ema, ema_state, rest)
