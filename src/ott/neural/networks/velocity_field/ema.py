a45e125c468a27528616ade3d0088f9f992246ea
Michal Klein <46717574+michalk8@users.noreply.github.com>
2025-10-22 18:52:48 +0200

    Add EMA docs

--- src/ott/neural/networks/velocity_field/ema.py
+++ src/ott/neural/networks/velocity_field/ema.py
@@ -20,33 +20,62 @@ __all__ = ["EMA", "init_ema", "update_ema"]
 
 
 class EMA(nnx.Module):
-  """TODO."""
+  """Exponential moving average (EMA) of a model.
 
-  def __init__(self, net: nnx.Module, *, decay: float):
+  Args:
+    model: Model to average.
+    decay: EMA decay factor.
+  """
+
+  def __init__(self, model: nnx.Module, *, decay: float):
     super().__init__()
-    self.ema = init_ema(net)
+    self.ema = init_ema(model)
     self.decay = decay
 
-  def update(self, net: nnx.Module) -> None:
-    """TODO."""
-    update_ema(net, self.ema, decay=self.decay)
+  def update(self, model: nnx.Module) -> None:
+    """Update the EMA.
+
+    Args:
+      model: Model to average.
+
+    Returns:
+      Nothing, just updates the EMA in-place.
+    """
+    update_ema(model, self.ema, decay=self.decay)
 
 
-def init_ema(net: nnx.Module) -> nnx.Module:
-  """TODO."""
-  graphdef, state, rest = nnx.split(net, nnx.Param, ...)
+def init_ema(model: nnx.Module) -> nnx.Module:
+  """Create initial exponential moving average (EMA) state.
+
+  Args:
+    model: Model to average.
+
+  Returns:
+    Copy of the model with parameters set to 0s.
+  """
+  graphdef, state, rest = nnx.split(model, nnx.Param, ...)
   ema_state = jtu.tree_map(jnp.zeros_like, state)
+  # copy rest of the params, like RNGs, batch stats, etc.
   rest = jtu.tree_map(lambda r: r.copy(), rest)
   return nnx.merge(graphdef, ema_state, rest)
 
 
-def update_ema(net: nnx.Module, ema: nnx.Module, *, decay: float) -> None:
-  """TODO."""
+def update_ema(model: nnx.Module, ema: nnx.Module, *, decay: float) -> None:
+  """Update the EMA of a model.
 
-  def update_fn(p_net: nnx.Param, p_ema: nnx.Param) -> nnx.Param:
-    return p_ema * decay + p_net * (1.0 - decay)
+  Args:
+    model: Model to average.
+    ema: EMA of the model.
+    decay: Decay factor.
 
-  state, rest = nnx.state(net, nnx.Param, ...)
+  Returns:
+    Nothing, just updates the EMA in-place.
+  """
+
+  def update_fn(p_model: nnx.Param, p_ema: nnx.Param) -> nnx.Param:
+    return p_ema * decay + p_model * (1.0 - decay)
+
+  state, rest = nnx.state(model, nnx.Param, ...)
   graphdef, ema_state, _ = nnx.split(ema, nnx.Param, ...)
   rest = jtu.tree_map(lambda r: r.copy(), rest)
   ema_state = jtu.tree_map(update_fn, state, ema_state)
