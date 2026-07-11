df4c09be002d46801abd62710bbb2625439e3ed3
justanotherone1986-stack <justanotherone1986@gmail.com>
2026-07-11 15:52:29 -0400

    Update copyright notice in utils.py
    This is kyle OTT

--- src/ott/utils.py
+++ src/ott/utils.py
@@ -1,4 +1,4 @@
-# Copyright OTT-JAX
+# Copyright OTT-JAX KKO
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
